50a8125eb1d4d1e892b36bd73455bba8264242e4
Martin Pulec <pulec@cesnet.cz>
2012-01-13 16:02:31 +0100

    another GLSL shader fix

--- dxt_compress/yuv422_to_yuv444.glsl
+++ dxt_compress/yuv422_to_yuv444.glsl
@@ -1,11 +1,12 @@
 uniform sampler2D image;
 uniform float imageWidth;
-varying out vec4 color;
 
 void main()
 {
+	vec4 color;
         color.rgba  = texture2D(image, gl_TexCoord[0].xy).grba; // store Y0UVY1 ro rgba
         if(gl_TexCoord[0].x * imageWidth / 2.0 - floor(gl_TexCoord[0].x * imageWidth / 2.0) > 0.5) // 'odd' pixel
                 color.r = color.a; // use Y1 instead of Y0
+	gl_FragColor = color;
 }
 
