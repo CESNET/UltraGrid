55bf62b1f83665d6f34a65e991db255847869de1
Martin Pulec <pulec@cesnet.cz>
2012-08-02 16:42:59 +0200

    Moved shared testcard objects to testcard_common
    * solves problem when building dynamic libraries

---
+++ src/video_capture/testcard_common.c
@@ -0,0 +1,227 @@
+/*
+ * FILE:   testcard.c
+ * AUTHOR: Colin Perkins <csp@csperkins.org
+ *         Alvaro Saurin <saurin@dcs.gla.ac.uk>
+ *         Martin Benes     <martinbenesh@gmail.com>
+ *         Lukas Hejtmanek  <xhejtman@ics.muni.cz>
+ *         Petr Holub       <hopet@ics.muni.cz>
+ *         Milos Liska      <xliska@fi.muni.cz>
+ *         Jiri Matela      <matela@ics.muni.cz>
+ *         Dalibor Matura   <255899@mail.muni.cz>
+ *         Ian Wesley-Smith <iwsmith@cct.lsu.edu>
+ *
+ * Copyright (c) 2005-2006 University of Glasgow
+ * Copyright (c) 2005-2010 CESNET z.s.p.o.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ * 
+ *      This product includes software developed by the University of Southern
+ *      California Information Sciences Institute. This product also includes
+ *      software developed by CESNET z.s.p.o.
+ * 
+ * 4. Neither the name of the University, Institute, CESNET nor the names of
+ *    its contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING,
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#include "video_capture/testcard_common.h"
+
+void rgb2yuv422(unsigned char *in, unsigned int width, unsigned int height)
+{
+        unsigned int i, j;
+        int r, g, b;
+        int y, u, v, y1, u1, v1;
+        unsigned char *dst;
+
+        dst = in;
+
+        for (j = 0; j < height; j++) {
+                for (i = 0; i < width; i += 2) {
+                        r = *(in++);
+                        g = *(in++);
+                        b = *(in++);
+                        in++;   /*skip alpha */
+
+                        y = r * 0.299 + g * 0.587 + b * 0.114;
+                        u = b * 0.5 - r * 0.168736 - g * 0.331264;
+                        v = r * 0.5 - g * 0.418688 - b * 0.081312;
+                        //y -= 16;
+                        if (y > 255)
+                                y = 255;
+                        if (y < 0)
+                                y = 0;
+                        if (u < -128)
+                                u = -128;
+                        if (u > 127)
+                                u = 127;
+                        if (v < -128)
+                                v = -128;
+                        if (v > 127)
+                                v = 127;
+                        u += 128;
+                        v += 128;
+
+                        r = *(in++);
+                        g = *(in++);
+                        b = *(in++);
+                        in++;   /*skip alpha */
+
+                        y1 = r * 0.299 + g * 0.587 + b * 0.114;
+                        u1 = b * 0.5 - r * 0.168736 - g * 0.331264;
+                        v1 = r * 0.5 - g * 0.418688 - b * 0.081312;
+                        if (y1 > 255)
+                                y1 = 255;
+                        if (y1 < 0)
+                                y1 = 0;
+                        if (u1 < -128)
+                                u1 = -128;
+                        if (u1 > 127)
+                                u1 = 127;
+                        if (v1 < -128)
+                                v1 = -128;
+                        if (v1 > 127)
+                                v1 = 127;
+                        u1 += 128;
+                        v1 += 128;
+
+                        *(dst++) = (u + u1) / 2;
+                        *(dst++) = y;
+                        *(dst++) = (v + v1) / 2;
+                        *(dst++) = y1;
+                }
+        }
+}
+
+unsigned char *tov210(unsigned char *in, unsigned int width,
+                      unsigned int aligned_x, unsigned int height, double bpp)
+{
+        struct {
+                unsigned a:10;
+                unsigned b:10;
+                unsigned c:10;
+                unsigned p1:2;
+        } *p;
+        unsigned int i, j;
+
+        unsigned int linesize = aligned_x * bpp;
+
+        unsigned char *dst = (unsigned char *)malloc(aligned_x * height * bpp);
+        unsigned char *src;
+        unsigned char *ret = dst;
+
+        for (j = 0; j < height; j++) {
+                p = (void *)dst;
+                dst += linesize;
+                src = in;
+                in += width * 2;
+                for (i = 0; i < width; i += 3) {
+                        unsigned int u, y, v;
+
+                        u = *(src++);
+                        y = *(src++);
+                        v = *(src++);
+
+                        p->a = u << 2;
+                        p->b = y << 2;
+                        p->c = v << 2;
+                        p->p1 = 0;
+
+                        p++;
+
+                        u = *(src++);
+                        y = *(src++);
+                        v = *(src++);
+
+                        p->a = u << 2;
+                        p->b = y << 2;
+                        p->c = v << 2;
+                        p->p1 = 0;
+
+                        p++;
+                }
+        }
+        return ret;
+}
+
+void toR10k(unsigned char *in, unsigned int width, unsigned int height)
+{
+        struct {
+                unsigned r:8;
+
+                unsigned gh:6;
+                unsigned p1:2;
+
+                unsigned bh:4;
+                unsigned p2:2;
+                unsigned gl:2;
+
+                unsigned p3:2;
+                unsigned p4:2;
+                unsigned bl:4;
+        } *d;
+
+        unsigned int i, j;
+
+        d = (void *)in;
+
+        for (j = 0; j < height; j++) {
+                for (i = 0; i < width; i++) {
+                        unsigned int r, g, b;
+
+                        r = *(in++);
+                        g = *(in++);
+                        b = *(in++);
+                        in++;
+
+                        d->r = r;
+                        d->gh = g >> 2;
+                        d->gl = g & 0x3;
+                        d->bh = b >> 4;
+                        d->bl = b & 0xf;
+
+                        d->p1 = 0;
+                        d->p2 = 0;
+                        d->p3 = 0;
+                        d->p4 = 0;
+
+                        d++;
+                }
+        }
+}
+
+char *toRGB(unsigned char *in, unsigned int width, unsigned int height)
+{
+        unsigned int i;
+        unsigned char *ret = malloc(width * height * 3);
+        for(i = 0; i < height; ++i) {
+                vc_copylineRGBAtoRGB(ret + i * width * 3, in + i * width * 4, width * 3);
+        }
+        return (char *) ret;
+}
+
---
+++ src/video_capture/testcard_common.h
@@ -0,0 +1,50 @@
+/*
+ * FILE:   testcard_common.h
+ *
+ * A fake video capture device, used for systems that either have no capture
+ * hardware or do not wish to transmit. This fits the interface of the other
+ * capture devices, but never produces any video.
+ *
+ * Copyright (c) 2004 University of Glasgow
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted provided that the following conditions
+ * are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ * 
+ *      This product includes software developed by the University of Southern
+ *      California Information Sciences Institute.
+ * 
+ * 4. Neither the name of the University nor of the Institute may be used
+ *    to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING,
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+void rgb2yuv422(unsigned char *in, unsigned int width, unsigned int height);
+unsigned char *tov210(unsigned char *in, unsigned int width, unsigned int align_x,
+                unsigned int height, double bpp);
+void toR10k(unsigned char *in, unsigned int width, unsigned int height);
+char * toRGB(unsigned char *in, unsigned int width, unsigned int height);
