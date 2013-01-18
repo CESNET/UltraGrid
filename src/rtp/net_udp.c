50f323880ab7aeb188e3ce407bda4f1ea8622b0e
Martin Pulec <pulec@cesnet.cz>
2013-01-18 10:19:37 +0100

    Windows fix: read with recv() instead of read() from socket

--- src/rtp/net_udp.c
+++ src/rtp/net_udp.c
@@ -548,7 +548,7 @@ void udp_flush_recv_buf(socket_udp *s)
         FD_SET(s->fd, &select_fd);
 
         while(select(s->fd + 1, &select_fd, NULL, NULL, &timeout) > 0) {
-                ssize_t bytes = read(s->fd, buf, len);
+                ssize_t bytes = recv(s->fd, buf, len, 0);
                 if(bytes <= 0)
                         break;
         }
