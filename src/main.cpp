87de2c4b1c52027f5debb376b8870975d38a6309
Castillo, Gerard <gerard.castillo@i2cat.net>
2014-03-10 17:01:14 +0100

    import done from huge refactor

--- src/main.cpp
+++ src/main.cpp
@@ -424,6 +424,7 @@ int main(int argc, char *argv[])
         const char *requested_audio_fec = DEFAULT_AUDIO_FEC;
         char *audio_channel_map = NULL;
         const char *audio_scale = "mixauto";
+        rtsp_serv_t* rtsp_server = NULL;
         bool isStd = FALSE;
         int recv_port_number = PORT_BASE;
         int send_port_number = PORT_BASE;
@@ -948,17 +949,13 @@ int main(int argc, char *argv[])
                                 display_device, requested_mtu,
                                 argc, argv);
         }else if (video_protocol == H264_STD) {
-#ifdef HAVE_RTSP_SERVER
             rtps_types_t avType;
             if(strcmp("none", vidcap_params_get_driver(vidcap_params_head)) != 0 && (strcmp("none",audio_send) != 0)) avType = avStdDyn; //AVStream
             else if((strcmp("none",audio_send) != 0)) avType = audioPCMUdyn; //AStream
             else if(strcmp("none", vidcap_params_get_driver(vidcap_params_head))) avType = videoH264; //VStream
-            else printf("[RTSP SERVER] no stream type... check capture devices input\n");
-            rtsp_server = init_rtsp_server(0, &root_mod, avType); //port, root_module, avType
-            c_start_server(rtsp_server);
-#endif
+            else printf("[RTSP SERVER CHECK] no stream type... check capture devices input...\n");
 
-                uv->state_video_rxtx = new h264_rtp_video_rxtx(&root_mod, video_exporter,
+            uv->state_video_rxtx = new h264_rtp_video_rxtx(&root_mod, video_exporter,
                                 requested_compression, requested_encryption,
                                 requested_receiver, recv_port_number, send_port_number,
                                 ipv6, requested_mcast_if, requested_video_fec, requested_mtu,
