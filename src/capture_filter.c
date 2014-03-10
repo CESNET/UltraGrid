00f4ff1b2c9ab1d81aea5ee7bd57b10561337b05
Castillo, Gerard <gerard.castillo@i2cat.net>
2014-03-10 17:14:22 +0100

    merge

--- src/capture_filter.c
+++ src/capture_filter.c
@@ -61,7 +61,6 @@
 #include "capture_filter/every.h"
 #include "capture_filter/logo.h"
 #include "capture_filter/none.h"
-#include "video_capture/muxer.h"
 
 
 static struct capture_filter_info *capture_filters[] = {
@@ -69,7 +68,6 @@ static struct capture_filter_info *capture_filters[] = {
         &capture_filter_every,
         &capture_filter_logo,
         &capture_filter_none,
-        &capture_filter_muxer,
 };
 
 struct capture_filter {
