5b58e21b264e8b1517a208065fa96dcf3ec7d722
Martin Pulec <pulec@cesnet.cz>
2015-08-20 12:56:39 +0200

    Added unit tests (CppUnit)

---
+++ unittest/run_tests.cpp
@@ -0,0 +1,30 @@
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
+
+extern "C" void exit_uv(int status);
+
+void exit_uv(int status)
+{
+        exit(status);
+}
+
+int main(int, char *[])
+{
+  // Get the top level suite from the registry
+  CPPUNIT_NS::Test *suite = CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest();
+
+  // Adds the test to the list of test to run
+  CPPUNIT_NS::TextUi::TestRunner runner;
+  runner.addTest( suite );
+
+  // Change the default outputter to a compiler error format outputter
+  runner.setOutputter( new CPPUNIT_NS::CompilerOutputter( &runner.result(),
+                                                          CPPUNIT_NS::stdCOut() ) );
+  // Run the test.
+  bool wasSucessful = runner.run();
+
+  // Return error code 1 if the one of test failed.
+  return wasSucessful ? 0 : 1;
+}
+
