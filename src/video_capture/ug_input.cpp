150f2999e8790c748b4feff8b4b518ee50fd981b
Martin Pulec <pulec@cesnet.cz>
2015-09-15 20:35:20 +0200

    Ug_input: listen on both IPv4 and IPv6

--- src/video_capture/ug_input.cpp
+++ src/video_capture/ug_input.cpp
@@ -107,7 +107,7 @@ static void *vidcap_ug_input_init(const struct vidcap_params *cap_params)
         //RTP
         params["mtu"].i = 9000; // doesn't matter anyway...
         // should be localhost and RX TX ports the same (here dynamic) in order to work like a pipe
-        params["receiver"].ptr = (void *) "localhost";
+        params["receiver"].ptr = (void *) "::1";
         if (isdigit(vidcap_params_get_fmt(cap_params)[0]))
                 params["rx_port"].i = atoi(vidcap_params_get_fmt(cap_params));
         else
