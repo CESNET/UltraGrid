226b48ef009958543ad04562dba87cf6a169b20e
Martin Pulec <pulec@cesnet.cz>
2015-12-21 13:34:35 +0100

    Control socket: added "mute" command

--- src/control_socket.cpp
+++ src/control_socket.cpp
@@ -453,6 +453,11 @@ static int process_msg(struct control_state *s, fd_t client_fd, char *message, s
                 if (msg) {
                         resp = send_message(s->root_module, path, (struct message *) msg);
                 }
+        } else if (prefix_matches(message, "mute")) {
+                strncpy(path, "audio.receiver", sizeof path);
+                struct msg_receiver *msg = (struct msg_receiver *) new_message(sizeof(struct msg_receiver));
+                msg->type = RECEIVER_MSG_MUTE;
+                resp = send_message(s->root_module, path, (struct message *) msg);
         } else if(strcasecmp(message, "bye") == 0) {
                 ret = CONTROL_CLOSE_HANDLE;
                 resp = new_response(RESPONSE_OK, NULL);
