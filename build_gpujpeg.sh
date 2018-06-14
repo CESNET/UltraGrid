a10f5b27e003ac2c9005223867826673ef48c270
Martin Pulec <pulec@cesnet.cz>
2018-06-14 15:22:30 +0200

    Added script to build GPUJPEG in MSYS2 (Win)

---
+++ build_gpujpeg.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+PWD=`pwd`
+
+cd gpujpeg
+
+export MSVC_PATH=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio\ 12.0/
+export PATH=$PATH:$MSVC_PATH/Common7/IDE/:$MSVC_PATH/VC/bin/
+export PATH=$PATH:$CUDA_PATH\\bin
+export INCLUDE=.
+
+nvcc -DGPUJPEG_EXPORTS -o gpujpeg.dll --shared src/gpujpeg_*cpp src/gpujpeg*cu
+
+cp gpujpeg.lib /usr/local/lib
+cp gpujpeg.dll /usr/local/bin
+cp -r libgpujpeg /usr/local/include
+
+cd $PWD
+
