9fa27c6223f341495052914eedda76754d263814
Martin Pulec <pulec@cesnet.cz>
2018-10-12 16:07:07 +0200

    GUI: check if UltraGrid executable works
    The UltraGrid binary may be present and executable, however corrupted and
    thus unusable (missing libraries, incompatible libraries in AppImage
    etc.).

--- gui/QT/main.cpp
+++ gui/QT/main.cpp
@@ -12,19 +12,29 @@
 int main(int argc, char *argv[]){
 	QApplication app(argc, argv);
 	QStringList args = QCoreApplication::arguments();
-	QString ultragridExecutable;
+	QString ultragridExecutable = UltragridWindow::findUltragridExecutable();
 	QProcess process;
 
 	//important: If this line is removed parsing float numbers for vu meter fails
 	std::setlocale(LC_NUMERIC, "C");
 
-	process.start("\"" + UltragridWindow::findUltragridExecutable() + "\"");
+	process.start("\"" + ultragridExecutable + "\"");
 	if(process.waitForStarted(1000) == false) {
 		QMessageBox msgBox;
 		msgBox.setText(ultragridExecutable + " doesn't seem to be executable.");
 		msgBox.setInformativeText("Please install uv (UltraGrid binary) to your system "
 				"path or supply a '--with-uv' parameter.\n\n"
 				"Please check also if the binary is executable.");
+		msgBox.setIcon(QMessageBox::Critical);
+		msgBox.exec();
+		return 1;
+	}
+
+	process.waitForFinished();
+
+	if (process.exitStatus() == QProcess::CrashExit || process.exitCode() != 0) {
+		QMessageBox msgBox;
+		msgBox.setText("UltraGrid has crashes when invoked without arguments!");
 		msgBox.setIcon(QMessageBox::Critical);
 		msgBox.exec();
 		return 1;
