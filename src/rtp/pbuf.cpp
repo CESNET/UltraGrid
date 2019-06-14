6efc5c334f6072a26acadd794afdcc47d3cfc76d
Martin Pulec <martin.pulec@cesnet.cz>
2019-06-14 11:07:30 +0200

    Pbuf: print lost count + dups
    * fixed out-of-order packets - duplicate packet is not considered
      out-of-order

--- src/rtp/pbuf.cpp
+++ src/rtp/pbuf.cpp
@@ -111,6 +111,7 @@ struct pbuf {
         uint32_t last_display_ts;
         int longest_gap; // longest loss
         bool out_of_order_pkts;
+        bool dups; // duplicite packets
 };
 
 static void free_cdata(struct coded_data *head);
@@ -356,8 +357,11 @@ void pbuf_insert(struct pbuf *playout_buf, rtp_packet * pkt)
                 }
         }
         unsigned long long current_bit = 1ull << (pkt->seq % number_word_bits);
-        if (playout_buf->packets[pkt->seq / number_word_bits] > current_bit) {
+        if ((playout_buf->packets[pkt->seq / number_word_bits] & ~current_bit) > current_bit) {
                 playout_buf->out_of_order_pkts = true;
+        }
+        if (playout_buf->packets[pkt->seq / number_word_bits] & current_bit) {
+                playout_buf->dups = true;
         }
         playout_buf->packets[pkt->seq / number_word_bits] |= current_bit;
         if ((uint16_t) (pkt->seq - playout_buf->last_report_seq) >= STATS_INTERVAL * 2) {
@@ -388,12 +392,15 @@ void pbuf_insert(struct pbuf *playout_buf, rtp_packet * pkt)
                         << playout_buf->expected_pkts << " packets received ("
                         << (loss_pct < 100.0 ? fg::red : fg::reset)
                         << setprecision(4) << loss_pct << "%" << fg::reset
-                        << "), max loss " << playout_buf->longest_gap
-                        << (playout_buf->out_of_order_pkts ? ", out-of-order pkts" : "") << ".\n";
+                        << "), " << playout_buf->expected_pkts - playout_buf->received_pkts
+                        << " lost, max loss " << playout_buf->longest_gap
+                        << (playout_buf->out_of_order_pkts ? ", reordered pkts" : "")
+                        << (playout_buf->dups ? ", dups" : "") << ".\n";
                 playout_buf->expected_pkts = playout_buf->received_pkts = 0;
                 playout_buf->last_display_ts = pkt->ts;
                 playout_buf->longest_gap = 0;
                 playout_buf->out_of_order_pkts = false;
+                playout_buf->dups = false;
         }
 
         if (playout_buf->frst == NULL && playout_buf->last == NULL) {
