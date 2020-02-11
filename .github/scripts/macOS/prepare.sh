149d08915b2758a42981ef86843986080d7aace5
Martin Pulec <martin.pulec@cesnet.cz>
2020-02-11 17:16:00 +0100

    CI: added Syphon [macOS]
    - removed spurious $SDKPATH
    - added -eu bash flags
    - download/extract to a temporary directory which is then removed

--- .github/scripts/macOS/prepare.sh
+++ .github/scripts/macOS/prepare.sh
@@ -1,8 +1,11 @@
-#!/bin/bash
+#!/bin/bash -eu
+
+TEMP_INST=/tmp/install
+AJA_INST=/var/tmp/ntv2sdk # AJA installation directory
 
 set -ex
 
-echo "::set-env name=AJA_DIRECTORY::/var/tmp/ntv2sdk"
+echo "::set-env name=AJA_DIRECTORY::$AJA_INST"
 echo "::set-env name=UG_SKIP_NET_TESTS::1"
 echo "::set-env name=CPATH::/usr/local/include:/usr/local/opt/qt/include"
 echo "::set-env name=LIBRARY_PATH::/usr/local/lib:/usr/local/opt/qt/lib"
@@ -18,22 +21,26 @@ brew install ffmpeg portaudio sdl2
 brew install ossp-uuid # for cineform
 ( cd cineform-sdk/ && cmake . && make CFHDCodecStatic )
 
+mkdir $TEMP_INST
+cd $TEMP_INST
+
 # Install XIMEA
 if [ -n "$sdk_pass" ]; then
         curl --netrc-file <(cat <<<"machine frakira.fi.muni.cz login sdk password $sdk_pass") https://frakira.fi.muni.cz/~xpulec/sdks/m3api.tar.xz -O
-        sudo tar xJf m3api.tar.xz -C $(xcrun --show-sdk-path)/$SDKPATH/System/Library/Frameworks
+        sudo tar xJf m3api.tar.xz -C $(xcrun --show-sdk-path)/System/Library/Frameworks
 fi
 
 # Install AJA
 if [ -n "$sdk_pass" ]; then
         curl --netrc-file <(cat <<<"machine frakira.fi.muni.cz login sdk password $sdk_pass") https://frakira.fi.muni.cz/~xpulec/sdks/ntv2sdkmac.zip -O
-        unzip ntv2sdkmac.zip -d /var/tmp
-        mv /var/tmp/ntv2sdk* /var/tmp/ntv2sdk
-        cd /var/tmp/ntv2sdk/ajalibraries/ajantv2/build
+        unzip ntv2sdkmac.zip -d /tmp
+        mv /tmp/ntv2sdk* $AJA_INST
+        cd $AJA_INST/ajalibraries/ajantv2/build
         xcodebuild -project ajantv2.xcodeproj
         sudo rm -f /usr/local/lib/libajantv2.dylib
         sudo cp ../../../bin/ajantv2.dylib /usr/local/lib/libajantv2.dylib
         sudo ln -fs /usr/local/lib/libajantv2.dylib /usr/local/lib/ajantv2.dylib
+        cd $TEMP_INST
 fi
 
 # Install live555
@@ -43,3 +50,13 @@ git checkout 35c375
 ./genMakefiles macosx
 make install
 cd ..
+
+# Install Syphon
+wget https://github.com/Syphon/Syphon-Framework/releases/download/5/Syphon.SDK.5.zip
+unzip Syphon.SDK.5.zip
+sudo cp -R 'Syphon SDK 5/Syphon.framework' /Library/Frameworks
+
+# Remove installation files
+cd
+rm -rf $TEMP_INST
+
