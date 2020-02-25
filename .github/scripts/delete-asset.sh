c3aeb202d7f20de7d9f3238ab8caf53c43f62f4b
Martin Pulec <martin.pulec@cesnet.cz>
2020-02-25 14:56:42 +0100

    CI: {delete,upload}-asset.sh - check more errors

--- .github/scripts/delete-asset.sh
+++ .github/scripts/delete-asset.sh
@@ -1,15 +1,39 @@
-#!/bin/sh
+#!/bin/sh -eux
+
+check_errors() {
+        TYPE=$(echo "$1" | jq -r type)
+        if [ "$TYPE" != object ]; then
+                return
+        fi
+        ERRORS=$(echo "$1" | jq -r '.errors')
+        if [ "$ERRORS" != null ]; then
+                echo $ERRORS >&2
+                exit 1
+        fi
+}
+
+check_type() {
+        TYPE=$(echo "$1" | jq -r type)
+        if [ "$TYPE" != "$2" ]; then
+                return
+        fi
+}
 
 TAG_NAME=${1?}
 PATTERN=${2?}
-RELEASE_ID=$(curl -H "Authorization: token $GITHUB_TOKEN" -X GET https://api.github.com/repos/$GITHUB_REPOSITORY/releases/tags/$TAG_NAME | jq -r '.id')
-curl -H "Authorization: token $GITHUB_TOKEN" -X GET https://api.github.com/repos/$GITHUB_REPOSITORY/releases/$RELEASE_ID/assets > assets.json
-LEN=`jq "length" assets.json`
+JSON=$(curl -S -H "Authorization: token $GITHUB_TOKEN" -X GET https://api.github.com/repos/$GITHUB_REPOSITORY/releases/tags/$TAG_NAME)
+check_errors "$JSON"
+RELEASE_ID=$(echo "$JSON" | jq -r '.id')
+JSON=$(curl -S -H "Authorization: token $GITHUB_TOKEN" -X GET https://api.github.com/repos/$GITHUB_REPOSITORY/releases/$RELEASE_ID/assets)
+check_errors "$JSON"
+check_type "$JSON" array 
+LEN=$(echo "$JSON" | jq length)
 for n in `seq 0 $(($LEN-1))`; do
-        NAME=`jq '.['$n'].name' assets.json`
+        NAME=$(echo "$JSON" | jq '.['$n'].name')
         if expr "$NAME" : "\"$PATTERN\"$"; then
-                ID=`jq '.['$n'].id' assets.json`
-                curl -H "Authorization: token $GITHUB_TOKEN" -X DELETE "https://api.github.com/repos/$GITHUB_REPOSITORY/releases/assets/$ID"
+                ID=$(echo "$JSON" | jq '.['$n'].id')
+                JSON=$(curl -S -H "Authorization: token $GITHUB_TOKEN" -X DELETE "https://api.github.com/repos/$GITHUB_REPOSITORY/releases/assets/$ID")
+                check_errors "$JSON"
         fi
 done
 
--- .github/scripts/upload-asset.sh
+++ .github/scripts/upload-asset.sh
@@ -1,10 +1,26 @@
 #!/bin/sh -eu
 
+check_errors() {
+        TYPE=$(echo "$1" | jq -r type)
+        if [ "$TYPE" != object ]; then
+                return
+        fi
+        ERRORS=$(echo "$1" | jq -r '.errors')
+        if [ "$ERRORS" != null ]; then
+                echo $ERRORS >&2
+                exit 1
+        fi
+}
+
 TAG_NAME=${1?}
 FILE=${2?}
 CONTENT_TYPE=${3?}
 LABEL=${4?}
 
-UPLOAD_URL=$(curl -H "Authorization: token $GITHUB_TOKEN" -X GET https://api.github.com/repos/$GITHUB_REPOSITORY/releases/tags/nightly | jq -r .upload_url | sed "s/{.*}//")
+JSON=$(curl -S -H "Authorization: token $GITHUB_TOKEN" -X GET https://api.github.com/repos/$GITHUB_REPOSITORY/releases/tags/nightly)
+check_errors "$JSON"
+UPLOAD_URL=$(echo "$JSON" | jq -r .upload_url | sed "s/{.*}//")
 
-curl -H "Authorization: token $GITHUB_TOKEN" -H "Content-Type: $CONTENT_TYPE" -X POST "$UPLOAD_URL?name=$FILE&label=$LABEL" -T $FILE
+JSON=$(curl -S -H "Authorization: token $GITHUB_TOKEN" -H "Content-Type: $CONTENT_TYPE" -X POST "$UPLOAD_URL?name=$FILE&label=$LABEL" -T $FILE)
+check_errors "$JSON"
+
