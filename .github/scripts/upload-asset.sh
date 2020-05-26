d2aa27da2a74a12c288bf153b5f15d420bd6d155
Martin Pulec <martin.pulec@cesnet.cz>
2020-05-26 20:37:38 +0200

    GitHub CI: use JSON file
    Use JSON as a file to fix '\n' in JSON handling.

--- .github/scripts/upload-asset.sh
+++ .github/scripts/upload-asset.sh
@@ -8,12 +8,11 @@ CONTENT_TYPE=${3?}
 LABEL=${4?}
 
 JSON=$(fetch_json https://api.github.com/repos/$GITHUB_REPOSITORY/releases/tags/$TAG_NAME $GITHUB_TOKEN)
-UPLOAD_URL=$(echo "$JSON" | jq -r .upload_url | sed "s/{.*}//")
+UPLOAD_URL=$(jq -r .upload_url $JSON | sed "s/{.*}//")
 
-TMPNAME=$(mktemp)
-STATUS=$(curl -S -H "Authorization: token $GITHUB_TOKEN" -H "Content-Type: $CONTENT_TYPE" -X POST "$UPLOAD_URL?name=$FILE&label=$LABEL" -T $FILE -w %{http_code} -o $TMPNAME)
-JSON=$(cat $TMPNAME)
-rm $TMPNAME
+JSON=$(mktemp)
+STATUS=$(curl -S -H "Authorization: token $GITHUB_TOKEN" -H "Content-Type: $CONTENT_TYPE" -X POST "$UPLOAD_URL?name=$FILE&label=$LABEL" -T $FILE -w %{http_code} -o $JSON)
 check_errors "$JSON"
 check_status $STATUS
+rm $JSON
 
