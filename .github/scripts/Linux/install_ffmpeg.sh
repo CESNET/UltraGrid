f3003ed18f1ccb412bcb9e779ecbeb9a02613fa7
Martin Pulec <martin.pulec@cesnet.cz>
2020-08-04 16:34:12 +0200

    GitHub CI: build own x264 [Linux]
    In order to support 10-bit codecs.

--- .github/scripts/Linux/install_ffmpeg.sh
+++ .github/scripts/Linux/install_ffmpeg.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -eux
 
 cd /var/tmp/ffmpeg
+( cd nasm && sudo make install )
+( cd x264 && sudo make install )
 ( cd nv-codec-headers && sudo make install )
 ( cd aom/build && sudo make install )
 sudo make install
