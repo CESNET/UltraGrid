6183c2c58021d55f6e60862fa3eb410502215aa0
Martin Pulec <martin.pulec@cesnet.cz>
2020-08-10 12:04:16 +0200

    GitHub CI: added support for SVT to FFmpeg [Linux]

--- .github/scripts/Linux/install_ffmpeg.sh
+++ .github/scripts/Linux/install_ffmpeg.sh
@@ -5,5 +5,7 @@ cd /var/tmp/ffmpeg
 ( cd x264 && sudo make install )
 ( cd nv-codec-headers && sudo make install )
 ( cd aom/build && sudo make install )
+( cd SVT-HEVC/Build/linux/Release && sudo make install )
+( cd SVT-AV1/Build && sudo make install )
 sudo make install
 sudo ldconfig
