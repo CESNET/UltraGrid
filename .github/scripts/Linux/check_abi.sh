7f3a49c58dfe95a63686a29190750c55ecc0c1c7
Martin Pulec <martin.pulec@cesnet.cz>
2020-09-15 08:59:31 +0200

    GitHub CI: check also libc ABI [Lin]

--- .github/scripts/Linux/check_abi.sh
+++ .github/scripts/Linux/check_abi.sh
@@ -1,11 +1,15 @@
 #!/bin/sh -eu
 
-# Checks libstdc++ ABI version
+# Checks libc/ibstdc++ ABI version
 # see also https://gcc.gnu.org/onlinedocs/libstdc++/manual/abi.html
 
-GLIBCXX_MAX=$1
-CXX_MAX=$2
-shift 2
+## @todo
+## consider removing semver.sh and utilize sort -V to compare
+
+GLIBC_MAX=$1
+GLIBCXX_MAX=$2
+CXX_MAX=$3
+shift 3
 
 SEMVER_CMP=$(dirname $0)/utils/semver.sh
 
@@ -15,12 +19,19 @@ if [ ! -x $SEMVER_CMP ]; then
 fi
 
 while test $# -gt 0; do
-        if [ -f $1 ]; then
+        if [ ! -f $1 ]; then
                 shift
                 continue
         fi
+        GLIBC_CUR=$(ldd -r -v $1 | sed -n 's/.*(GLIBC_\([0-9.]*\)).*/\1/p'  | sort -V | tail -n 1)
+        ## @todo
+        ## perpaps use ldd as well for the remaining 2?
         GLIBCXX_CUR=$(nm $1 | sed -n 's/.*GLIBCXX_\([0-9.]*\).*/\1/p' | sort -V | tail -n 1)
         CXX_CUR=$(nm $1 | sed -n 's/.*CXXABI_\([0-9.]*\).*/\1/p' | sort -V | tail -n 1)
+        if [ -n "$GLIBC_CUR" -a "$($SEMVER_CMP $GLIBC_CUR $GLIBC_MAX)" -gt 0 ]; then
+                echo "$1: GLIBC $GLIBC_CUR ($GLIBC_MAX required)" 1>&2
+                exit 1
+        fi
         if [ -n "$GLIBCXX_CUR" -a "$($SEMVER_CMP $GLIBCXX_CUR $GLIBCXX_MAX)" -gt 0 ]; then
                 echo "$1: GLIBCXX $GLIBCXX_CUR ($GLIBCXX_MAX required)" 1>&2
                 exit 1
