0174245524d0d4306e76706aa7a748512e8625f8
Martin Pulec <martin.pulec@cesnet.cz>
2020-10-16 13:51:42 +0200

    JACK audio: Dynamically open library
    The JACK library is generally incompatible with the one bundled in a
    binary distribution. As a solution, the library is dynamically loaded
    from system.

--- src/audio/capture/jack.c
+++ src/audio/capture/jack.c
@@ -1,35 +1,26 @@
+/**
+ * @file   audio/capture/jack.c
+ * @author Martin Pulec     <pulec@cesnet.cz>
+ */
 /*
- * FILE:    audio/capture/jack.c
- * AUTHORS: Martin Benes     <martinbenesh@gmail.com>
- *          Lukas Hejtmanek  <xhejtman@ics.muni.cz>
- *          Petr Holub       <hopet@ics.muni.cz>
- *          Milos Liska      <xliska@fi.muni.cz>
- *          Jiri Matela      <matela@ics.muni.cz>
- *          Dalibor Matura   <255899@mail.muni.cz>
- *          Ian Wesley-Smith <iwsmith@cct.lsu.edu>
- *
- * Copyright (c) 2005-2010 CESNET z.s.p.o.
+ * Copyright (c) 2012-2020 CESNET z.s.p.o.
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, is permitted provided that the following conditions
  * are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- * 
- *      This product includes software developed by CESNET z.s.p.o.
- * 
- * 4. Neither the name of CESNET nor the names of its contributors may be used 
- *    to endorse or promote products derived from this software without specific
- *    prior written permission.
- * 
+ *
+ * 3. Neither the name of CESNET nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING,
  * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
@@ -42,8 +33,6 @@
  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
  * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- *
  */
 
 #ifdef HAVE_CONFIG_H
@@ -73,6 +62,8 @@ static int jack_samplerate_changed_callback(jack_nframes_t nframes, void *arg);
 static int jack_process_callback(jack_nframes_t nframes, void *arg);
 
 struct state_jack_capture {
+        struct libjack_connection *libjack;
+
         struct audio_frame frame;
         jack_client_t *client;
         jack_port_t *input_ports[MAX_PORTS];
@@ -104,7 +95,7 @@ static int jack_process_callback(jack_nframes_t nframes, void *arg)
         }
 
         for (i = 0; i < s->frame.ch_count; ++i) {
-                jack_default_audio_sample_t *in = jack_port_get_buffer(s->input_ports[i], nframes);
+                jack_default_audio_sample_t *in = s->libjack->port_get_buffer(s->input_ports[i], nframes);
                 mux_channel(s->tmp, (char *) in, sizeof(int32_t), channel_size, s->frame.ch_count, i, 1.0);
         }
 
@@ -161,6 +152,11 @@ static void * audio_cap_jack_init(const char *cfg)
                 log_msg(LOG_LEVEL_ERROR, MOD_NAME "Unable to allocate memory.\n");
                 return NULL;
         }
+        s->libjack = open_libjack();
+        if (s->libjack == NULL) {
+                free(s);
+                return NULL;
+        }
 
         char *dup = strdup(cfg);
         assert(dup != NULL);
@@ -192,13 +188,13 @@ static void * audio_cap_jack_init(const char *cfg)
         free(dup);
         dup = NULL;
 
-        s->client = jack_client_open(client_name, JackNullOption, &status);
+        s->client = s->libjack->client_open(client_name, JackNullOption, &status);
         if(status & JackFailure) {
                 log_msg(LOG_LEVEL_ERROR, MOD_NAME "Opening JACK client failed.\n");
                 goto error;
         }
 
-        ports = jack_get_ports(s->client, source_name, NULL, JackPortIsOutput);
+        ports = s->libjack->get_ports(s->client, source_name, NULL, JackPortIsOutput);
         if(ports == NULL) {
                 log_msg(LOG_LEVEL_ERROR, MOD_NAME "Unable to output ports matching \"%s\".\n", source_name);
                 goto release_client;
@@ -219,7 +215,7 @@ static void * audio_cap_jack_init(const char *cfg)
         if (audio_capture_sample_rate) {
                 log_msg(LOG_LEVEL_WARNING, "[JACK capture] Ignoring user specified sample rate!\n");
         }
-        s->frame.sample_rate = jack_get_sample_rate (s->client);
+        s->frame.sample_rate = s->libjack->get_sample_rate (s->client);
         s->frame.max_size = s->frame.ch_count * s->frame.bps * s->frame.sample_rate;
         s->frame.data = malloc(s->frame.max_size);
 
@@ -227,17 +223,17 @@ static void * audio_cap_jack_init(const char *cfg)
 
         s->data = ring_buffer_init(s->frame.max_size);
         
-        if(jack_set_sample_rate_callback(s->client, jack_samplerate_changed_callback, (void *) s)) {
+        if (s->libjack->set_sample_rate_callback(s->client, jack_samplerate_changed_callback, (void *) s)) {
                 log_msg(LOG_LEVEL_ERROR, MOD_NAME "Registring callback problem.\n");
                 goto release_client;
         }
 
-        if(jack_set_process_callback(s->client, jack_process_callback, (void *) s) != 0) {
+        if (s->libjack->set_process_callback(s->client, jack_process_callback, (void *) s) != 0) {
                 log_msg(LOG_LEVEL_ERROR, MOD_NAME "Process callback registration problem.\n");
                 goto release_client;
         }
 
-        if(jack_activate(s->client)) {
+        if (s->libjack->activate(s->client)) {
                 log_msg(LOG_LEVEL_ERROR, MOD_NAME "Cannot activate client.\n");
                 goto release_client;
         }
@@ -248,9 +244,9 @@ static void * audio_cap_jack_init(const char *cfg)
 
                 for(port = 0; port < s->frame.ch_count; port++) {
                         snprintf(name, 32, "capture_%02u", port);
-                        s->input_ports[port] = jack_port_register(s->client, name, JACK_DEFAULT_AUDIO_TYPE, JackPortIsInput, 0);
+                        s->input_ports[port] = s->libjack->port_register(s->client, name, JACK_DEFAULT_AUDIO_TYPE, JackPortIsInput, 0);
                         /* attach ports */
-                        if(jack_connect(s->client, ports[port], jack_port_name(s->input_ports[port]))) {
+                        if (s->libjack->connect(s->client, ports[port], s->libjack->port_name(s->input_ports[port]))) {
                                 log_msg(LOG_LEVEL_ERROR, MOD_NAME "Cannot connect input ports.\n");
                         }
                 }
@@ -263,8 +259,9 @@ static void * audio_cap_jack_init(const char *cfg)
         return s;
 
 release_client:
-        jack_client_close(s->client);   
+        s->libjack->client_close(s->client);
 error:
+        close_libjack(s->libjack);
         free(dup);
         free(s);
         return NULL;
@@ -287,10 +284,11 @@ static void audio_cap_jack_done(void *state)
 {
         struct state_jack_capture *s = (struct state_jack_capture *) state;
 
-        jack_client_close(s->client);
+        s->libjack->client_close(s->client);
         free(s->tmp);
         ring_buffer_destroy(s->data);
         free(s->frame.data);
+        close_libjack(s->libjack);
         free(s);
 }
 
