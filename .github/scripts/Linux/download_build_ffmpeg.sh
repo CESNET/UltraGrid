f7bf7e9ff29ef12b1e437d5893e5e53e1180b3dc
Martin Pulec <martin.pulec@cesnet.cz>
2021-03-03 13:30:16 +0100

    GitHub CI [Linux]: use cmake for build/install instead of make
    To avoid rebuilds when reloaded from cache.

--- .github/scripts/Linux/download_build_ffmpeg.sh
+++ .github/scripts/Linux/download_build_ffmpeg.sh
@@ -17,7 +17,7 @@ cd /var/tmp/ffmpeg
 sed -i '1,/sliceModeData = 1;/s/sliceModeData = 1;/sliceModeData = 8;/' libavcodec/nvenc.c # only first occurence - for H.264, not HEVC
 ( git clone --depth 1 -b nasm-2.13.xx https://github.com/sezero/nasm.git && cd nasm && ./autogen.sh && ./configure && make nasm.1 && make ndisasm.1 && make -j $(nproc) && sudo make install || exit 1 )
 ( git clone --depth 1 http://git.videolan.org/git/x264.git && cd x264 && ./configure --disable-static --enable-shared && make -j $(nproc) && sudo make install || exit 1 )
-( git clone --depth 1 https://aomedia.googlesource.com/aom && mkdir -p aom/build && cd aom/build && cmake -DBUILD_SHARED_LIBS=1 .. && make -j $(nproc) && sudo make install || exit 1 )
+( git clone --depth 1 https://aomedia.googlesource.com/aom && mkdir -p aom/build && cd aom/build && cmake -DBUILD_SHARED_LIBS=1 .. &&  cmake --build . --parallel && sudo cmake --install . || exit 1 )
 install_nv_codec_headers
 install_svt
 ./configure --disable-static --enable-shared --enable-gpl --enable-libx264 --enable-libx265 --enable-libopus --enable-nonfree --enable-nvenc --enable-libaom --enable-libvpx --enable-libspeex --enable-libmp3lame --enable-libsvthevc --enable-libsvtav1
