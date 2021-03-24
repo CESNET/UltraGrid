1a7981f594c4d3822d56f2ffccf2c1fa1dbcb143
Martin Pulec <martin.pulec@cesnet.cz>
2021-03-24 15:58:01 +0100

    upload-ndi-build.sh: removed RDIR
    Replace REMOTE+RDIR with NDI_REMOTE_SSH.
    
    SSH host key is now in NDI_REMOTE_SSH_HOST_KEY_URL.

--- .github/scripts/upload-ndi-build.sh
+++ .github/scripts/upload-ndi-build.sh
@@ -1,15 +1,14 @@
 #!/bin/sh -eu
 
 ASSET=${1?Asset name to upload must be given}
-REMOTE=${REMOTE:-xpulec@frakira.fi.muni.cz}
-RDIR=${RDIR:-'$HOME/Public/ug-ndi-builds'}
-SSH_HOST_KEY_PUB_URL=${SSH_HOST_KEY_PUB:-https://frakira.fi.muni.cz/~xpulec/ssh_known_hosts_github}
+NDI_REMOTE_SSH=${NDI_REMOTE_SSH:-'xpulec@frakira.fi.muni.cz:$HOME/Public/ug-ndi-builds'}
+NDI_REMOTE_SSH_HOST_KEY_URL=${NDI_REMOTE_SSH_KEY_PUB_URL:-https://frakira.fi.muni.cz/~xpulec/ssh_known_hosts_github}
 if [ -z "$SSH_KEY" ]; then
         echo "SSH private key required!" >&2
 fi
 
 mkdir -p ~/.ssh
-curl -S $SSH_HOST_KEY_PUB_URL -o ~/.ssh/known_hosts
+curl -S $NDI_REMOTE_SSH_HOST_KEY_URL -o ~/.ssh/known_hosts
 echo "$SSH_KEY" > ~/.ssh/id_rsa
 chmod -R go-rwx ~/.ssh
-scp $ASSET $REMOTE:$RDIR
+scp $ASSET $NDI_REMOTE_SSH
