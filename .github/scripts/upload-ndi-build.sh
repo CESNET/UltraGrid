7bbbe43917cb466159946c9f575aec089b1052cb
Martin Pulec <martin.pulec@cesnet.cz>
2021-03-25 15:12:49 +0100

    GitHub CI: rename SSH_KEY to NDI_REMOTE_SSH_KEY
    To better denote secret semantics.

--- .github/scripts/upload-ndi-build.sh
+++ .github/scripts/upload-ndi-build.sh
@@ -3,12 +3,13 @@
 ASSET=${1?Asset name to upload must be given}
 NDI_REMOTE_SSH=${NDI_REMOTE_SSH:-'xpulec@frakira.fi.muni.cz:$HOME/Public/ug-ndi-builds'}
 NDI_REMOTE_SSH_HOST_KEY_URL=${NDI_REMOTE_SSH_KEY_PUB_URL:-https://frakira.fi.muni.cz/~xpulec/ssh_known_hosts_github}
-if [ -z "$SSH_KEY" ]; then
+if [ -z "$NDI_REMOTE_SSH_KEY" ]; then
         echo "SSH private key required!" >&2
+        exit 1
 fi
 
 mkdir -p ~/.ssh
 curl -S $NDI_REMOTE_SSH_HOST_KEY_URL -o ~/.ssh/known_hosts
-echo "$SSH_KEY" > ~/.ssh/id_rsa
+echo "$NDI_REMOTE_SSH_KEY" > ~/.ssh/id_rsa
 chmod -R go-rwx ~/.ssh
 scp $ASSET $NDI_REMOTE_SSH
