0d4e07da1dba0459e0447851fdd3b3c891d35eb4
Martin Pulec <martin.pulec@cesnet.cz>
2021-04-15 17:10:32 +0200

    GitHub CI [Linux]: prepare to apply series of patches to FFmpeg
    Use Git-formatted patches.

--- .github/scripts/Linux/download_build_ffmpeg.sh
+++ .github/scripts/Linux/download_build_ffmpeg.sh
@@ -24,13 +24,15 @@ install_nv_codec_headers() {
 
 git clone --depth 1 https://git.ffmpeg.org/ffmpeg.git /var/tmp/ffmpeg
 cd /var/tmp/ffmpeg
-sed -i '1,/sliceModeData = 1;/s/sliceModeData = 1;/sliceModeData = 8;/' libavcodec/nvenc.c # only first occurence - for H.264, not HEVC
 ( git clone --depth 1 -b nasm-2.13.xx https://github.com/sezero/nasm.git && cd nasm && ./autogen.sh && ./configure && make nasm.1 && make ndisasm.1 && make -j $(nproc) && sudo make install || exit 1 )
 ( git clone --depth 1 http://git.videolan.org/git/x264.git && cd x264 && ./configure --disable-static --enable-shared && make -j $(nproc) && sudo make install || exit 1 )
 ( git clone --depth 1 https://aomedia.googlesource.com/aom && mkdir -p aom/build && cd aom/build && cmake -DBUILD_SHARED_LIBS=1 .. &&  cmake --build . --parallel && sudo cmake --install . || exit 1 )
 install_libvpx
 install_nv_codec_headers
 install_svt
-patch -N -p1 < $GITHUB_WORKSPACE/.github/scripts/Linux/nvenc-intra-refresh.patch
+# apply patches
+for n in $GITHUB_WORKSPACE/.github/scripts/Linux/ffmpeg-patches/*patch; do
+        git apply $n
+done
 ./configure --disable-static --enable-shared --enable-gpl --enable-libx264 --enable-libx265 --enable-libopus --enable-nonfree --enable-nvenc --enable-libaom --enable-libvpx --enable-libspeex --enable-libmp3lame --enable-libsvthevc --enable-libsvtav1
 make -j $(nproc)
