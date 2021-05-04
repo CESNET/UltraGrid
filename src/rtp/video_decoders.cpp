c5566f214ed303f9387cd7c7585b9e30317bd49c
Martin Pulec <martin.pulec@cesnet.cz>
2021-05-04 15:17:46 +0200

    Video decoder: fixed passing pitches

--- src/rtp/video_decoders.cpp
+++ src/rtp/video_decoders.cpp
@@ -604,7 +604,7 @@ static void *decompress_worker(void *data)
                         d->compressed->tiles[d->pos].data_len,
                         d->buffer_num,
                         &decoder->frame->callbacks,
-                        &d->internal_codec, nullptr);
+                        &d->internal_codec, d->pitches);
         return d;
 }
 
