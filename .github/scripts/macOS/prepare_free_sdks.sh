a4ada7cca97979b190682e37807516cfa79b27d0
Martin Pulec <martin.pulec@cesnet.cz>
2021-06-22 08:45:25 +0200

    GitHub CI: check ETags for cache keys

--- .github/scripts/macOS/prepare_free_sdks.sh
+++ .github/scripts/macOS/prepare_free_sdks.sh
@@ -1,6 +1,13 @@
 #!/bin/sh -eux
 
+XIMEA_DOWNLOAD_URL=https://www.ximea.com/downloads/recent/XIMEA_OSX_SP.dmg
+
+if [ $# -ge 1 ] && [ x$1 = x-e ]; then
+        $(dirname $0)/../get-etags.sh $XIMEA_DOWNLOAD_URL
+        exit 0
+fi
+
 mkdir -p /var/tmp/sdks-free
 cd /var/tmp/sdks-free
-curl -S -LO https://www.ximea.com/downloads/recent/XIMEA_OSX_SP.dmg
+curl -S -LO $XIMEA_DOWNLOAD_URL
 
