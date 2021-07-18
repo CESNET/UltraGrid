bc3e4e1a28b5aa03424f053c04dd33a3f00e8d70
Martin Pulec <martin.pulec@cesnet.cz>
2021-07-18 22:28:32 +0200

    GitHub CI [ARM]: fixes

--- .github/scripts/Linux/arm/bootstrap.sh
+++ .github/scripts/Linux/arm/bootstrap.sh
@@ -1,9 +1,10 @@
-#!/bin/sh -eu
+#!/bin/sh -exu
 
 # If changing the file, do not forget to regenerate cache in ARM Build GitHub action
 
 ARCH=$1
 OLDPWD=$(pwd)
+CMAKE_FORCE_VER=
 
 if grep -q Raspbian /etc/os-release; then # https://bugs.launchpad.net/ubuntu/+source/qemu/+bug/1670905 workaround
         sed -i s-http://deb.debian.org/debian-http://mirrordirector.raspbian.org/raspbian/- /etc/apt/sources.list
@@ -11,6 +12,7 @@ if grep -q Raspbian /etc/os-release; then # https://bugs.launchpad.net/ubuntu/+s
         curl http://archive.raspberrypi.org/debian/raspberrypi.gpg.key | apt-key add -
         echo 'deb http://archive.raspberrypi.org/debian buster main' >> /etc/apt/sources.list
         apt -y update
+        CMAKE_FORCE_VER="=3.13.4-1" # solves https://gitlab.kitware.com/cmake/cmake/-/issues/20568
 fi
 
 apt -y install build-essential git pkg-config autoconf automake libtool
@@ -19,7 +21,7 @@ apt -y install portaudio19-dev libsdl2-dev libglib2.0-dev libglew-dev libcurl4-o
 # FFmpeg
 if [ $ARCH = armhf ]; then # Raspbian - build own FFmpeg with OMX camera patch
         git clone --depth 1 https://github.com/raspberrypi/firmware.git firmware && mv firmware/* / && echo /opt/vc/lib > /etc/ld.so.conf.d/00-vmcs.conf && ldconfig
-        sed -i '/^deb /p;s/deb/deb-src/' /etc/apt/sources.list
+        sed -i '/^deb /p;s/deb /deb-src /' /etc/apt/sources.list
         apt -y update && apt -y build-dep ffmpeg
         apt -y remove libavcodec58 && apt -y autoremove
         git clone --depth 1 https://github.com/FFmpeg/FFmpeg.git && cd FFmpeg
@@ -32,7 +34,7 @@ else
 fi
 
 # appimagetool
-apt -y install desktop-file-utils git-core libfuse-dev libcairo2-dev cmake wget zsync
+apt -y install desktop-file-utils git-core libfuse-dev libcairo2-dev cmake$CMAKE_FORCE_VER cmake-data$CMAKE_FORCE_VER wget zsync
 git clone -b 12 https://github.com/AppImage/AppImageKit.git
 cd AppImageKit && patch -N -p1 < /mksquashfs-compilation-fix.patch
 ./build.sh
