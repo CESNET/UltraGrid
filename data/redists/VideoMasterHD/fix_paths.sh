de361193227867b9975d819f9749fa08502bdad8
Martin Pulec <martin.pulec@cesnet.cz>
2021-07-19 09:57:52 +0200

    Make: get VideoMasterHD directly from SDK
    Removed data/redists/VideoMaster*.
    
    Now it copies directly from SDK, paths need to be fixed as done with
    .github/scripts/macOS/prepare_nonfree_sdks.sh.

--- data/redists/VideoMasterHD/fix_paths.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-ORIG_PREFIX=/Library/Frameworks
-
-sudo install_name_tool -id @executable_path/../Frameworks/libVideoMasterHD_SP.dylib libVideoMasterHD_SP.dylib
-sudo install_name_tool -id @executable_path/../Frameworks/VideoMasterHD_Audio.framework/Versions/A/VideoMasterHD_Audio VideoMasterHD_Audio.framework/Versions/A/VideoMasterHD_Audio
-sudo install_name_tool -id @executable_path/../Frameworks/VideoMasterHD.framework/Versions/A/VideoMasterHD VideoMasterHD.framework/VideoMasterHD
-sudo install_name_tool -change $ORIG_PREFIX/libVideoMasterHD_SP.dylib @executable_path/../Frameworks/libVideoMasterHD_SP.dylib VideoMasterHD.framework/VideoMasterHD
-sudo install_name_tool -change $ORIG_PREFIX/VideoMasterHD.framework/Versions/A/VideoMasterHD @executable_path/../Frameworks/VideoMasterHD.framework/Versions/A/VideoMasterHD VideoMasterHD_Audio.framework/Versions/A/VideoMasterHD_Audio
