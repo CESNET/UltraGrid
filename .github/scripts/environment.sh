628f3ba74f80bca542ae2f4d73a84cefd818952e
Martin Pulec <martin.pulec@cesnet.cz>
2021-11-02 13:52:02 +0100

    Add release zsync update files to continuous
    As there will be releases 1.7.1, 1.7.2 etc., storing zsync in release
    asset doesn't make sense (further version won't update previous update
    information). Thus store this in continuous release assets - update
    channel "release" was introduced aside to existing "continuous".

--- .github/scripts/environment.sh
+++ .github/scripts/environment.sh
@@ -5,6 +5,7 @@ NDI=--disable-ndi
 if expr $GITHUB_REF : 'refs/tags/'; then
   TAG=${GITHUB_REF#refs/tags/}
   VERSION=${TAG#v}
+  CHANNEL=release
 elif [ $GITHUB_REF = 'refs/heads/ndi-build' ]; then
   NDI=--enable-ndi
   VERSION=ndi
@@ -14,8 +15,13 @@ else
   TAG=$VERSION
 fi
 
-export NDI TAG VERSION
+if [ -z ${CHANNEL-""} ]; then
+        CHANNEL=$VERSION
+fi
 
+export CHANNEL NDI TAG VERSION
+
+echo "CHANNEL=$CHANNEL" >> $GITHUB_ENV
 echo "NDI=$NDI" >> $GITHUB_ENV
 echo "TAG=$TAG" >> $GITHUB_ENV
 echo "VERSION=$VERSION" >> $GITHUB_ENV
