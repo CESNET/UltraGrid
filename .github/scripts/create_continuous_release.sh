ddee6968c8e66cc3c96e56068e22bb87e082abdd
Martin Pulec <martin.pulec@cesnet.cz>
2021-11-02 15:51:23 +0100

    GitHub CI: create continuous release if needed
    For Linux AppImages in release channel, zsync files are stored as an
    artefact in continuous release assets so ensure that it is present.

---
+++ .github/scripts/create_continuous_release.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -eu
+#
+# Ensures that tag "continuous" is present is released on GH. This is required for
+# zsync AppImage release asset.
+#
+
+sudo apt install jq
+URL=$(curl -S -H "Authorization: token $GITHUB_TOKEN" -X GET https://api.github.com/repos/$GITHUB_REPOSITORY/releases/tags/$TAG | jq -r '.url')
+if [ $URL != null ]; then # release exists
+        exit 0
+fi
+git fetch --prune --unshallow --tags
+git tag continuous && git push origin refs/tags/continuous
+curl -S -H "Authorization: token $GITHUB_TOKEN" -X POST $URL -T - <<EOF
+{
+  "tag_name": "continuous"}
+}
