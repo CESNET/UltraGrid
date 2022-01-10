ac6ffee02a55f8f241a5372cbe563f2f0242c836
Martin Pulec <martin.pulec@cesnet.cz>
2022-01-10 12:10:32 +0100

    GitHub CI: build NDI by default if SDK is found
    Removed separate NDI handling (branch ndi-build and build it as upstream (if SDK is found)
    since NDI is now dynamically runtime loaded.

--- .github/scripts/environment.sh
+++ .github/scripts/environment.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
-NDI=--disable-ndi
-
 if expr $GITHUB_REF : 'refs/tags/'; then
   TAG=${GITHUB_REF#refs/tags/}
   VERSION=${TAG#v}
   CHANNEL=release
-elif [ $GITHUB_REF = 'refs/heads/ndi-build' ]; then
-  NDI=--enable-ndi
-  VERSION=ndi
-  TAG=$VERSION
 else
   VERSION=continuous
   TAG=$VERSION
@@ -19,9 +13,8 @@ if [ -z ${CHANNEL-""} ]; then
         CHANNEL=$VERSION
 fi
 
-export CHANNEL NDI TAG VERSION
+export CHANNEL TAG VERSION
 
 echo "CHANNEL=$CHANNEL" >> $GITHUB_ENV
-echo "NDI=$NDI" >> $GITHUB_ENV
 echo "TAG=$TAG" >> $GITHUB_ENV
 echo "VERSION=$VERSION" >> $GITHUB_ENV
