fb79b23ef90e686323f949d5eb3f98c02dd995b3
Martin Pulec <martin.pulec@cesnet.cz>
2022-01-12 15:05:27 +0100

    GitHub CI: enable UDP hole punching

--- .github/scripts/install-common-deps.sh
+++ .github/scripts/install-common-deps.sh
@@ -10,6 +10,17 @@ case "$(uname -s)" in
                 ;;
 esac
 
+install_juice() {
+(
+        git clone https://github.com/paullouisageneau/libjuice.git
+        mkdir libjuice/build
+        cd libjuice/build
+        cmake ..
+        make -j $(nproc)
+        ${SUDO}make install
+)
+}
+
 install_pcp() {
         git clone https://github.com/MartinPulec/pcp.git
         (
@@ -26,6 +37,7 @@ install_zfec() {
         ( cd $GITHUB_WORKSPACE && git submodule update --init ext-deps/zfec || exit 1 )
 }
 
+install_juice
 install_pcp
 install_zfec
 
