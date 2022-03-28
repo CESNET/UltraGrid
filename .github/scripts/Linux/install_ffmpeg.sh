7f96dab49d45dc908cd423b517670e9926516ef0
Martin Pulec <martin.pulec@cesnet.cz>
2022-03-28 08:32:12 +0200

    Partially revert "GitHub CI C/C++ Lin. FFmpeg: disable SVT HEVC/VP9"
    This partially reverts commit 9fbd18b53d9429e1d80bd1090c64258b1669a2ad.
    
    Note: VP9 patch is still not working.

--- .github/scripts/Linux/install_ffmpeg.sh
+++ .github/scripts/Linux/install_ffmpeg.sh
@@ -6,7 +6,7 @@ cd /var/tmp/ffmpeg
 ( cd nv-codec-headers && sudo make install )
 ( cd aom/build && sudo cmake --install . )
 ( cd SVT-AV1/Build && sudo make install )
-#( cd SVT-HEVC/Build/linux/Release && sudo make install || exit 1 )
+( cd SVT-HEVC/Build/linux/Release && sudo make install || exit 1 )
 #( cd SVT-VP9/Build && sudo make install || exit 1 )
 sudo make install
 sudo ldconfig
