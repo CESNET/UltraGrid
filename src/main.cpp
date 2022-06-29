a40d1d7c1854871de5f64776b67e22e255284df1
Martin Pulec <martin.pulec@cesnet.cz>
2022-06-29 10:29:35 +0200

    highlight video capture module name

--- src/main.cpp
+++ src/main.cpp
@@ -434,7 +434,7 @@ static void print_fps(steady_clock::time_point *t0, int *frames, const string &n
         double seconds = duration_cast<duration<double>>(t1 - *t0).count();
         if (seconds >= 5.0) {
                 double fps = *frames / seconds;
-                LOG(LOG_LEVEL_INFO) << "[" << name << "] " << *frames << " frames in " << seconds << " seconds = " << BOLD(fps << " FPS\n");
+                LOG(LOG_LEVEL_INFO) << BOLD(rang::bg::black << rang::fg::bright_green << "[" << name << "]" << rang::fg::reset << rang::bg::reset) << " " << *frames << " frames in " << seconds << " seconds = " << BOLD(fps << " FPS\n");
                 *t0 = t1;
                 *frames = 0;
         }
