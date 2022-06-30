52fad90bbf4c86c98a923f0bf9d9836d6bce1394
Martin Pulec <martin.pulec@cesnet.cz>
2022-06-30 15:47:57 +0200

    GL: option for GLFW window hints
    See previous commit -- where eg. auto-iconify is desirable, user may opt
    in.

--- src/video_display/gl.cpp
+++ src/video_display/gl.cpp
@@ -88,6 +88,7 @@
 #define MOD_NAME         "[GL] "
 #define DEFAULT_WIN_NAME "Ultragrid - OpenGL Display"
 #define GL_DISABLE_10B_OPT_PARAM_NAME "gl-disable-10b"
+#define GL_WINDOW_HINT_OPT_PARAM_NAME "glfw-window-hint"
 #define MAX_BUFFER_SIZE 1
 #define ADAPTIVE_VSYNC -1
 #define SYSTEM_VSYNC 0xFE
@@ -503,7 +504,7 @@ static void gl_print_monitors(bool fullhelp) {
 static void gl_show_help(bool full) {
         cout << "usage:\n";
         cout << rang::style::bold << rang::fg::red << "\t-d gl" << rang::fg::reset << "[:d|:fs[=<monitor>]|:aspect=<v>/<h>|:cursor|:size=X%%|:syphon[=<name>]|:spout[=<name>]|:modeset[=<fps>]|:nodecorate|:fixed_size[=WxH]|:vsync[=<x>|single]]* | gl:[full]help"
-                << (full ? " [--param " GL_DISABLE_10B_OPT_PARAM_NAME "]" : "")
+                << (full ? " [--param " GL_DISABLE_10B_OPT_PARAM_NAME "|" GL_WINDOW_HINT_OPT_PARAM_NAME "=<k>=<v>]" : "")
                 << "\n\n" << rang::style::reset;
         cout << "options:\n";
         cout << BOLD("\td")           << "\t\tdeinterlace\n";
@@ -523,6 +524,7 @@ static void gl_show_help(bool full) {
         cout << BOLD("\t[no]pbo")     << "\t\tWhether or not use PBO (ignore if not sure)\n";
         if (full) {
                 cout << BOLD("\t--param " GL_DISABLE_10B_OPT_PARAM_NAME)     << "\tdo not set 10-bit framebuffer (performance issues)\n";
+                cout << BOLD("\t--param " GL_WINDOW_HINT_OPT_PARAM_NAME) << "<k>=<v>[:<k2>=<v2>] set GLFW window hint key <k> to value <v>, eg. 0x20006=1 to autoiconify (experts only)\n";
         }
 
         printf("\nkeyboard shortcuts:\n");
@@ -1349,9 +1351,35 @@ static GLuint gl_substitute_compile_link(const char *vprogram, const char *fprog
         return ret;
 }
 
+static void display_gl_set_user_window_hints() {
+        const char *hints = get_commandline_param(GL_WINDOW_HINT_OPT_PARAM_NAME);
+        if (hints == nullptr) {
+                return;
+        }
+        vector<char> data(strlen(hints) + 1);
+        copy(hints, hints + strlen(hints) + 1, data.begin());
+        char *hints_c = data.data();
+        char *tmp = hints_c;
+        char *tok = nullptr;
+        char *save_ptr = nullptr;
+        while ((tok = strtok_r(tmp, ":", &save_ptr)) != nullptr) {
+                tmp = nullptr;
+                if (strchr(tok, '=') == nullptr) {
+                        LOG(LOG_LEVEL_ERROR) << MOD_NAME << "Malformed window hint - missing value (expected <k>=<v>).\n";
+                        continue;
+                }
+                int key = stoi(tok, 0, 0);
+                int val = stoi(strchr(tok, '=') + 1, 0, 0);
+                glfwWindowHint(key, val);
+        }
+}
+
 ADD_TO_PARAM(GL_DISABLE_10B_OPT_PARAM_NAME ,
          "* " GL_DISABLE_10B_OPT_PARAM_NAME "\n"
          "  Disable 10 bit codec processing to improve performance\n");
+ADD_TO_PARAM(GL_WINDOW_HINT_OPT_PARAM_NAME ,
+         "* " GL_WINDOW_HINT_OPT_PARAM_NAME "=<k>=<v>[:<k2>=<v2>...]\n"
+         "  Set window hint <k> to value <v>\n");
 /**
  * Initializes OpenGL stuff. If this function succeeds, display_gl_cleanup_opengl() needs
  * to be called to release resources.
@@ -1391,6 +1419,7 @@ static bool display_gl_init_opengl(struct state_gl *s)
                 height = mode->height;
                 glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
         }
+        display_gl_set_user_window_hints();
         if ((s->window = glfwCreateWindow(width, height, IF_NOT_NULL_ELSE(get_commandline_param("window-title"), DEFAULT_WIN_NAME), mon, NULL)) == nullptr) {
                 return false;
         }
