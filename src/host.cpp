67c4bad83bd07d95b4f712713476f3c30d7a6f5a
Martin Pulec <martin.pulec@cesnet.cz>
2022-07-01 14:46:38 +0200

    param help: added usage warning
    --params should not be considered a part of standard options provided to
    users but rather a developer/expert mode.

--- src/host.cpp
+++ src/host.cpp
@@ -690,6 +690,7 @@ bool validate_param(const char *param)
 static bool parse_params(char *optarg)
 {
         if (optarg != nullptr && strcmp(optarg, "help") == 0) {
+                puts("Use of params below is experimental and should be used with a caution and a knowledge of consequences and affected functionality!\n");
                 puts("Params can be one or more (separated by comma) of following:");
                 print_param_doc();
                 return false;
