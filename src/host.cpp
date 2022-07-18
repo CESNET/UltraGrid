CESNET/UltraGrid

commit 1dc899209d78f6f05cb7c729728eddce0b93a180

set output buffering early
setvbuf() should be called on a stream prior to any operation with the
stream. Previously it was to late -- it was even after configuration
summary was printed to stdout.

src/host.cpp
@@ -124,6 +124,8 @@ struct init_data {
         list <void *> opened_libs;
 };
 
+static bool parse_params(char *optarg);
+
 void common_cleanup(struct init_data *init)
 {
         if (init) {
@@ -150,7 +152,7 @@ ADD_TO_PARAM("stdout-buf",
 ADD_TO_PARAM("stderr-buf",
          "* stderr-buf={no|line|full}\n"
          "  Buffering for stderr\n");
-bool set_output_buffering() {
+static bool set_output_buffering() {
         const unordered_map<const char *, pair<FILE *, int>> outs = { // pair<output, default mode>
                 { "stdout-buf", pair{stdout, _IOLBF} },
                 { "stderr-buf", pair{stderr, _IONBF} }
@@ -264,11 +266,18 @@ bool parse_audio_capture_format(const char *optarg)
         return true;
 }
 
-static bool parse_set_logging(int argc, char *argv[])
+/**
+ * Sets things that must be set before anything else (logging and params)
+ *
+ * (params because used by set
+ */
+static bool parse_opts_set_logging(int argc, char *argv[])
 {
         char *log_opt = nullptr;
         static struct option getopt_options[] = {
-                {"verbose", optional_argument, nullptr, 'V'}, { nullptr, 0, nullptr, 0 }
+                {"param", no_argument, nullptr, OPT_PARAM}, // no_argument -- sic (!), see below
+                {"verbose", optional_argument, nullptr, 'V'},
+                { nullptr, 0, nullptr, 0 }
         };
         int saved_opterr = opterr;
         opterr = 0; // options are further handled in main.cpp
@@ -289,7 +298,16 @@ static bool parse_set_logging(int argc, char *argv[])
                                         log_level += 1;
                                 }
                                 break;
-                        default:
+                        case OPT_PARAM:
+                                if (i == argc - 1) {
+                                        fprintf(stderr, "Missing argument to \"--param\"!\n");
+                                        return false;
+                                }
+                                if (!parse_params(argv[i + 1])) {
+                                        return false;
+                                }
+                                break;
+                        default: // will be handled in main
                                 break;
                         }
                 }
@@ -313,9 +331,14 @@ struct init_data *common_preinit(int argc, char *argv[])
         uv_argc = argc;
         uv_argv = argv;
 
-        if (!parse_set_logging(argc, argv)) {
+        if (!parse_opts_set_logging(argc, argv)) {
                 return nullptr;
         }
+
+        if (!set_output_buffering()) {
+                LOG(LOG_LEVEL_WARNING) << "Cannot set console output buffering!\n";
+        }
+        std::clog.rdbuf(std::cout.rdbuf()); // use stdout for logs by default
 
 #ifdef HAVE_X
         void *handle = dlopen(X11_LIB_NAME, RTLD_NOW);
@@ -664,7 +687,7 @@ bool validate_param(const char *param)
 /**
  * Parses command-line parameters given as "--param <key>=<val>[...".
  */
-bool parse_params(char *optarg)
+static bool parse_params(char *optarg)
 {
         if (optarg != nullptr && strcmp(optarg, "help") == 0) {
                 puts("Params can be one or more (separated by comma) of following:");

src/host.h
@@ -65,6 +65,7 @@
 #define EXIT_FAIL_CONTROL_SOCK 9
 #define EXIT_FAIL_NETWORK      10
 #define EXIT_FAIL_AUDIO        11
+#define OPT_PARAM (('O' << 8) | 'P')
 
 #define BUG_MSG "Please report a bug to " PACKAGE_BUGREPORT " if you reach here."
 
@@ -137,9 +138,7 @@ void print_configuration(void);
 
 const char *get_commandline_param(const char *key);
 
-bool set_output_buffering();
 bool parse_audio_capture_format(const char *optarg);
-bool parse_params(char *optarg);
 void register_param(const char *param, const char *doc);
 bool validate_param(const char *param);
 void print_param_doc(void);

src/main.cpp
@@ -137,7 +137,6 @@ static constexpr const char *DEFAULT_AUDIO_CODEC = "PCM";
 #define OPT_IMPORT (('I' << 8) | 'M')
 #define OPT_LIST_MODULES (('L' << 8) | 'M')
 #define OPT_MCAST_IF (('M' << 8) | 'I')
-#define OPT_PARAM (('O' << 8) | 'P')
 #define OPT_PIX_FMTS (('P' << 8) | 'F')
 #define OPT_PROTOCOL (('P' << 8) | 'R')
 #define OPT_START_PAUSED (('S' << 8) | 'P')
@@ -1066,10 +1065,7 @@ static int parse_options(int argc, char *argv[], struct ug_options *opt) {
                         opt->start_paused = true;
                         break;
                 case OPT_PARAM:
-                        if (!parse_params(optarg)) {
-                                return EXIT_SUCCESS;
-                        }
-                        break;
+                        break; // already handled in common_preinit()
                 case OPT_PIX_FMTS:
                         print_pixel_formats();
                         return EXIT_SUCCESS;
@@ -1158,11 +1154,6 @@ static int adjust_params(struct ug_options *opt) {
                 LOG(LOG_LEVEL_WARNING) << "IPv6 support missing, setting IPv4-only mode.\n";
                 opt->force_ip_version = 4;
         }
-
-        if (!set_output_buffering()) {
-                LOG(LOG_LEVEL_WARNING) << "Cannot set console output buffering!\n";
-        }
-        std::clog.rdbuf(std::cout.rdbuf()); // use stdout for logs by default
 
         // default values for different RXTX protocols
         if (strcasecmp(opt->video_protocol, "rtsp") == 0 || strcasecmp(opt->video_protocol, "sdp") == 0) {

src/hd-rum-translator/hd-rum-translator.cpp
@@ -613,9 +613,7 @@ static int parse_fmt(int argc, char **argv, struct cmdline_parameters *parsed)
         } else if(strcmp(argv[start_index], "--verbose") == 0) {
             parsed->verbose = true;
         } else if(strcmp(argv[start_index], "--param") == 0 && start_index < argc - 1) {
-            if (!parse_params(argv[++start_index])) {
-                return -1;
-            }
+            // already handled in common_preinit()
         } else {
             LOG(LOG_LEVEL_FATAL) << MOD_NAME << "Unknown global parameter: " << argv[start_index] << "\n\n";
             usage(argv[0]);

commit e561e459332c7a895659c10b88889f6a44fcd86a

Fixed "--param" handling
The parsing was done early before dynamic modules that export own
parameters were loaded.

Now, the parsing is done in 2 steps -- first scan only for already known
parameters (output buffer settings is needed for preinit), ignore
"help".

The second step is to do the full parse (in main), when there are all
modules loaded (latter in common_preinit()).

Fixes #237.

The regression was introduced by 1dc89920.

src/host.cpp
@@ -124,7 +124,6 @@ struct init_data {
         list <void *> opened_libs;
 };
 
-static bool parse_params(char *optarg);
 static void print_param_doc(void);
 static bool validate_param(const char *param);
 
@@ -276,7 +275,7 @@ bool parse_audio_capture_format(const char *optarg)
 /**
  * Sets things that must be set before anything else (logging and params)
  *
- * (params because used by set
+ * (params because "std{out,err}-buf" param used by set_output_buffering())
  */
 static bool parse_opts_set_logging(int argc, char *argv[])
 {
@@ -310,7 +309,7 @@ static bool parse_opts_set_logging(int argc, char *argv[])
                                         fprintf(stderr, "Missing argument to \"--param\"!\n");
                                         return false;
                                 }
-                                if (!parse_params(argv[i + 1])) {
+                                if (!parse_params(argv[i + 1], true)) {
                                         return false;
                                 }
                                 break;
@@ -694,10 +693,18 @@ bool validate_param(const char *param)
 
 /**
  * Parses command-line parameters given as "--param <key>=<val>[...".
+ *
+ * @param optarg   command-line arguments given to "--param", will be modified by strtok_r(),
+ *                 must not be NULL
+ * @param preinit  true  - only parse/set known parameters (notably output buffer setting), ignore help
+ *                 false - set also remaining parameters including full check and "help" output
+ *
+ * @note
+ * This function will be usually called twice - first with preinit=true and then with false
  */
-static bool parse_params(char *optarg)
+bool parse_params(char *optarg, bool preinit)
 {
-        if (strcmp(optarg, "help") == 0) {
+        if (!preinit && strcmp(optarg, "help") == 0) {
                 puts("Use of params below is experimental and should be used with a caution and a knowledge of consequences and affected functionality!\n");
                 puts("Params can be one or more (separated by comma) of following:");
                 print_param_doc();
@@ -706,8 +713,12 @@ static bool parse_params(char *optarg)
         char *item = nullptr;
         char *save_ptr = nullptr;
         while ((item = strtok_r(optarg, ",", &save_ptr)) != nullptr) {
+                optarg = nullptr;
                 char *key_cstr = item;
                 if (!validate_param(key_cstr)) {
+                        if (preinit) {
+                                continue;
+                        }
                         LOG(LOG_LEVEL_ERROR) << "Unknown parameter: " << key_cstr << "\n";
                         LOG(LOG_LEVEL_INFO) << "Type '" << uv_argv[0] << " --param help' for list.\n";
                         return false;
@@ -719,7 +730,6 @@ static bool parse_params(char *optarg)
                 } else {
                         commandline_params[key_cstr] = string();
                 }
-                optarg = nullptr;
         }
         return true;
 }

src/host.h
@@ -139,6 +139,7 @@ void print_configuration(void);
 const char *get_commandline_param(const char *key);
 
 bool parse_audio_capture_format(const char *optarg);
+bool parse_params(char *optarg, bool preinit);
 void print_pixel_formats(void);
 void print_video_codecs(void);
 void register_param(const char *param, const char *doc);

src/main.cpp
@@ -1065,7 +1065,10 @@ static int parse_options(int argc, char *argv[], struct ug_options *opt) {
                         opt->start_paused = true;
                         break;
                 case OPT_PARAM:
-                        break; // already handled in common_preinit()
+                        if (!parse_params(optarg, false)) {
+                                return EXIT_SUCCESS;
+                        }
+                        break;
                 case OPT_PIX_FMTS:
                         print_pixel_formats();
                         return EXIT_SUCCESS;