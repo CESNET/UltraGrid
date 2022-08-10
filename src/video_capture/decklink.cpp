555935afa1e209d9a4bac785f57e8e134ede352e
Martin Pulec <martin.pulec@cesnet.cz>
2022-08-10 09:27:44 +0200

    vidcap_decklink_grab: simplified
    - do the RGBA conversion only once (not in all branches)
    - exit early if some tile was not captured

--- src/video_capture/decklink.cpp
+++ src/video_capture/decklink.cpp
@@ -1647,31 +1647,23 @@ vidcap_decklink_grab(void *state, struct audio_frame **audio)
                 if (s->state[0].delegate->pixelFrame != NULL &&
                                 s->state[0].delegate->pixelFrameRight != NULL) {
                         s->frame->tiles[0].data = (char*)s->state[0].delegate->pixelFrame;
-                        if (s->codec == RGBA) {
-                            vc_copylineToRGBA_inplace((unsigned char*) s->frame->tiles[0].data,
-                                        (unsigned char*)s->frame->tiles[0].data,
-                                        s->frame->tiles[i].data_len, 16, 8, 0);
-                        }
                         s->frame->tiles[1].data = (char*)s->state[0].delegate->pixelFrameRight;
-                        if (s->codec == RGBA) {
-                            vc_copylineToRGBA_inplace((unsigned char*) s->frame->tiles[1].data,
-                                        (unsigned char*)s->frame->tiles[1].data,
-                                        s->frame->tiles[i].data_len, 16, 8, 0);
-                        }
                         ++count;
                 } // else count == 0 -> return NULL
         } else {
                 for (i = 0; i < s->devices_cnt; ++i) {
-                        if (s->state[i].delegate->pixelFrame != NULL) {
-                                s->frame->tiles[i].data = (char*)s->state[i].delegate->pixelFrame;
-                                if (s->codec == RGBA) {
-                                    vc_copylineToRGBA_inplace((unsigned char*) s->frame->tiles[i].data,
-                                                (unsigned char*)s->frame->tiles[i].data,
-                                                s->frame->tiles[i].data_len, 16, 8, 0);
-                                }
-                                ++count;
-                        } else
+                        if (s->state[i].delegate->pixelFrame == NULL) {
                                 break;
+                        }
+                        s->frame->tiles[i].data = (char*)s->state[i].delegate->pixelFrame;
+                        ++count;
+                }
+        }
+        if (s->codec == RGBA) {
+                for (unsigned i = 0; i < s->frame->tile_count; ++i) {
+                        vc_copylineToRGBA_inplace((unsigned char*) s->frame->tiles[i].data,
+                                        (unsigned char*)s->frame->tiles[i].data,
+                                        s->frame->tiles[i].data_len, 16, 8, 0);
                 }
         }
         if (count == s->devices_cnt) {
