a7d0e447503865923063e76c86e274c78e499763
Martin Pulec <martin.pulec@cesnet.cz>
2022-08-16 15:24:14 +0200

    video_display: use logger for FPS indicator
    Origianlly it was not possible to use colors with logger C API but this
    has changed so this reflects recent changes.

--- src/video_display.c
+++ src/video_display.c
@@ -396,12 +396,10 @@ int display_put_frame(struct display *d, struct video_frame *frame, int flags)
         time_ns_t t = get_time_in_ns();
         long long seconds_ns = t - d->t0;
         if (seconds_ns > 5 * NS_IN_SEC) {
-                if (log_level >= LOG_LEVEL_INFO) {
-                        color_printf(TERM_BOLD TERM_FG_MAGENTA "[%s]" TERM_RESET " %d frames in %g seconds = " TERM_BOLD "%g FPS\n" TERM_RESET,
-                                        d->display_name,
-                                        d->frames, (double) seconds_ns / NS_IN_SEC,
-                                        (double) d->frames * NS_IN_SEC / seconds_ns);
-                }
+                log_msg(LOG_LEVEL_INFO, TERM_BOLD TERM_FG_MAGENTA "[%s]" TERM_RESET " %d frames in %g seconds = " TERM_BOLD "%g FPS\n" TERM_RESET,
+                                d->display_name,
+                                d->frames, (double) seconds_ns / NS_IN_SEC,
+                                (double) d->frames * NS_IN_SEC / seconds_ns);
                 d->frames = 0;
                 d->t0 = t;
         }
