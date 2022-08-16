cc23ccfb65dd6c72998d38fbf902ae5a429f5290
Matej Hrica <492778@mail.muni.cz>
2022-08-16 22:45:21 +0200

    testcard2: add another default font for Linux
    None of the specified font paths exits on Fedora 36 Workstation.
    The added font path exists in a default installation.

--- src/video_capture/testcard2.c
+++ src/video_capture/testcard2.c
@@ -88,8 +88,10 @@ static const char * const font_candidates[] = { "cour.ttf", };
 static const char * const font_candidates[] = { "Monaco.ttf", "Geneva.ttf", "Keyboard.ttf", };
 #else
 #define DEFAULT_FONT_DIR "/usr/share/fonts"
-static const char * const font_candidates[] = { "DejaVuSansMono.ttf", "truetype/freefont/FreeMonoBold.ttf", "truetype/DejaVuSansMono.ttf",
-        "TTF/DejaVuSansMono.ttf", "liberation/LiberationMono-Regular.ttf", }; // Arch
+static const char * const font_candidates[] = {
+        "DejaVuSansMono.ttf", "truetype/freefont/FreeMonoBold.ttf", "truetype/DejaVuSansMono.ttf","TTF/DejaVuSansMono.ttf", "liberation/LiberationMono-Regular.ttf", // Arch
+        "liberation-mono/LiberationMono-Regular.ttf", // Fedora
+}; 
 #endif
 #endif // defined HAVE_LIBSDL_TTF
 
