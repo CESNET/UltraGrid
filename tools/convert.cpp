db02374999a0b3606825ce6a715a1506a6a43f5a
Martin Pulec <martin.pulec@cesnet.cz>
2022-08-18 14:21:51 +0200

    tools/convert: added benchmark

--- tools/convert.cpp
+++ tools/convert.cpp
@@ -1,4 +1,5 @@
 #include <cassert>
+#include <chrono>
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -7,12 +8,38 @@
 #include "../src/config_unix.h"
 #include "../src/video_codec.h"
 
+using std::chrono::duration_cast;
+using std::chrono::high_resolution_clock;
+using std::chrono::microseconds;
 using std::cout;
 using std::cerr;
 using std::ifstream;
 using std::ofstream;
 using std::stoi;
 using std::string;
+using std::vector;
+
+static void benchmark() {
+        int width = 3840;
+        int height = 2160;
+        vector<unsigned char> in(width * height * MAX_BPS + MAX_PADDING);
+        vector<unsigned char> out(width * height * MAX_BPS + MAX_PADDING);
+        for (int i = 0; i < VIDEO_CODEC_END; ++i) {
+                bool src_print = false;
+                for (int j = 0; j < VIDEO_CODEC_END; ++j) {
+                        codec_t inc = static_cast<codec_t>(i);
+                        codec_t outc = static_cast<codec_t>(j);
+                        decoder_t conv = nullptr;
+                        if ((conv = get_decoder_from_to(inc, outc)) == nullptr || i == j) {
+                                continue;
+                        }
+                        auto t0 = high_resolution_clock::now();
+                        conv(out.data(), in.data(), vc_get_linesize(width, outc) * height, DEFAULT_R_SHIFT, DEFAULT_G_SHIFT, DEFAULT_B_SHIFT);
+                        auto t1 = high_resolution_clock::now();
+                        cout << get_codec_name(inc) << "->" << get_codec_name(outc) << ": " << duration_cast<microseconds>(t1 - t0).count() / 1000.0 << " ms\n";
+                }
+        }
+}
 
 static void print_conversions() {
         for (int i = 0; i < VIDEO_CODEC_END; ++i) {
@@ -35,12 +62,17 @@ int main(int argc, char *argv[]) {
                 print_conversions();
                 return 0;
         }
+        if (argc == 2 && string("benchmark") == argv[1]) {
+                benchmark();
+                return 0;
+        }
         if (argc < 7) {
                 cout << "Usage:\n"
-                                "\t" << argv[0] << " <width> <height> <in_codec> <out_codec> <in_file> <out_file> | list-conversions\n"
+                                "\t" << argv[0] << " <width> <height> <in_codec> <out_codec> <in_file> <out_file> | benchmark | list-conversions\n"
                                 "\n"
                                 "where\n"
-                                "\t" << "list-conversions - prints valid conversion pairs\n";
+                                "\t" << "list-conversions - prints valid conversion pairs\n"
+                                "\t" << "benchmark - benchmark conversions\n";
                 return 1;
         }
         int width = stoi(argv[1]);
@@ -58,9 +90,9 @@ int main(int argc, char *argv[]) {
         assert(in.tellg() >= in_size);
         in.seekg (0, ifstream::beg);
 
-        std::vector<char> in_data(in_size);
+        vector<char> in_data(in_size);
         in.read(in_data.data(), in_size);
-        std::vector<char> out_data(vc_get_datalen(width, height, out_codec));
+        vector<char> out_data(vc_get_datalen(width, height, out_codec));
 
         auto *decode = get_decoder_from_to(in_codec, out_codec);
         if (decode == nullptr) {
