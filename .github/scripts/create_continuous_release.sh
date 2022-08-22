67c83f017269b89975fa28c3cdbfe71725e8457c
Martin Pulec <martin.pulec@cesnet.cz>
2022-08-22 11:02:00 +0200

    create_continuous_release.sh: other fixes
    - fetch really 'continuous' release, not $TAG (perhaps c&p error from
      create_release.sh)
    - create tag only if not exists

--- .github/scripts/create_continuous_release.sh
+++ .github/scripts/create_continuous_release.sh
@@ -5,12 +5,14 @@
 #
 
 sudo apt install jq
-URL=$(curl -S -H "Authorization: token $GITHUB_TOKEN" -X GET "https://api.github.com/repos/$GITHUB_REPOSITORY/releases/tags/$TAG" | jq -r '.url')
+URL=$(curl -S -H "Authorization: token $GITHUB_TOKEN" -X GET "https://api.github.com/repos/$GITHUB_REPOSITORY/releases/tags/continuous" | jq -r '.url')
 if [ "$URL" != null ]; then # release exists
         exit 0
 fi
 git fetch --prune --unshallow --tags -f
-git tag continuous && git push origin refs/tags/continuous
+if git tag continuous; then # tag may or may not exists
+        git push origin refs/tags/continuous
+fi
 curl -S -H "Authorization: token $GITHUB_TOKEN" -X POST "$URL" -T - <<EOF
 {
   "tag_name": "continuous"}
