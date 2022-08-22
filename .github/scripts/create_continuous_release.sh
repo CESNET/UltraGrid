284d60dcdf15d8db1fed286d2ab6b25b82731494
Martin Pulec <martin.pulec@cesnet.cz>
2022-08-22 10:24:16 +0200

    create_continuous_release.sh: force fetch
    It looks like git on GH tags without signatures first and thus the
    consequent fetch in create_continuous_release.sh fails because it
    fetches signed tag (although it is the same object).
    
    See also:
    https://github.com/MartinPulec/UltraGrid/actions/runs/2902280934
    
    + enable debugging in the script (`-x`)

--- .github/scripts/create_continuous_release.sh
+++ .github/scripts/create_continuous_release.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -eu
+#!/bin/sh -eux
 #
 # Ensures that tag "continuous" is present is released on GH. This is required for
 # zsync AppImage release asset.
@@ -9,7 +9,7 @@ URL=$(curl -S -H "Authorization: token $GITHUB_TOKEN" -X GET "https://api.github
 if [ "$URL" != null ]; then # release exists
         exit 0
 fi
-git fetch --prune --unshallow --tags
+git fetch --prune --unshallow --tags -f
 git tag continuous && git push origin refs/tags/continuous
 curl -S -H "Authorization: token $GITHUB_TOKEN" -X POST "$URL" -T - <<EOF
 {
