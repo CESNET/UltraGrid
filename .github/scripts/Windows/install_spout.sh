e351f182cd9ec3f69b204b0a1a32e99af1c872ac
Martin Pulec <martin.pulec@cesnet.cz>
2022-09-15 12:55:33 +0200

    CI: use SpoutLibrary build

--- .github/scripts/Windows/install_spout.sh
+++ .github/scripts/Windows/install_spout.sh
@@ -1,15 +1,8 @@
 #!/bin/sh -eux
 # Install SPOUT
 
-git clone -b 2.006 --depth 1 https://github.com/leadedge/Spout2.git
-mkdir Spout2/SpoutSDK/Source/build
-cd Spout2/SpoutSDK/Source/build
-cmake -DBUILD_SHARED_LIBS=ON -G 'MSYS Makefiles' ..
-cmake --build . --parallel
-cp libSpout.dll /usr/local/bin
-cp libSpout.dll.a /usr/local/lib
-cd -
-mkdir /usr/local/include/SpoutSDK
-cp Spout2/SpoutSDK/Source/*.h /usr/local/include/SpoutSDK
-rm -rf Spout2
+git clone --depth 1 https://github.com/leadedge/Spout2.git
+cp Spout2/BUILD/Binaries/x64/SpoutLibrary.dll /usr/local/bin
+cp Spout2/BUILD/Binaries/x64/SpoutLibrary.lib /usr/local/lib
+cp Spout2/SPOUTSDK/SpoutLibrary/SpoutLibrary.h /usr/local/include
 
