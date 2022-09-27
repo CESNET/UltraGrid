0a07ab837f111a79350ae9bd0aff6e1b15b208b6
Martin Pulec <martin.pulec@cesnet.cz>
2022-09-27 09:53:45 +0200

    CI: fixed fetching of AJA assets
    pass also GITHUB_TOKEN when fetching AJA GH assets

--- .github/scripts/Windows/prepare_msys.sh
+++ .github/scripts/Windows/prepare_msys.sh
@@ -62,7 +62,9 @@ install_aja() {(
         aja_release_json=$(fetch_json https://api.github.com/repos/aja-video/ntv2/releases "$GITHUB_TOKEN" array)
         aja_gh_release=$(jq -r '.[0].assets_url' "$aja_release_json")
         rm -- "$aja_release_json"
-        aja_gh_path=$(curl -s "$aja_gh_release" | jq -r '[.[] | select(.name | test(".*libs_windows_.*"))] | .[0].browser_download_url')
+        aja_gh_path_json=$(fetch_json "$aja_gh_release" "$GITHUB_TOKEN" array)
+        aja_gh_path=$(jq -r '[.[] | select(.name | test(".*libs_windows_.*"))] | .[0].browser_download_url' "$aja_gh_path_json")
+        rm -- "$aja_gh_path_json"
         curl -sSL "$aja_gh_path" -o aja_build.zip
         rm README.md # would be overriden from zip below
         unzip aja_build.zip
--- .github/scripts/macOS/prepare.sh
+++ .github/scripts/macOS/prepare.sh
@@ -48,7 +48,9 @@ install_aja() {
         aja_release_json=$(fetch_json https://api.github.com/repos/aja-video/ntv2/releases "$GITHUB_TOKEN" array)
         aja_gh_release=$(jq -r '.[0].assets_url' "$aja_release_json")
         rm -- "$aja_release_json"
-        aja_gh_path=$(curl -s "$aja_gh_release" | jq -r '[.[] | select(.name | test(".*libs_mac_.*"))] | .[0].browser_download_url')
+        aja_gh_path_json=$(fetch_json "$aja_gh_release" "$GITHUB_TOKEN" array)
+        aja_gh_path=$(jq -r '[.[] | select(.name | test(".*libs_mac_.*"))] | .[0].browser_download_url' "$aja_gh_path_json")
+        rm -- "$aja_gh_path_json"
         curl -sSL "$aja_gh_path" | tar xzf -
         sudo cp Release/x64/* /usr/local/lib
         cd $TEMP_INST
