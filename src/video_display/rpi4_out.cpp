e9d85e3c74019c9c4c1c3bd9e124254ae29e7685
Martin Pulec <martin.pulec@cesnet.cz>
2022-10-27 20:14:15 +0200

    rpi4_out: print FPS

--- src/video_display/rpi4_out.cpp
+++ src/video_display/rpi4_out.cpp
@@ -651,7 +651,7 @@ static const struct video_display_info display_rpi4_info = {
         display_rpi4_put_audio_frame,
         display_rpi4_reconfigure_audio,
         DISPLAY_DOESNT_NEED_MAINLOOP,
-        DISPLAY_NO_GENERIC_FPS_INDICATOR,
+        "[RPi display] ",
 };
 
 REGISTER_MODULE(rpi4, &display_rpi4_info, LIBRARY_CLASS_VIDEO_DISPLAY, VIDEO_DISPLAY_ABI_VERSION);
