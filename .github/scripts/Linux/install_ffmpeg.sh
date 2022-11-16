fd8a6e977ac2137a2bd971dc10133dab1496c5aa
Martin Pulec <martin.pulec@cesnet.cz>
2022-11-16 14:24:59 +0100

    GitHub CI [Linux]: use cmake for build/install instead of make
    see also commit f7bf7e9f

--- .github/scripts/Linux/install_ffmpeg.sh
+++ .github/scripts/Linux/install_ffmpeg.sh
@@ -5,8 +5,9 @@ cd /var/tmp/ffmpeg
 ( cd x264 && sudo make install )
 ( cd nv-codec-headers && sudo make install )
 ( cd aom/build && sudo cmake --install . )
-( cd SVT-AV1/Build && sudo make install )
+sudo cmake --install SVT-AV1/Build
 ( cd SVT-HEVC/Build/linux/Release && sudo make install || exit 1 )
-( cd SVT-VP9/Build && sudo make install || exit 1 )
+sudo cmake --install SVT-VP9/Build
+
 sudo make install
 sudo ldconfig
