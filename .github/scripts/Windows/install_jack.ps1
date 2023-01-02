9dcceaf3f373a651cdf173d76a11e3186ed2a171
Martin Pulec <martin.pulec@cesnet.cz>
2023-01-02 10:55:30 +0100

    CI: pass GITHUB_TOKEN to bypass rate limiting
    see https://github.com/MartinPulec/UltraGrid/actions/runs/3821590834/jobs/6500891823

--- .github/scripts/Windows/install_jack.ps1
+++ .github/scripts/Windows/install_jack.ps1
@@ -7,9 +7,9 @@ if (Test-Path 'C:\Program Files\JACK2') {
 # latest release grab inspired by https://gist.github.com/MarkTiedemann/c0adc1701f3f5c215fc2c2d5b1d5efd3
 $repo = "jackaudio/jack2-releases"
 $releases = "https://api.github.com/repos/$repo/releases"
-$tag = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].tag_name
+$tag = (Invoke-WebRequest $releases -Headers @{Authorization = "token ${Env:GITHUB_TOKEN}"} | ConvertFrom-Json)[0].tag_name
 $download = "https://github.com/$repo/releases/download/$tag/jack2-win64-$tag.exe"
-Invoke-WebRequest $download -o jack2.exe
+Invoke-WebRequest $download -Headers @{Authorization = "token ${Env:GITHUB_TOKEN}"} -o jack2.exe
 Start-Process -FilePath '.\jack2.exe' -ArgumentList '/SILENT' -Wait -NoNewWindow
 
 # The lib is moved to the JACK library for 2 reasons:
