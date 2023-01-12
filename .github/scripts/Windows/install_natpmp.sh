1cfc38cfb32bfc0615e0e8285d7530fc76c26e9b
Martin Pulec <martin.pulec@cesnet.cz>
2023-01-12 09:35:49 +0100

    CI: fixed Windows natpmp build error
    Fixes failed run  https://github.com/CESNET/UltraGrid/actions/runs/3894640814/jobs/6648919900.
    
    cmd.exe script for unknown reason ceased to work, copied appropriate
    parts of script to our script.
    
    + use shell debug flag for the script

--- .github/scripts/Windows/install_natpmp.sh
+++ .github/scripts/Windows/install_natpmp.sh
@@ -1,8 +1,13 @@
-#!/bin/sh -eu
+#!/bin/sh -eux
 
 git clone --depth 1 https://github.com/miniupnp/libnatpmp
 cd libnatpmp
-cmd /c build.bat
+
+gcc -c -Wall -Os -DWIN32 -DNATPMP_STATICLIB -DENABLE_STRNATPMPERR getgateway.c
+gcc -c -Wall -Os -DWIN32 -DNATPMP_STATICLIB -DENABLE_STRNATPMPERR natpmp.c
+gcc -c -Wall -Os -DWIN32 wingettimeofday.c
+ar cr natpmp.a getgateway.o natpmp.o wingettimeofday.o
+
 cp natpmp.a /usr/local/lib/libnatpmp.a
 cp natpmp_declspec.h natpmp.h /usr/local/include
 cd -
