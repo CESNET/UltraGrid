d3edea113f4f9b7707382cf86fd99988eadadc78
Martin Pulec <martin.pulec@cesnet.cz>
2023-01-24 09:13:34 +0100

    v210_to_xv30: finish non-full v210 block

--- src/libavcodec/to_lavc_vid_conv.c
+++ src/libavcodec/to_lavc_vid_conv.c
@@ -466,7 +466,7 @@ static void v210_to_xv30(AVFrame * __restrict out_frame, const unsigned char * _
                 const uint32_t *src = (const void *) (in_data + y * vc_get_linesize(width, v210));
                 uint32_t *dst = (uint32_t *)(void *) (out_frame->data[0] + out_frame->linesize[0] * y);
 
-                OPTIMIZED_FOR (int x = 0; x < width / 6; ++x) {
+                OPTIMIZED_FOR (int x = 0; x < (width + 5) / 6; ++x) {
                         uint32_t w0 = *src++;
                         uint32_t w1 = *src++;
                         uint32_t w2 = *src++;
