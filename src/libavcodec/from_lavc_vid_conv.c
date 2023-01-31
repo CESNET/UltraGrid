d00b53ef128f673e9c58f717b7f8ce8b05c4e603
Martin Pulec <martin.pulec@cesnet.cz>
2023-01-31 13:47:39 +0100

    reverted back wrong change from HEAD^^
    the pitch should be actually the linesize

--- src/libavcodec/from_lavc_vid_conv.c
+++ src/libavcodec/from_lavc_vid_conv.c
@@ -2344,7 +2344,7 @@ void av_to_uv_convert(av_to_uv_convert_t *state, char * __restrict dst_buffer, A
                 src_linesize = vc_get_linesize(width, priv->src_pixfmt);
                 dec_input = tmp = malloc(vc_get_datalen(width, height, priv->src_pixfmt) + MAX_PADDING);
                 int default_rgb_shift[] = { DEFAULT_R_SHIFT, DEFAULT_G_SHIFT, DEFAULT_B_SHIFT };
-                priv->convert((char *) dec_input, in_frame, width, height, vc_get_size(width, priv->src_pixfmt), default_rgb_shift);
+                priv->convert((char *) dec_input, in_frame, width, height, src_linesize, default_rgb_shift);
         }
         if (priv->dec) {
                 int dst_size = vc_get_size(width, priv->dst_pixfmt);
