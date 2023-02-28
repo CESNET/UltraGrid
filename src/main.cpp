d04fff8c9e1f810cd0ec5b9f7f12a9da9262e3c3
Martin Pulec <martin.pulec@cesnet.cz>
2023-02-28 15:33:55 +0100

    main: catch stoi exceptions
    Using short getopt options, a user may make a mistake in parameter name
    resulting in passing a non-numeric string to stoi, which leads to crash
    on not-nice uncaught exception crash.
    
    + remove try/catch from parse_port (no longer needed due the above)

--- src/main.cpp
+++ src/main.cpp
@@ -744,30 +744,25 @@ struct ug_options {
 };
 
 static bool parse_port(char *optarg, struct ug_options *opt) {
-        try {
-                if (strcmp(optarg, "help") == 0) {
-                        color_printf("see\n\n    " TBOLD("%s --fullhelp") "\n\nfor port specification usage\n", uv_argv[0]);
-                }
-                if (strchr(optarg, ':') != nullptr) {
-                        char *save_ptr = nullptr;
-                        opt->video_rx_port = stoi(strtok_r(optarg, ":", &save_ptr), nullptr, 0);
-                        opt->video_tx_port = stoi(strtok_r(nullptr, ":", &save_ptr), nullptr, 0);
-                        char *tok = nullptr;
+        if (strcmp(optarg, "help") == 0) {
+                color_printf("see\n\n    " TBOLD("%s --fullhelp") "\n\nfor port specification usage\n", uv_argv[0]);
+        }
+        if (strchr(optarg, ':') != nullptr) {
+                char *save_ptr = nullptr;
+                opt->video_rx_port = stoi(strtok_r(optarg, ":", &save_ptr), nullptr, 0);
+                opt->video_tx_port = stoi(strtok_r(nullptr, ":", &save_ptr), nullptr, 0);
+                char *tok = nullptr;
+                if ((tok = strtok_r(nullptr, ":", &save_ptr)) != nullptr) {
+                        opt->audio.recv_port = stoi(tok, nullptr, 0);
                         if ((tok = strtok_r(nullptr, ":", &save_ptr)) != nullptr) {
-                                opt->audio.recv_port = stoi(tok, nullptr, 0);
-                                if ((tok = strtok_r(nullptr, ":", &save_ptr)) != nullptr) {
-                                        opt->audio.send_port = stoi(tok, nullptr, 0);
-                                } else {
-                                        usage(uv_argv[0]);
-                                        return false;
-                                }
+                                opt->audio.send_port = stoi(tok, nullptr, 0);
+                        } else {
+                                usage(uv_argv[0]);
+                                return false;
                         }
-                } else {
-                        opt->port_base = stoi(optarg, nullptr, 0);
                 }
-        } catch (invalid_argument &e) {
-                LOG(LOG_LEVEL_ERROR) << MOD_NAME << "Non-numeric value passed!\n";
-                return false;
+        } else {
+                opt->port_base = stoi(optarg, nullptr, 0);
         }
         if (opt->audio.recv_port < -1 || opt->audio.send_port < -1 || opt->video_rx_port < -1 || opt->video_tx_port < -1 || opt->port_base < -1 ||
                         opt->audio.recv_port > UINT16_MAX || opt->audio.send_port > UINT16_MAX || opt->video_rx_port > UINT16_MAX || opt->video_tx_port > UINT16_MAX || opt->port_base > UINT16_MAX) {
@@ -1416,8 +1411,17 @@ int main(int argc, char *argv[])
         print_version();
         printf("\n");
 
-        if (int ret = parse_options(argc, argv, &opt)) {
-                EXIT(ret < 0 ? -ret : EXIT_SUCCESS);
+        try {
+                if (int ret = parse_options(argc, argv, &opt)) {
+                        EXIT(ret < 0 ? -ret : EXIT_SUCCESS);
+                }
+        } catch (invalid_argument &e) {
+                if (strcmp(e.what(), "stoi") == 0) {
+                        LOG(LOG_LEVEL_ERROR) << MOD_NAME << "Non-numeric value passed to option expecting a number!\n";
+                } else {
+                        LOG(LOG_LEVEL_ERROR) << MOD_NAME << "Invalid argument: " << e.what() << "!\n";
+                }
+                return -1;
         }
 
         if (int ret = adjust_params(&opt)) {
