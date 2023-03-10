cdcf1abc3027e9066b62e57020c99c6ce4210422
Martin Pulec <martin.pulec@cesnet.cz>
2023-03-10 15:22:56 +0100

    video decompressors: update copyright dates

--- src/video_decompress/cineform.cpp
+++ src/video_decompress/cineform.cpp
@@ -3,7 +3,7 @@
  * @author Martin Piatka     <piatka@cesnet.cz>
  */
 /*
- * Copyright (c) 2019 CESNET, z. s. p. o.
+ * Copyright (c) 2019-2023 CESNET, z. s. p. o.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
