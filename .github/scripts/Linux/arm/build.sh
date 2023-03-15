e46d5cfed3ccca8a914ef40d202fd2c9d6217339
Martin Pulec <martin.pulec@cesnet.cz>
2023-03-15 09:53:28 +0100

    CI ARM: configure - require present features
    Explicitly require features that are already present in the builds
    (tenative, some may be removed, but just to ensure that features that
    should be present get not removed by accident).

--- .github/scripts/Linux/arm/build.sh
+++ .github/scripts/Linux/arm/build.sh
@@ -10,7 +10,17 @@ export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:${PKG_CONFIG_PATH:+":$PKG_CONFIG
 ARCH=$(dpkg --print-architecture)
 APPNAME=UltraGrid-latest-${ARCH}.AppImage
 
-./autogen.sh --enable-plugins
+set -- --enable-plugins --enable-openssl --enable-soxr                                                  # general
+set -- "$@" --enable-alsa --enable-jack --enable-jack-transport                                         # audio
+set -- "$@" --enable-decklink --enable-file --enable-rtsp --enable-screen=x11 --enable-swmix --enable-v4l2 # vcap
+set -- "$@" --enable-gl-display --enable-panogl_disp --enable-sdl                                       # display
+set -- "$@" --enable-libavcodec --enable-rtdxt --enable-libswscale --enable-uyvy                        # compression
+set -- "$@" --enable-blank --enable-scale --enable-testcard-extras                                      # extras (pp. etc)
+if [ "$ARCH" = armhf ]; then # Raspbian
+        set -- "$@" --enable-lavc-hw-accel-rpi4
+fi
+
+./autogen.sh "$@"
 make -j "$(nproc)"
 make check
 make distcheck
