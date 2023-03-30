757b03fd6732dd388dbfef4fc2ffa760d2df17dd
Martin Pulec <martin.pulec@cesnet.cz>
2023-03-30 13:26:22 +0200

    cuda_wrapper/cuda_runtime.h wrapper: added documentation

--- src/cuda_wrapper/cuda_runtime.h
+++ src/cuda_wrapper/cuda_runtime.h
@@ -1,3 +1,43 @@
+/**
+ * @file   cuda_wrapper/cuda_runtime.h
+ * @author Martin Pulec     <pulec@cesnet.cz>
+ *
+ * Replaces some CUDA functions with wrapped variant for modules, that doesn't
+ * use cuda_wrapper directly. Currently used by LDGM, which is outside UG so we
+ * don't make it dependent on UltraGrid codebase).
+ */
+/*
+ * Copyright (c) 2014 CESNET z.s.p.o.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of CESNET nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING,
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 #define cudaError_t int
 #define cudaFreeHost cuda_wrapper_free_host
 #define cudaMalloc cuda_wrapper_malloc
