c7c77ed7f76c2d69c78c76d0abd2973dd787d32a
Martin Pulec <martin.pulec@cesnet.cz>
2023-05-02 08:41:35 +0200

    use SoundFont 3 (compressed)

--- .github/scripts/defs.sh
+++ .github/scripts/defs.sh
@@ -1,1 +1,1 @@
-DEFAULT_SF_URL=http://frakira.fi.muni.cz/~xpulec/TimGM6mb_but_fixed__piano_.sf2 # https://musical-artifacts.com/artifacts/2642
+DEFAULT_SF_URL=http://frakira.fi.muni.cz/~xpulec/TimGM6mb_but_fixed__piano_.sf3 # https://musical-artifacts.com/artifacts/2642
