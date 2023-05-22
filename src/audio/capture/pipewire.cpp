69bc251a27b7ca7f79d731e68df721076f3e10d1
Martin Piatka <piatka@cesnet.cz>
2023-05-22 15:33:09 +0200

    acap/pipewire: Stream setup & reading buffers

--- src/audio/capture/pipewire.cpp
+++ src/audio/capture/pipewire.cpp
@@ -41,6 +41,7 @@
 #endif
 
 #include <memory>
+#include <vector>
 
 #include "audio/audio_capture.h"
 #include "audio/types.h"
@@ -61,6 +62,71 @@ struct state_pipewire_cap{
 
         audio_desc desc;
         ring_buffer_uniq ring_buf;
+
+        struct audio_frame frame;
+        std::vector<char> frame_data;
+};
+
+/*
+ * Called from real time thread. Only realtime-safe function calls allowed here.
+ */
+static void on_process(void *state){
+        auto s = static_cast<state_pipewire_cap *>(state);
+
+        struct pw_buffer *b = pw_stream_dequeue_buffer(s->stream.get());
+        if (!b) {
+                pw_log_warn("out of buffers: %m");
+                return;
+        }
+        struct spa_buffer *buf = b->buffer;
+
+        char *src = static_cast<char *>(buf->datas[0].data);
+        if (!src)
+                return;
+
+        assert(buf->datas[0].chunk->offset == 0);
+        ring_buffer_write(s->ring_buf.get(), src, buf->datas[0].chunk->size);
+
+        pw_stream_queue_buffer(s->stream.get(), b);
+}
+
+static void on_param_changed(void *state, uint32_t id, const struct spa_pod *param){
+        spa_audio_info audio_params;
+
+        if(!param || id != SPA_PARAM_Format)
+                return;
+
+        //TODO check return
+        spa_format_parse(param, &audio_params.media_type, &audio_params.media_subtype);
+        if(audio_params.media_type != SPA_MEDIA_TYPE_audio
+                        || audio_params.media_subtype != SPA_MEDIA_SUBTYPE_raw)
+        {
+                return;
+        }
+
+        spa_format_audio_raw_parse(param, &audio_params.info.raw);
+
+        log_msg(LOG_LEVEL_NOTICE, "Format change: %u %u %u\n",
+                        audio_params.info.raw.format,
+                        audio_params.info.raw.rate,
+                        audio_params.info.raw.channels);
+
+        //TODO: Actually handle format changes
+}
+
+const static pw_stream_events stream_events = { 
+        .version = PW_VERSION_STREAM_EVENTS,
+        .destroy = nullptr,
+        .state_changed = nullptr,
+        .control_info = nullptr,
+        .io_changed = nullptr,
+        .param_changed = on_param_changed,
+        .add_buffer = nullptr,
+        .remove_buffer = nullptr,
+        .process = on_process,
+        .drained = nullptr,
+        .command = nullptr,
+        .trigger_done = nullptr,
 };
 
 static void audio_cap_pw_help(){
@@ -94,14 +160,84 @@ static void *audio_cap_pipewire_init(struct module *parent, const char *cfg){
 
         initialize_pw_common(s->pw);
 
+        auto props = pw_properties_new(
+                        PW_KEY_MEDIA_TYPE, "Audio",
+                        PW_KEY_MEDIA_CATEGORY, "Capture",
+                        PW_KEY_MEDIA_ROLE, "Communication",
+                        PW_KEY_APP_NAME, "UltraGrid",
+                        PW_KEY_APP_ICON_NAME, "ultragrid",
+                        PW_KEY_NODE_NAME, "ug capture",
+                        PW_KEY_NODE_TARGET, s->target.c_str(), //TODO: deprecated in newer
+                        nullptr);
+
+        //TODO: Don't hardcode these
+        unsigned rate = 48000;
+        unsigned quant = 128;
+        spa_audio_format format = SPA_AUDIO_FORMAT_S16;
+        unsigned ch_count = 1;
+
+        s->frame.ch_count = ch_count;
+        s->frame.bps = 2;
+        s->frame.sample_rate = rate;
+        s->frame.max_size = s->frame.ch_count * s->frame.bps * s->frame.sample_rate;
+        s->frame_data.resize(s->frame.max_size);
+        s->frame.data = s->frame_data.data();
+
+        pw_properties_setf(props, PW_KEY_NODE_RATE, "1/%u", rate);
+        pw_properties_setf(props, PW_KEY_NODE_LATENCY, "%u/%u", quant, rate);
+
+        std::byte buffer[1024];
+        auto pod_builder = SPA_POD_BUILDER_INIT(buffer, sizeof(buffer));
+
+        auto audio_info = SPA_AUDIO_INFO_RAW_INIT(
+                        .format = format,
+                        .rate = rate,
+                        .channels = ch_count,
+                        );
+        const spa_pod *params = spa_format_audio_raw_build(&pod_builder, SPA_PARAM_EnumFormat,
+                        &audio_info);
+
+        /*
+         * Pipewire thread loop lock
+         */
+        pipewire_thread_loop_lock_guard lock(s->pw.pipewire_loop.get());
+
+        s->stream.reset(pw_stream_new(
+                                s->pw.pipewire_core.get(),
+                                "UltraGrid capture",
+                                props));
+
+        pw_stream_add_listener(
+                        s->stream.get(),
+                        &s->stream_listener.get(),
+                        &stream_events,
+                        s.get());
+
+        int buf_len_ms = 50;
+        int ring_size = /*desc.bps TODO*/ 2 * ch_count * (rate * buf_len_ms / 1000);
+        s->ring_buf.reset(ring_buffer_init(ring_size));
+
+        pw_stream_connect(s->stream.get(),
+                        PW_DIRECTION_INPUT,
+                        PW_ID_ANY,
+                        static_cast<pw_stream_flags>(
+                                PW_STREAM_FLAG_AUTOCONNECT |
+                                PW_STREAM_FLAG_MAP_BUFFERS |
+                                PW_STREAM_FLAG_RT_PROCESS),
+                        &params, 1);
+
         return s.release();
 }
 
 static struct audio_frame *audio_cap_pipewire_read(void *state){
         auto s = static_cast<state_pipewire_cap *>(state);
 
+        s->frame.data_len = ring_buffer_read(s->ring_buf.get(), s->frame.data, s->frame.max_size);
 
-        return nullptr;
+        if(!s->frame.data_len)
+                return nullptr;
+
+        return &s->frame;
 }
 
 static void audio_cap_pipewire_done(void *state){
