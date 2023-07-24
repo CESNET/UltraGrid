10c280959bb192042d3bd4cb84ecfc84a3ed65fb
Martin Pulec <martin.pulec@cesnet.cz>
2023-07-24 16:19:15 +0200

    Revert "CI Linux FFmpeg: remove NVENC IDR patch"
    This reverts commit e457ab87f74b9f1cd784b289300c34f59d5591eb but the
    patch is updated to match current FFmpeg.
    
    The FFmpeg commit that seemed to do the same (7eeef72c6) is not working
    correctly because it assigns idrPeriod cc->gopLength, which was set to
    NVENC_INFINITE_GOPLENGTH above in case of intra-refresh.
    
    refer to GH-330

--- src/video_compress/libavcodec.cpp
+++ src/video_compress/libavcodec.cpp
@@ -1539,19 +1539,16 @@ static void configure_nvenc(AVCodecContext *codec_ctx, struct setparam_param *pa
         }
 
         set_forced_idr(codec_ctx, 1);
-#if LIBAVCODEC_VERSION_INT >= AV_VERSION_INT(60, 22, 100)
-        const bool new_ff = true;
+#ifdef PATCHED_FF_NVENC_NO_INFINITE_GOP
+        const bool patched_ff = true;
 #else
-        const bool new_ff = false;
+        const bool patched_ff = false;
         if (param->periodic_intra != 0) {
-                LOG(LOG_LEVEL_WARNING) << MOD_NAME
-                    "Old FFmpeg, " << (param->periodic_intra != 1 ? "not " : "")
-                                       << "enabling Intra Refresh.\n";
+                LOG(LOG_LEVEL_WARNING) << MOD_NAME "FFmpeg not patched, " << (param->periodic_intra != 1 ? "not " : "") << "enabling Intra Refresh.\n";
         }
 #endif
 
-        if ((new_ff && param->periodic_intra != 0) ||
-            param->periodic_intra == 1) {
+        if ((patched_ff && param->periodic_intra != 0) || param->periodic_intra == 1) {
                 check_av_opt_set<int>(codec_ctx->priv_data, "intra-refresh", 1);
         }
 
