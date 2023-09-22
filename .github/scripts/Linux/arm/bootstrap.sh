bca280918df3a619090187be66f5ce7f97e6c53d
Martin Pulec <martin.pulec@cesnet.cz>
2023-09-22 10:02:23 +0200

    CI ARM: update to bullseye

--- .github/scripts/Linux/arm/bootstrap.sh
+++ .github/scripts/Linux/arm/bootstrap.sh
@@ -2,35 +2,19 @@
 
 # If changing the file, do not forget to regenerate cache in ARM Build GitHub action
 
-ARCH=$1
 OLDPWD=$(pwd)
-
-raspbian_build_sdl2() {
-        (
-        apt-get -y build-dep libsdl2-dev
-        apt -y install libgbm-dev
-        SDL_VER=2.0.10 # 2.0.14 doesn't compile with Rasbpian 10
-        curl -k -LO https://www.libsdl.org/release/SDL2-$SDL_VER.tar.gz
-        tar xaf SDL2-$SDL_VER.tar.gz
-        cd SDL2-$SDL_VER
-        ./configure --enable-video-kmsdrm
-        make -j "$(nproc)" install
-        )
-}
 
 apt -y install curl gnupg
 echo -k > ~/.curlrc
 
 if grep -q Raspbian /etc/os-release; then
         curl http://archive.raspberrypi.org/debian/raspberrypi.gpg.key | apt-key add -
-        echo 'deb http://archive.raspberrypi.org/debian buster main' >> /etc/apt/sources.list
+        echo 'deb http://archive.raspberrypi.org/debian bullseye main' >> /etc/apt/sources.list
         apt -y update
         apt -y install libraspberrypi-dev
 fi
 
-apt -y install cmake=3.13.4-1 cmake-data=3.13.4-1 # 3.16 in the above added repository is broken with chrooted qemu-user-static
-
-apt -y install autoconf automake build-essential git pkg-config libtool sudo
+apt -y install autoconf automake build-essential cmake git pkg-config libtool sudo
 apt -y install libcurl4-openssl-dev libsoxr-dev libspeexdsp-dev libssl-dev
 apt -y install libasound2-dev portaudio19-dev libjack-dev
 apt -y install libglew-dev libglfw3-dev libglm-dev
