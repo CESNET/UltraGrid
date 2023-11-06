0d54328bbcb272c1ea262fe6f1772fb5d699dfed
Martin Pulec <martin.pulec@cesnet.cz>
2023-11-06 09:08:46 +0100

    CI Win: build own Spout
    closes GH-352
    
    It looks like the pre-build library BUILD/Binaries/x64/SpoutLibrary.dll
    is no longer compatible with the headers in Git.
    
    Pre-built library version is 2.007.010 while the latest version on GitHub
    is Spout 2.007.012. When building Spout from sources, the referenced
    issue no longer occurs. Also the libraries are not expected to be included
    in the Git in future (leadedge/Spout2#103).

--- .github/scripts/Windows/install_spout.sh
+++ .github/scripts/Windows/install_spout.sh
@@ -2,7 +2,11 @@
 # Install SPOUT
 
 git clone --depth 1 https://github.com/leadedge/Spout2.git
-cp Spout2/BUILD/Binaries/x64/SpoutLibrary.dll /usr/local/bin
-cp Spout2/BUILD/Binaries/x64/SpoutLibrary.lib /usr/local/lib
-cp Spout2/SPOUTSDK/SpoutLibrary/SpoutLibrary.h /usr/local/include
+cd Spout2
+/c/Program\ Files/CMake/bin/cmake.exe -Bbuild2 . # ./BUILD already exists
+/c/Program\ Files/CMake/bin/cmake.exe --build build2 --parallel
+mkdir -p /usr/local/bin /usr/local/include /usr/local/lib
+cp build2/Binaries/x64/SpoutLibrary.dll /usr/local/bin
+cp build2/Binaries/x64/SpoutLibrary.lib /usr/local/lib
+cp SPOUTSDK/SpoutLibrary/SpoutLibrary.h /usr/local/include
 
