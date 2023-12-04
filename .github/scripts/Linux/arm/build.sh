5782dfd3cfbbaf9dd06cd634ff76bd1737a0c662
Martin Pulec <martin.pulec@cesnet.cz>
2023-12-04 13:23:59 +0100

    CI ARM: enforce Pipewire + Vulkan (64b only)

--- .github/scripts/Linux/arm/build.sh
+++ .github/scripts/Linux/arm/build.sh
@@ -11,15 +11,18 @@ ARCH=$(dpkg --print-architecture)
 APPNAME=UltraGrid-latest-${ARCH}.AppImage
 
 set -- --enable-plugins --enable-openssl --enable-soxr --enable-speexdsp                                # general
-set -- "$@" --enable-alsa --enable-jack --enable-jack-transport                                         # audio
+set -- "$@" --enable-alsa --enable-jack --enable-jack-transport --enable-pipewire                       # audio
 # vidcap (+ duplex video)
 set -- "$@" --enable-decklink --enable-file --enable-ndi --enable-rtsp \
             --enable-screen --enable-swmix --enable-v4l2 --enable-ximea
 set -- "$@" --enable-caca --enable-gl-display --enable-panogl_disp --enable-sdl                         # display
 set -- "$@" --enable-libavcodec --enable-rtdxt --enable-libswscale --enable-uyvy                        # compression
 set -- "$@" --enable-blank --enable-holepunch --enable-natpmp --enable-pcp --enable-resize --enable-scale --enable-sdp-http --enable-testcard-extras --enable-text --enable-video-mixer --enable-zfec # extras (pp. etc)
-if [ "$ARCH" = armhf ]; then # 64b doesn't have needed headers
+if [ "$ARCH" = armhf ]; then
+        # 64b doesn't have needed headers
         set -- "$@" --enable-lavc-hw-accel-rpi4
+else
+        set -- "$@" --enable-vulkan
 fi
 
 ./autogen.sh "$@"
