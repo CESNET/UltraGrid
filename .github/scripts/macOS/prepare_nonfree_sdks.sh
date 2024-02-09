d82c10a7b15412f838dbcfa49f6025ed730f08ac
Martin Pulec <martin.pulec@cesnet.cz>
2024-02-09 15:37:01 +0100

    CI mac: videomasterhd preparation improvement
    Remove absolute path from library location - we are in the directory
    anyways.
    
    This will be useful for local use (to copy and paste) when the library
    is already copied to `$(xcrun --show-sdk-path)..` etc.
    
    \+ early return

--- .github/scripts/macOS/prepare_nonfree_sdks.sh
+++ .github/scripts/macOS/prepare_nonfree_sdks.sh
@@ -4,17 +4,19 @@ cd /tmp
 
 # DELTACAST
 DELTA_CACHE_INST=$SDK_NONFREE_PATH/VideoMasterHD_inst
-if [ -f "$SDK_NONFREE_PATH/VideoMaster_SDK_MacOSX.zip" ]; then
-        unzip "$SDK_NONFREE_PATH/VideoMaster_SDK_MacOSX.zip"
-        sudo installer -pkg VideoMaster_SDK.pkg -target / || true
-        cd /Library/Frameworks
-        sudo install_name_tool -change /Library/Frameworks/VideoMasterHD.framework/Versions/A/VideoMasterHD @executable_path/../Frameworks/VideoMasterHD.framework/Versions/A/VideoMasterHD /Library/Frameworks/VideoMasterHD.framework/VideoMasterHD
-        sudo install_name_tool -id @executable_path/../Frameworks/VideoMasterHD.framework/Versions/A/VideoMasterHD /Library/Frameworks/VideoMasterHD.framework/VideoMasterHD
-        sudo install_name_tool -id @executable_path/../Frameworks/VideoMasterHD_Audio.framework/Versions/A/VideoMasterHD_Audio /Library/Frameworks/VideoMasterHD_Audio.framework/Versions/A/VideoMasterHD_Audio
-        sudo install_name_tool -change /Library/Frameworks/VideoMasterHD.framework/Versions/A/VideoMasterHD @executable_path/../Frameworks/VideoMasterHD.framework/Versions/A/VideoMasterHD /Library/Frameworks/VideoMasterHD_Audio.framework/Versions/A/VideoMasterHD_Audio
-        #sudo cp -a VideoMasterHD.framework VideoMasterHD_Audio.framework libVideoMasterHD_SP.dylib $(xcrun --show-sdk-path)/System/Library/Frameworks
-        mkdir "$DELTA_CACHE_INST"
-        sudo cp -a VideoMasterHD.framework VideoMasterHD_Audio.framework "$DELTA_CACHE_INST/"
-        cd -
-        sudo rm -rf /Library/Frameworks/VideoMasterHD* # ensure that only the copy above is used
+if [ ! -f "$SDK_NONFREE_PATH/VideoMaster_SDK_MacOSX.zip" ]; then
+        return
 fi
+
+unzip "$SDK_NONFREE_PATH/VideoMaster_SDK_MacOSX.zip"
+sudo installer -pkg VideoMaster_SDK.pkg -target / || true
+cd /Library/Frameworks
+sudo install_name_tool -change /Library/Frameworks/VideoMasterHD.framework/Versions/A/VideoMasterHD @executable_path/../Frameworks/VideoMasterHD.framework/Versions/A/VideoMasterHD VideoMasterHD.framework/VideoMasterHD
+sudo install_name_tool -id @executable_path/../Frameworks/VideoMasterHD.framework/Versions/A/VideoMasterHD VideoMasterHD.framework/VideoMasterHD
+sudo install_name_tool -id @executable_path/../Frameworks/VideoMasterHD_Audio.framework/Versions/A/VideoMasterHD_Audio VideoMasterHD_Audio.framework/Versions/A/VideoMasterHD_Audio
+sudo install_name_tool -change /Library/Frameworks/VideoMasterHD.framework/Versions/A/VideoMasterHD @executable_path/../Frameworks/VideoMasterHD.framework/Versions/A/VideoMasterHD VideoMasterHD_Audio.framework/Versions/A/VideoMasterHD_Audio
+#sudo cp -a VideoMasterHD.framework VideoMasterHD_Audio.framework libVideoMasterHD_SP.dylib $(xcrun --show-sdk-path)/System/Library/Frameworks
+mkdir "$DELTA_CACHE_INST"
+sudo cp -a VideoMasterHD.framework VideoMasterHD_Audio.framework "$DELTA_CACHE_INST/"
+cd -
+sudo rm -rf /Library/Frameworks/VideoMasterHD* # ensure that only the copy above is used
