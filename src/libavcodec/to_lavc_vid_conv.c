cba9699cb8163a6b7a2297f7dc9bed908fa4fb4b
Martin Pulec <martin.pulec@cesnet.cz>
2024-03-01 15:55:20 +0100

    to_lavc_vid_conv_init: set tmp_frame attributes
    Set tmp_frame width, height and format. Those are not copied by
    av_frame_copy_props as supposed by commit 5d3c31c115 (2023-10-31).
    As such, this partially revers that commit.

--- src/libavcodec/to_lavc_vid_conv.c
+++ src/libavcodec/to_lavc_vid_conv.c
@@ -1611,9 +1611,9 @@ struct to_lavc_vid_conv *to_lavc_vid_conv_init(codec_t in_pixfmt, int width, int
                 return NULL;
         }
         s->out_frame->pts = -1;
-        s->out_frame->format = out_pixfmt;
-        s->out_frame->width = width;
-        s->out_frame->height = height;
+        s->tmp_frame->format = s->out_frame->format = out_pixfmt;
+        s->tmp_frame->width = s->out_frame->width = width;
+        s->tmp_frame->height = s->out_frame->height = height;
         get_av_pixfmt_details(out_pixfmt, &s->out_frame->colorspace,
                               &s->out_frame->color_range);
         av_frame_copy_props(s->tmp_frame, s->out_frame);
