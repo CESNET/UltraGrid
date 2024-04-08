d5af762fe4d784146250ee4d1cac1352bb0af6f6
Martin Pulec <martin.pulec@cesnet.cz>
2024-04-08 15:59:25 +0200

    CI Windows: build CUDA stuff for CC 3.5
    Kepler cards are still (and few months will be) supported by NVIDIA.

--- .github/scripts/Windows/prepare_msys.sh
+++ .github/scripts/Windows/prepare_msys.sh
@@ -8,6 +8,7 @@ export CPATH=/usr/local/include:/usr/include
 export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:/usr/local/share/pkgconfig:/usr/lib/pkgconfig:$MSYSTEM_PREFIX/lib/pkgconfig
 export LIBRARY_PATH=/usr/local/lib
 export INCLUDE='C:\msys64\clang64\include' # for MSVC (CUDA)
+export CUDA_FLAGS='--generate-code arch=compute_35,code=sm_35'
 
 CUDA_D=$(ls -d /c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/*)
 if test -d "$CUDA_D"; then
