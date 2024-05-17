f53619778d5ecac4e1796199ac6d7127f3e8d4cb
Martin Piatka <piatka@cesnet.cz>
2024-05-17 13:14:01 +0200

    display/drm: Add copyright

--- src/video_display/drm.cpp
+++ src/video_display/drm.cpp
@@ -1,3 +1,45 @@
+/**
+ * @file   video_display/drm.cpp
+ * @author Martin Piatka <piatka@cesnet.cz>
+ */
+/*
+ * Copyright (c) 2024 CESNET z.s.p.o.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *
+ *      This product includes software developed by the University of Southern
+ *      California Information Sciences Institute. This product also includes
+ *      software developed by CESNET z.s.p.o.
+ *
+ * 4. Neither the name of the University, Institute, CESNET nor the names of
+ *    its contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING,
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include "config.h"
 #include "config_unix.h"
@@ -1042,7 +1084,7 @@ static bool display_drm_reconfigure(void *state, struct video_desc desc)
                 /* We don't create dumb buffers in this case, we import framebuffers from video frames instead
                  */
                 pix_fmt = 0; //UNUSED
-                if(s->drm.mode_info->hdisplay > desc.width || s->drm.mode_info->vdisplay > desc.height){
+                if(!s->drm.mode_info || s->drm.mode_info->hdisplay > desc.width || s->drm.mode_info->vdisplay > desc.height){
                         log_msg(LOG_LEVEL_WARNING, MOD_NAME "Video mode resolution must be less or equal to DRM_PRIME frames. Trying to find a suitable mode...\n");
                         s->drm.mode_info = nullptr;
                         for(int i = 0; i < s->drm.connector->count_modes; i++){
