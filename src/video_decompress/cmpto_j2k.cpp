bd44e8a61f94e1c4d7c2dca5a44e47d394537519
atrivialatomic <layzeekid11@gmail.com>
2024-09-06 18:25:44 -0400

    Implement 9304717 from master
    Implement [9304717](https://github.com/CESNET/UltraGrid/commit/9304717149e67cff1bcc7ba5c38f66bbdd9fdeac) from master

--- src/video_decompress/cmpto_j2k.cpp
+++ src/video_decompress/cmpto_j2k.cpp
@@ -3,7 +3,7 @@
  * @author Martin Pulec     <pulec@cesnet.cz>
  */
 /*
- * Copyright (c) 2013-2023 CESNET, z. s. p. o.
+ * Copyright (c) 2013-2024 CESNET
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -56,14 +56,17 @@
  * another stream, which, however, creates a new decoder).
  */
 
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#include "config_unix.h"
-#include "config_win32.h"
-#endif // HAVE_CONFIG_H
-
-#include <cmpto_j2k_dec.h>
-
+#include <algorithm>           // for min
+#include <cassert>             // for assert
+#include <cmpto_j2k_dec.h>     // for cmpto_sample_format_type, cmpto_j2k_de...
+#include <cstdint>             // for int64_t
+#include <cstdlib>             // for free, atoi, malloc, abort
+#include <cstring>             // for size_t, NULL, memcpy
+#include <mutex>               // for mutex, lock_guard, unique_lock
+#include <ostream>             // for operator<<, basic_ostream, char_traits
+#include <pthread.h>           // for pthread_create, pthread_join, pthread_t
+#include <queue>               // for queue
+#include <utility>             // for pair
 #include <algorithm>
 #include <mutex>
 #include <queue>
@@ -72,12 +75,21 @@
 #include "debug.h"
 #include "host.h"
 #include "lib_common.h"
+#include "pixfmt_conv.h"       // for get_decoder_from_to, decoder_t
+#include "types.h"             // for video_desc, pixfmt_desc, R12L, RGBA
 #include "utils/macros.h"
 #include "utils/misc.h"
-#include "video.h"
+#include "video_codec.h" // for vc_get_linesize, codec_is_a_rgb, get_b...
 #include "video_decompress.h"
 
 constexpr const char *MOD_NAME = "[Cmpto J2K dec.]";
+
+using std::lock_guard;
+using std::min;
+using std::mutex;
+using std::pair;
+using std::queue;
+using std::unique_lock;
 
 #define NOOP ((void) 0)
 
