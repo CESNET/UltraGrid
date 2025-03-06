e6cf35ca0b13d3bd1e49a14731a066e769fedd04
Martin Pulec <martin.pulec@cesnet.cz>
2025-03-06 15:26:08 +0100

    CI Linux: switch to ubuntu-22.04
    ubuntu-20.04 GH image is being deprecated and will be eventually removed
    from GH

--- .github/scripts/Linux/prepare.sh
+++ .github/scripts/Linux/prepare.sh
@@ -28,11 +28,6 @@ sudo apt install libcurl4-nss-dev
 sudo apt install i965-va-driver-shaders # instead of i965-va-driver
 sudo apt install uuid-dev # Cineform
 
-sudo apt install gcc-10
-sudo apt install g++-10
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 10
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-10 10
-
 get_build_deps_excl() { # $2 - pattern to exclude
         apt-cache showsrc "$1" | sed -n '/^Build-Depends:/{s/Build-Depends://;p;q}' | tr ',' '\n' | cut -f 2 -d\  | grep -v "$2"
 }
@@ -42,8 +37,6 @@ sdl2_ttf_build_dep=$(get_build_deps_excl libsdl2-ttf libsdl2-dev)
 # shellcheck disable=SC2086 # intentional
 sudo apt install $sdl2_mix_build_dep $sdl2_ttf_build_dep
 
-# FFmpeg deps
-sudo add-apt-repository ppa:savoury1/vlc3 # new x265
 # for FFmpeg - libzmq3-dev needs to be ignored (cannot be installed, see run #380)
 ffmpeg_build_dep=$(get_build_deps_excl ffmpeg 'libzmq3-dev\|libsdl2-dev')
 # shellcheck disable=SC2086 # intentional
