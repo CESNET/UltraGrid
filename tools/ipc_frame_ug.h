7b44acee22f319421de9b9c474ffdaf6064138a4
Martin Piatka <piatka@cesnet.cz>
2025-07-17 13:59:28 +0200

    tools/ipc_frame: Fix color_spec selection

--- tools/ipc_frame_ug.h
+++ tools/ipc_frame_ug.h
@@ -33,4 +33,6 @@ bool ipc_frame_write_to_fd(const struct Ipc_frame *f, int fd);
 
 int ipc_frame_get_scale_factor(int src_w, int src_h, int target_w, int target_h);
 
+Ipc_frame_color_spec ipc_frame_color_spec_from_ug(codec_t codec);
+
 #endif //IPC_FRAME_UG_32ee5c748f3e
