9c4df61c8d4a82ba314d7e8d8965a658817eaa9a
Martin Piatka <piatka@cesnet.cz>
2025-08-13 13:21:36 +0200

    utils: Add SDP parser

---
+++ src/utils/sdp_parser.cpp
@@ -0,0 +1,174 @@
+/**
+ * @file utils/sdp_parser.cpp
+ * @author Martin Piatka     <piatka@cesnet.cz>
+ */
+/*
+ * Copyright (c) 2025 CESNET, zájmové sdružení právnických osob
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of CESNET nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING,
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "sdp_parser.hpp"
+#include "utils/string_view_utils.hpp"
+
+Sap_packet_view Sap_packet_view::from_buffer(const void *buf, size_t size){
+        Sap_packet_view ret;
+
+        std::string_view sap(static_cast<const char *>(buf), size);
+
+        if(sap.empty()){
+                return ret;
+        }
+
+        ret.flags = sap[0];
+        sap.remove_prefix(1);
+
+        ret.version = ret.flags >> 6;
+
+        uint8_t auth_len = 0;
+        if(sap.empty())
+                return ret;
+
+        auth_len = sap[0];
+        sap.remove_prefix(1);
+        
+        //TODO error check length
+        ret.hash = sap[0] << 8 | sap[1];
+        sap.remove_prefix(2);
+
+        //TODO error check length
+        ret.source = sap.substr(0, 4);
+        sap.remove_prefix(4);
+
+        //TODO error check length
+        sap.remove_prefix(auth_len);
+
+        if(sv_is_prefix(sap, "v=0")){
+                //RFC says that the payload type is optional if it's sdp and
+                //that it should be detected by the presence of v=0
+                ret.payload_type = "application/sdp";
+                ret.payload = sap;
+        } else {
+                auto null_idx = sap.find('\0');
+                if(null_idx == sap.npos){
+                        return ret;
+                }
+                ret.payload_type = sap.substr(0, null_idx);
+                ret.payload = sap.substr(null_idx + 1);
+        }
+
+        ret.valid = true;
+        return ret;
+}
+
+Sdp_view Sdp_view::from_buffer(const void *buf, size_t size){
+        Sdp_view ret;
+
+        std::string_view sdp(static_cast<const char *>(buf), size);
+
+        if(sdp.empty()){
+                return ret;
+        }
+
+        while(!sdp.empty()){
+                auto line = tokenize(sdp, '\n');
+                if(line.empty())
+                        continue;
+
+                if(line.back() == '\r')
+                        line.remove_suffix(1);
+
+                auto eq_idx = line.find('=');
+                if(eq_idx == line.npos){
+                        continue;
+                }
+                auto key = line.substr(0, eq_idx);
+                auto val = line.substr(eq_idx + 1);
+
+                if(key == "c"){
+                        if(ret.media.empty()){
+                                ret.connection = val;
+                        } else {
+                                ret.media.back().connection = val;
+                        }
+                } else if(key == "a"){
+                        auto attrib = tokenize(val, ':');
+                        auto attrib_val = tokenize(val, ':');
+
+                        if(ret.media.empty()){
+                                ret.session_attributes.push_back({attrib, attrib_val});
+                        } else {
+                                ret.media.back().attributes.push_back({attrib, attrib_val});
+                        }
+
+                } else if(key =="s"){
+                        ret.session_name = val;
+                } else if(key == "o"){
+                        ret.origin = val;
+                } else if(key == "m"){
+                        ret.media.emplace_back();
+                        auto& medium = ret.media.back();
+
+                        medium.media_desc = val;
+                }
+
+        }
+
+        ret.valid = true;
+
+        return ret;
+}
+
+Rtp_pkt_view Rtp_pkt_view::from_buffer(void *buf, size_t size){
+        Rtp_pkt_view ret{};
+
+        if(size < 12)
+                return ret;
+
+        auto charbuf = static_cast<unsigned char*>(buf);
+
+        uint8_t version = charbuf[0] >> 6;
+        bool padding = charbuf[0] & (1 << 5);
+        bool extension = charbuf[0] & (1 << 4);
+        ret.csrc_count = charbuf[0] & 0x0F;
+        ret.marker = charbuf[1] & 0x80;
+        ret.payload_type = charbuf[1] & 0x7F;
+        ret.seq = charbuf[2] << 8 | charbuf[3];
+        ret.timestamp = charbuf[4] << 24 | charbuf[5] << 16 | charbuf[6] << 8 | charbuf[7];
+        ret.ssrc = charbuf[8] << 24 | charbuf[9] << 16 | charbuf[10] << 8 | charbuf[11];
+
+
+        size_t data_offset = 12 + ret.csrc_count * 4;
+        ret.data = &charbuf[data_offset];
+        ret.data_len = size - data_offset;
+
+        ret.valid = true;
+        return ret;
+}
---
+++ src/utils/sdp_parser.hpp
@@ -0,0 +1,119 @@
+/**
+ * @file utils/sdp_parser.hpp
+ * @author Martin Piatka     <piatka@cesnet.cz>
+ */
+/*
+ * Copyright (c) 2025 CESNET, zájmové sdružení právnických osob
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of CESNET nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING,
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef SDP_PARSER_HPP_cccb52235120
+#define SDP_PARSER_HPP_cccb52235120
+
+#include <string_view>
+#include <vector>
+#include <cstdint>
+
+#define SAP_FLAG_COMPRESSED (1 << 0)
+#define SAP_FLAG_ENCRYPTED (1 << 1)
+#define SAP_FLAG_DELETION (1 << 2)
+#define SAP_FLAG_IPV6 (1 << 4)
+
+
+struct Sap_packet_view{
+        uint8_t version;
+        uint8_t flags;
+        uint16_t hash;
+
+        std::string_view source;
+        std::string_view payload_type;
+        std::string_view payload;
+
+        static Sap_packet_view from_buffer(const void *buf, size_t size);
+
+        bool isValid() const { return valid; }
+        bool isCompressed() const { return flags & SAP_FLAG_COMPRESSED; }
+        bool isEncrypted() const { return flags & SAP_FLAG_ENCRYPTED; }
+        bool isDeletion() const { return flags & SAP_FLAG_DELETION; }
+        bool isIpv6() const { return flags & SAP_FLAG_IPV6; }
+
+        bool valid = false;
+
+};
+
+struct Sdp_attribute{
+        std::string_view key;
+        std::string_view val;
+};
+
+struct Sdp_media_desc{
+        std::string_view media_desc;
+        std::string_view title;
+        std::string_view connection;
+        std::vector<Sdp_attribute> attributes;
+};
+
+struct Sdp_view{
+        std::string_view origin;
+        std::string_view session_name;
+        std::string_view session_info;
+        std::string_view connection;
+
+        std::string_view session_time;
+        std::vector<Sdp_attribute> session_attributes;
+
+        std::vector<Sdp_media_desc> media;
+
+        static Sdp_view from_buffer(const void *buf, size_t size);
+        bool isValid() const { return valid; }
+        
+        bool valid = false;
+};
+
+struct Rtp_pkt_view{
+        bool marker;
+        uint8_t payload_type;
+        uint16_t seq;
+        uint32_t timestamp;
+        uint32_t ssrc;
+        //uint32_t *csrcs;
+        size_t csrc_count;
+        void *data;
+        size_t data_len;
+
+        static Rtp_pkt_view from_buffer(void *buf, size_t size);
+        bool isValid() const { return valid; }
+        
+        bool valid = false;
+};
+
+
+#endif
