9e4607605759cb34e4a5df1615421441c34c0e73
Jan Frejlach <jan.frejlach@gmail.com>
2025-08-26 12:57:32 +0200

    jpegxs compress: init

--- src/video_compress/jpegxs.cpp
+++ src/video_compress/jpegxs.cpp
@@ -13,44 +13,69 @@ struct state_video_compress_jpegxs {
 private:
         state_video_compress_jpegxs(struct module *parent, const char *opts);
 public:
+        svt_jpeg_xs_encoder_api_t m_encoder;
 
+        static state_video_compress_jpegxs *create(struct module *parent, const char *opts);
         void push(std::shared_ptr<video_frame> in_frame);
         std::shared_ptr<video_frame> pop();
 };
 
+state_video_compress_jpegxs::state_video_compress_jpegxs(struct module *parent, const char *opts) {
+        (void) parent;
+
+        m_encoder.source_width = 1920;
+        m_encoder.source_height = 1080;
+        m_encoder.input_bit_depth = 8;
+        m_encoder.colour_format = COLOUR_FORMAT_PLANAR_YUV422;
+        m_encoder.bpp_numerator = 3;
+
+        svt_jpeg_xs_encoder_init(SVT_JPEGXS_API_VER_MAJOR, SVT_JPEGXS_API_VER_MINOR, &m_encoder);
+}
+
+state_video_compress_jpegxs *state_video_compress_jpegxs::create(struct module *parent, const char *opts) {
+        auto ret = new state_video_compress_jpegxs(parent, opts);
+        
+        return ret;
+}
+
 void *
-jpegxs_compress_init(struct module *parent, const char *opts)
-{
+jpegxs_compress_init(struct module *parent, const char *opts) {
         struct state_video_compress_jpegxs *s;
+        
+        if(opts && strcmp(opts, "help") == 0) {
+                printf("JPEG XS help message\n");
+                return INIT_NOERR;
+        }
+
+        s = state_video_compress_jpegxs::create(parent, opts);
+
+        return s;
+}
+
+shared_ptr<video_frame> jpegxs_compress(void *state, shared_ptr<video_frame> frame) {
         // TODO
 }
 
-shared_ptr<video_frame> jpegxs_compress(void *state, shared_ptr<video_frame> tx)
-{
-        // TODO
-}
+// void state_video_compress_jpegxs::push(std::shared_ptr<video_frame> in_frame)
+// {
+//         // TODO
+// }
 
-void state_video_compress_jpegxs::push(std::shared_ptr<video_frame> in_frame)
-{
-        // TODO
-}
+// std::shared_ptr<video_frame> state_video_compress_jpegxs::pop()
+// {
+//         // TODO
+// }
 
-std::shared_ptr<video_frame> state_video_compress_jpegxs::pop()
-{
-        // TODO
-}
+// static auto jpegxs_compress_push(void *state, std::shared_ptr<video_frame> in_frame) {
+//         static_cast<struct state_video_compress_jpegxs *>(state)->push(std::move(in_frame));
+// }
 
-static auto jpegxs_compress_push(void *state, std::shared_ptr<video_frame> in_frame) {
-        static_cast<struct state_video_compress_jpegxs *>(state)->push(std::move(in_frame));
-}
-
-static auto jpegxs_compress_pull(void *state) {
-        return static_cast<struct state_video_compress_jpegxs *>(state)->pop();
-}
+// static auto jpegxs_compress_pull(void *state) {
+//         return static_cast<struct state_video_compress_jpegxs *>(state)->pop();
+// }
 
 static void
-jpegxs_compress_done(void *state)
-{
+jpegxs_compress_done(void *state) {
         delete (struct state_video_compress_jpegxs *) state;
 }
 
