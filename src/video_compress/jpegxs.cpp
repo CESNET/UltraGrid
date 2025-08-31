219a03f52858144ac514d9d9fb5809e0f8cf3345
Jan Frejlach <jan.frejlach@gmail.com>
2025-08-31 21:10:04 +0200

    jpegxs compress: setup input buffer

--- src/video_compress/jpegxs.cpp
+++ src/video_compress/jpegxs.cpp
@@ -5,6 +5,8 @@
 #include "video_compress.h"
 #include "utils/video_frame_pool.h"
 #include <svt-jpegxs/SvtJpegxsEnc.h>
+#include <iostream>
+#include <string>
 
 using std::shared_ptr;
 
@@ -36,11 +38,67 @@ state_video_compress_jpegxs *state_video_compress_jpegxs::create(struct module *
         return ret;
 }
 
+ColourFormat subsampling_to_jpegxs(int ug_subs) {
+        switch (ug_subs) {
+        case 444:
+                return COLOUR_FORMAT_PLANAR_YUV444_OR_RGB;
+        case 422:
+                return COLOUR_FORMAT_PLANAR_YUV422;
+        case 420:
+                return COLOUR_FORMAT_PLANAR_YUV420;
+        case 400:
+                return COLOUR_FORMAT_PLANAR_YUV400;
+        default:
+                abort();
+        }
+}
+
+static bool setup_image_input_buffer(svt_jpeg_xs_image_buffer_t *in_buf, const svt_jpeg_xs_encoder_api_t *enc) {
+
+        uint32_t pixel_size = enc->input_bit_depth <= 8 ? 1 : 2;
+        uint32_t w = enc->source_width;
+        uint32_t h = enc->source_height;
+
+        uint32_t w_factor = 1;
+        uint32_t h_factor = 1;
+
+        switch (enc->colour_format) {
+        case COLOUR_FORMAT_PLANAR_YUV444_OR_RGB: // no subsampling
+                break;
+        case COLOUR_FORMAT_PLANAR_YUV422: // half horizontal
+                w_factor = 2;
+                break;
+        case COLOUR_FORMAT_PLANAR_YUV420: // half horizontal + vertical
+                w_factor = 2;
+                h_factor = 2;
+                break;
+        default:
+                fprintf(stderr, "Unsupported colour format!\n");
+                return false;
+        }
+
+        in_buf->stride[0] = w;
+        in_buf->stride[1] = w / w_factor;
+        in_buf->stride[2] = w / w_factor;
+
+        for (uint8_t i = 0; i < 3; ++i) {
+                in_buf->alloc_size[i] = in_buf->stride[i] * (h / h_factor) * pixel_size;
+                in_buf->data_yuv[i] = malloc(in_buf->alloc_size[i]);
+                if (!in_buf->data_yuv[i]) {
+                        fprintf(stderr, "Failed to allocate plane %d\n", i);
+                        return false;
+                }
+        }
+
+        return true;
+}
+
 static bool configure_with(struct state_video_compress_jpegxs *s, struct video_desc desc) {
         svt_jpeg_xs_encoder_api_t enc = {};
 
         SvtJxsErrorType_t err = svt_jpeg_xs_encoder_load_default_parameters(SVT_JPEGXS_API_VER_MAJOR, SVT_JPEGXS_API_VER_MINOR, &enc);
         if (err != SvtJxsErrorNone) {
+                fprintf(stderr, "Failed to load JPEG XS default parameters\n");
                 return false;
         }
 
@@ -53,20 +111,14 @@ static bool configure_with(struct state_video_compress_jpegxs *s, struct video_d
 
         err = svt_jpeg_xs_encoder_init(SVT_JPEGXS_API_VER_MAJOR, SVT_JPEGXS_API_VER_MINOR, &enc);
         if (err != SvtJxsErrorNone) {
+                fprintf(stderr, "Failed to initialize JPEG XS encoder\n");
                 return false;
         }
 
-        uint32_t pixel_size = enc.input_bit_depth <= 8 ? 1 : 2;
         svt_jpeg_xs_image_buffer_t in_buf;
-        in_buf.stride[0] = enc.source_width;
-        in_buf.stride[1] = enc.source_width / 2;
-        in_buf.stride[2] = enc.source_width / 2;
-        for (uint8_t i = 0; i < 3; ++i) {
-                in_buf.alloc_size[i] = in_buf.stride[i] * enc.source_height * pixel_size;
-                in_buf.data_yuv[i] = malloc(in_buf.alloc_size[i]);
-                if (!in_buf.data_yuv[i]) {
-                        return false;
-                }
+        if (!setup_image_input_buffer(&in_buf, &enc)) {
+                fprintf(stderr, "Failed to initialize input image buffer\n");
+                return false;
         }
 
         svt_jpeg_xs_bitstream_buffer_t out_buf;
@@ -76,6 +128,7 @@ static bool configure_with(struct state_video_compress_jpegxs *s, struct video_d
         out_buf.used_size = 0;
         out_buf.buffer = (uint8_t *) malloc(out_buf.allocation_size);
         if (!out_buf.buffer) {
+                fprintf(stderr, "Failed to initialize output image buffer\n");
                 return false;
         }
 
@@ -112,6 +165,33 @@ shared_ptr<video_frame> jpegxs_compress(void *state, shared_ptr<video_frame> fra
                         return NULL;
                 }
         }
+
+        // TODO: UYVY (packed) to planar (Y, U, V)
+
+        unsigned char *src = (unsigned char *) frame->tiles[0].data;
+        int src_stride =frame->tiles[0].width * 2;;
+
+        svt_jpeg_xs_frame_t enc_input;
+        enc_input.bitstream = s->out_buf;
+        enc_input.image = s->in_buf;
+        enc_input.user_prv_ctx_ptr = NULL;
+
+        SvtJxsErrorType_t err;
+        err = svt_jpeg_xs_encoder_send_picture(&s->encoder, &enc_input, 1 /*blocking*/);
+        if (err != SvtJxsErrorNone) {
+                fprintf(stderr, "Failed to send frame to encoder!\n");
+                return NULL;
+        }
+
+        svt_jpeg_xs_frame_t enc_output;
+        err = svt_jpeg_xs_encoder_get_packet(&s->encoder, &enc_output, 1 /*blocking*/);
+        if (err != SvtJxsErrorNone) {
+                fprintf(stderr, "Failed to get encoded packet!\n");
+                return NULL;
+        }
+
+        shared_ptr<video_frame> out_frame = s->pool.get_frame();
+        // TODO: copy enc_output to out_frame
         
         return frame;
 }
