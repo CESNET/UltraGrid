3443343b7cbee6a039cb84bed13b24d64e5a7e2c
Jan Frejlach <jan.frejlach@gmail.com>
2025-09-18 14:10:36 +0200

    jpegxs decompress: initial structure

--- src/video_decompress/jpegxs.cpp
+++ src/video_decompress/jpegxs.cpp
@@ -1,17 +1,28 @@
+#include <svt-jpegxs/SvtJpegxsDec.h>
+
+#include "debug.h"
 #include "lib_common.h"
 #include "video.h"
 #include "video_decompress.h"
-#include <svt-jpegxs/SvtJpegxsDec.h>
 
-struct state_decompress_gpujpeg {
+struct state_decompress_jpegxs {
         struct svt_jpeg_xs_decoder_api *decoder;
+        ~state_decompress_jpegxs() {
+                svt_jpeg_xs_decoder_close(decoder);
+        }
 
         struct video_desc desc;
         codec_t out_codec;
 };
 
+static void *jpegxs_decompress_init(void) {
+        struct state_decompress_jpegxs *s = new state_decompress_jpegxs();
+
+        return s;
+}
+
 static const struct video_decompress_info jpegxs_info = {
-        NULL, // jpegxs_decompress_init
+        jpegxs_decompress_init, // jpegxs_decompress_init
         NULL, // jpegxs_decompress_reconfigure
         NULL, // jpegxs_decompress
         NULL, // jpegxs_decompress_get_property
