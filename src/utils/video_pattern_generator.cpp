c3fd68a449e2067325770fdbb2cc541e738eec59
Martin Pulec <martin.pulec@cesnet.cz>
2025-10-08 13:44:21 +0200

    testcard: accept sym. col names for other patterns

--- src/utils/video_pattern_generator.cpp
+++ src/utils/video_pattern_generator.cpp
@@ -405,7 +405,11 @@ class image_pattern_gradient : public image_pattern {
         public:
                 explicit image_pattern_gradient(const string &config) {
                         if (!config.empty()) {
-                                color = stol(config, nullptr, 0);
+                                long long c = get_color_by_name(config.c_str());
+                                if (c == -1) {
+                                        throw 1;
+                                }
+                                color = c;
                         }
                 }
                 static constexpr uint32_t red = 0xFFU;
@@ -595,7 +599,11 @@ class image_pattern_text : public image_pattern {
         public:
                 explicit image_pattern_text(const string & config) {
                         if (config == "help"s) {
-                                col() << "Testcard text usage:\n\t" << SBOLD(SRED("-t testcard:pattern=text") << "[=pattern][,bg=0x<AABBGGRR<][,fg=0x<AABBGGRR>]") << "\n";
+                                col() << "Testcard text usage:\n\t"
+                                      << SBOLD(SRED("-t testcard:pattern=text")
+                                               << "[=pattern][,bg=0x<col>|help]"
+                                                  "[,fg=<col>|help]")
+                                      << "\n";
                                 throw 1;
                         }
                         if (!config.empty()) {
@@ -603,10 +611,19 @@ class image_pattern_text : public image_pattern {
                                 bool text_set = false;
                                 while (!sv.empty()) {
                                         auto tok = tokenize(sv, ',');
-                                        if (tok.substr(0,3) == "bg=") {
-                                                bg = stol((string) tok.substr(3), nullptr, 0);
-                                        } else if (tok.substr(0,3) == "fg=") {
-                                                fg = stol((string) tok.substr(3), nullptr, 0);
+                                        if (tok.substr(0,3) == "bg=" || tok.substr(0,3) == "fg=") {
+                                                auto key = tokenize(tok, '=');
+                                                auto val = tokenize(tok, '=');
+                                                long long c = get_color_by_name(
+                                                    ((string) val).c_str());
+                                                if (c == -1) {
+                                                        throw 1;
+                                                }
+                                                if (key == "bg") {
+                                                        bg = c;
+                                                } else {
+                                                        fg = c;
+                                                }
                                         } else if (!text_set) {
                                                 text = tok;
                                                 text_set = true;
@@ -638,7 +655,14 @@ class image_pattern_diagonal : public image_pattern{
         public:
                 explicit image_pattern_diagonal(const string & config) {
                         if (config == "help"s) {
-                                col() << "Testcard diagonal usage:\n\t" << SBOLD(SRED("-t testcard:pattern=diagonal") << "[,bg=0x<AABBGGRR<][,fg=0x<AABBGGRR>][,stride=<stride>][,line_width=<width>]") << "\n";
+                                col()
+                                    << "Testcard diagonal usage:\n\t"
+                                    << SBOLD(
+                                           SRED("-t testcard:pattern=diagonal")
+                                           << "[,bg=<color>|help][,fg=<color>|"
+                                              "help][,stride=<stride>][,line_"
+                                              "width=<width>]")
+                                    << "\n";
                                 throw 1;
                         }
                         if (!config.empty()) {
@@ -647,10 +671,17 @@ class image_pattern_diagonal : public image_pattern{
                                         auto tok = tokenize(sv, ',');
                                         auto key = tokenize(tok, '=');
                                         auto val = tokenize(tok, '=');
-                                        if (key == "bg") {
-                                                bg = stol((string) val, nullptr, 0);
-                                        } else if (key == "fg") {
-                                                fg = stol((string) val, nullptr, 0);
+                                        if (key == "bg" || key == "fg") {
+                                                long long c = get_color_by_name(
+                                                    ((string) val).c_str());
+                                                if (c == -1) {
+                                                        throw 1;
+                                                }
+                                                if (key == "bg") {
+                                                        bg = c;
+                                                } else {
+                                                        fg = c;
+                                                }
                                         } else if (key == "stride") {
                                                 stride = stol((string) val, nullptr, 0);
                                         } else if (key == "line_width") {
@@ -906,7 +937,7 @@ video_pattern_generator_create(const char *config, int width, int height, codec_
                         BLANK_USAGE,
                         "diagonal*",
                         "ebu_bars",
-                        "gradient[=0x<AABBGGRR>]",
+                        "gradient[=<color>|help]",
                         "gradient2*",
                         "gray",
                         "interlaced",
