90280720d221f02492e49291f0382ab6f9453e1b
Jan Frejlach <jan.frejlach@gmail.com>
2025-10-13 22:44:37 +0200

    jpegxs: rgb support

--- src/jpegxs/jpegxs_conv.h
+++ src/jpegxs/jpegxs_conv.h
@@ -10,15 +10,15 @@ extern "C" {
 #endif
 
 struct uv_to_jpegxs_conversion {
-    codec_t src;
-    ColourFormat_t dst;
-    void (*convert)(const uint8_t *src, int width, int height, svt_jpeg_xs_image_buffer_t *dst);
+        codec_t src;
+        ColourFormat_t dst;
+        void (*convert)(const uint8_t *src, int width, int height, svt_jpeg_xs_image_buffer_t *dst);
 };
 
 struct jpegxs_to_uv_conversion {
-    ColourFormat_t src;
-    codec_t dst;
-    void (*convert)(const svt_jpeg_xs_image_buffer_t *src, int width, int height, uint8_t *dst);
+        ColourFormat_t src;
+        codec_t dst;
+        void (*convert)(const svt_jpeg_xs_image_buffer_t *src, int width, int height, uint8_t *dst);
 };
 
 const struct uv_to_jpegxs_conversion *get_uv_to_jpegxs_conversion(codec_t codec);
--- src/jpegxs/to_jpegxs_conv.c
+++ src/jpegxs/to_jpegxs_conv.c
@@ -3,105 +3,102 @@
 
 #include "jpegxs_conv.h"
 #include "types.h"
+#include "color_space.h"
 
-static void uyvy_to_yuv422p(const uint8_t *uyvy, int width, int height, svt_jpeg_xs_image_buffer_t *in_buf) {
-        uint8_t *dst_y = (uint8_t *) in_buf->data_yuv[0];
-        uint8_t *dst_u = (uint8_t *) in_buf->data_yuv[1];
-        uint8_t *dst_v = (uint8_t *) in_buf->data_yuv[2];
+static void uyvy_to_yuv422p(const uint8_t *src, int width, int height, svt_jpeg_xs_image_buffer_t *dst) {
 
         for (int y = 0; y < height; ++y) {
-                const uint8_t *src_line = uyvy + y * width * 2;
-                uint8_t *dst_y_line = dst_y + y * width;
-                uint8_t *dst_u_line = dst_u + y * (width / 2);
-                uint8_t *dst_v_line = dst_v + y * (width / 2);
+                uint8_t *dst_y = (uint8_t *) dst->data_yuv[0] + y * dst->stride[0];
+                uint8_t *dst_u = (uint8_t *) dst->data_yuv[1] + y * dst->stride[1];
+                uint8_t *dst_v = (uint8_t *) dst->data_yuv[2] + y * dst->stride[2];
 
                 for (int x = 0; x < width; x += 2) {
-                        int i = x * 2;
-                        uint8_t u = src_line[i + 0];
-                        uint8_t y0 = src_line[i + 1];
-                        uint8_t v = src_line[i + 2];
-                        uint8_t y1 = src_line[i + 3];
-
-                        dst_y_line[x + 0] = y0;
-                        dst_y_line[x + 1] = y1;
-
-                        int chroma_index = x / 2;
-                        dst_u_line[chroma_index] = u;
-                        dst_v_line[chroma_index] = v;
+                        *dst_u++ = *src++;
+                        *dst_y++ = *src++;
+                        *dst_v++ = *src++;
+                        *dst_y++ = *src++;
                 }
         }
 }
 
-static void yuyv_to_yuv422p(const uint8_t *yuyv, int width, int height, svt_jpeg_xs_image_buffer_t *in_buf) {
-        uint8_t *dst_y = (uint8_t *) in_buf->data_yuv[0];
-        uint8_t *dst_u = (uint8_t *) in_buf->data_yuv[1];
-        uint8_t *dst_v = (uint8_t *) in_buf->data_yuv[2];
+static void yuyv_to_yuv422p(const uint8_t *src, int width, int height, svt_jpeg_xs_image_buffer_t *dst) {
 
         for (int y = 0; y < height; ++y) {
-                const uint8_t *src_line = yuyv + y * width * 2;
-                uint8_t *dst_y_line = dst_y + y * width;
-                uint8_t *dst_u_line = dst_u + y * (width / 2);
-                uint8_t *dst_v_line = dst_v + y * (width / 2);
+                uint8_t *dst_y = (uint8_t *) dst->data_yuv[0] + y * dst->stride[0];
+                uint8_t *dst_u = (uint8_t *) dst->data_yuv[1] + y * dst->stride[1];
+                uint8_t *dst_v = (uint8_t *) dst->data_yuv[2] + y * dst->stride[2];
 
                 for (int x = 0; x < width; x += 2) {
-                        int i = x * 2;
-                        uint8_t y0 = src_line[i + 0];
-                        uint8_t u = src_line[i + 1];
-                        uint8_t y1 = src_line[i + 2];
-                        uint8_t v = src_line[i + 3];
-
-                        dst_y_line[x + 0] = y0;
-                        dst_y_line[x + 1] = y1;
-
-                        int chroma_index = x / 2;
-                        dst_u_line[chroma_index] = u;
-                        dst_v_line[chroma_index] = v;
+                        *dst_y++ = *src++;
+                        *dst_u++ = *src++;
+                        *dst_y++ = *src++;
+                        *dst_v++ = *src++;
                 }
         }
 }
 
-static void i420_to_yuv420p(const uint8_t *i420, int width, int height, svt_jpeg_xs_image_buffer_t *in_buf) {
+static void i420_to_yuv420p(const uint8_t *src, int width, int height, svt_jpeg_xs_image_buffer_t *dst) {
+
         const int y_size = width * height;
-        const int uv_width = width / 2;
-        const int uv_height = height / 2;
-        const int u_size = uv_width * uv_height;
+        const int uv_size = (width / 2) * (height / 2);
 
-        const uint8_t *src_y = i420;
-        const uint8_t *src_u = i420 + y_size;
-        const uint8_t *src_v = i420 + y_size + u_size;
+        const uint8_t *src_y = src;
+        const uint8_t *src_u = src_y + y_size;
+        const uint8_t *src_v = src_u + uv_size;
 
-        uint8_t *dst_y = (uint8_t *) in_buf->data_yuv[0];
-        uint8_t *dst_u = (uint8_t *) in_buf->data_yuv[1];
-        uint8_t *dst_v = (uint8_t *) in_buf->data_yuv[2];
+        memcpy(dst->data_yuv[0], src_y, y_size);
+        memcpy(dst->data_yuv[1], src_u, uv_size);
+        memcpy(dst->data_yuv[2], src_v, uv_size);
+}
 
+static void rgb_to_yuv444p(const uint8_t *src, int width, int height, svt_jpeg_xs_image_buffer_t *dst) {
+
+        int bit_depth = 8;
+        
+        const struct color_coeffs cfs = *get_color_coeffs(CS_DFL, bit_depth);
         for (int y = 0; y < height; ++y) {
-                memcpy(dst_y + y * width, src_y + y * width, width);
-        }
+                uint8_t *dst_y = (uint8_t *) dst->data_yuv[0] + y * dst->stride[0];
+                uint8_t *dst_u = (uint8_t *) dst->data_yuv[1] + y * dst->stride[1];
+                uint8_t *dst_v = (uint8_t *) dst->data_yuv[2] + y * dst->stride[2];
 
-        for (int y = 0; y < uv_height; ++y) {
-                memcpy(dst_u + y * uv_width, src_u + y * uv_width, uv_width);
-        }
+                for (int x = 0; x < width; ++x) {
+                        const comp_type_t r = *src++;
+                        const comp_type_t g = *src++;
+                        const comp_type_t b = *src++;
 
-        for (int y = 0; y < uv_height; ++y) {
-                memcpy(dst_v + y * uv_width, src_v + y * uv_width, uv_width);
+                        const comp_type_t res_y =
+                            (RGB_TO_Y(cfs, r, g, b) >> COMP_BASE) +
+                            (1 << (bit_depth - 4));
+                        const comp_type_t res_cb =
+                            (RGB_TO_CB(cfs, r, g, b) >> COMP_BASE) +
+                            (1 << (bit_depth - 1));
+                        const comp_type_t res_cr =
+                            (RGB_TO_CR(cfs, r, g, b) >> COMP_BASE) +
+                            (1 << (bit_depth - 1));
+
+                        *dst_y++ = CLAMP_LIMITED_Y(res_y, bit_depth);
+                        *dst_u++ = CLAMP_LIMITED_Y(res_cb, bit_depth);
+                        *dst_v++ = CLAMP_LIMITED_Y(res_cr, bit_depth);
+                }
         }
 }
 
 static const struct uv_to_jpegxs_conversion uv_to_jpegxs_conversions[] = {
-    { UYVY, COLOUR_FORMAT_PLANAR_YUV422, uyvy_to_yuv422p },
-    { YUYV, COLOUR_FORMAT_PLANAR_YUV422, yuyv_to_yuv422p },
-    { I420, COLOUR_FORMAT_PLANAR_YUV420, i420_to_yuv420p },
-    { VIDEO_CODEC_NONE, COLOUR_FORMAT_INVALID, NULL }
+        { UYVY, COLOUR_FORMAT_PLANAR_YUV422, uyvy_to_yuv422p },
+        { YUYV, COLOUR_FORMAT_PLANAR_YUV422, yuyv_to_yuv422p },
+        { I420, COLOUR_FORMAT_PLANAR_YUV420, i420_to_yuv420p },
+        { RGB, COLOUR_FORMAT_PLANAR_YUV444_OR_RGB, rgb_to_yuv444p },
+        { VIDEO_CODEC_NONE, COLOUR_FORMAT_INVALID, NULL }
 };
 
 const struct uv_to_jpegxs_conversion *get_uv_to_jpegxs_conversion(codec_t codec) {
-    
-    const struct uv_to_jpegxs_conversion *conv = uv_to_jpegxs_conversions;
-    while (conv->src != VIDEO_CODEC_NONE) {
-        if (conv->src == codec)
-            return conv;
-        conv++;
-    }
 
-    return NULL;
+        const struct uv_to_jpegxs_conversion *conv = uv_to_jpegxs_conversions;
+        while (conv->src != VIDEO_CODEC_NONE) {
+                if (conv->src == codec)
+                        return conv;
+                conv++;
+        }
+
+        return NULL;
 }
