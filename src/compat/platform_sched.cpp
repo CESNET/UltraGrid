b27a99e28157e9aff525f19d12abefde589aadd1
Martin Piatka <piatka@cesnet.cz>
2025-10-29 13:53:19 +0100

    compat/platform_sched: Add Windows impl

--- src/compat/platform_sched.cpp
+++ src/compat/platform_sched.cpp
@@ -1,16 +1,17 @@
 #include "platform_sched.h"
-#include <sched.h>
 #include "debug.h"
 
 #define MOD_NAME "[sched] "
 
+#ifdef __unix__
+
+#include <sched.h>
+
 bool set_realtime_sched_this_thread(){
-        struct sched_param p;
-        memset(&p, 0, sizeof(p));
+        sched_param p = {};
 
         int policy = SCHED_RR;
-
-        p.sched_priority = sched_get_priority_max(policy); 
+        p.sched_priority = sched_get_priority_max(policy);
         int ret = sched_setscheduler(0, policy | SCHED_RESET_ON_FORK, &p);
         if(ret >= 0){
                 log_msg(LOG_LEVEL_NOTICE, MOD_NAME "Set realtime scheduling with priority %d\n", p.sched_priority);
@@ -20,3 +21,25 @@ bool set_realtime_sched_this_thread(){
 
         return ret >= 0;
 }
+
+#elifdef _WIN32
+
+#include <windows.h>
+
+bool set_realtime_sched_this_thread(){
+        if(!SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_TIME_CRITICAL)){
+                log_msg(LOG_LEVEL_WARNING, MOD_NAME "SetThreadPriority failed\n");
+                return false;
+        }
+
+        log_msg(LOG_LEVEL_NOTICE, MOD_NAME "Set thread priority to TIME_CRITICAL\n");
+        return true;
+}
+#else
+
+bool set_realtime_sched_this_thread(){
+        log_msg(LOG_LEVEL_ERROR, MOD_NAME "Unimplemented\n");
+        return false;
+}
+
+#endif
