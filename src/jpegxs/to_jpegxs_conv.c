7349b7df1cd97ad0c04355ac09164aa38d007c2c
Jan Frejlach <jan.frejlach@gmail.com>
2025-11-10 11:20:40 +0100

    jpegxs compress: image config

--- src/jpegxs/to_jpegxs_conv.c
+++ src/jpegxs/to_jpegxs_conv.c
@@ -1,8 +1,10 @@
 #include <svt-jpegxs/SvtJpegxs.h>
 #include <string.h>
+#include <stdio.h>
 
 #include "jpegxs_conv.h"
 #include "types.h"
+#include "video_codec.h"
 
 static void uyvy_to_yuv422p(const uint8_t *src, int width, int height, svt_jpeg_xs_image_buffer_t *dst) {
 
@@ -65,11 +67,44 @@ static void rgb_to_rgbp(const uint8_t *src, int width, int height, svt_jpeg_xs_i
         }
 }
 
+static void v210_to_yuv422p(const uint8_t *src, int width, int height, svt_jpeg_xs_image_buffer_t *dst) {
+
+        for (int y = 0; y < height; ++y) {
+                const uint32_t *src_row = (const uint32_t *)(src + y * vc_get_linesize(width, v210));
+                uint16_t *dst_y = (uint16_t *)((uint8_t *) dst->data_yuv[0] + y * dst->stride[0]);
+                uint16_t *dst_u = (uint16_t *)((uint8_t *) dst->data_yuv[1] + y * dst->stride[1]);
+                uint16_t *dst_v = (uint16_t *)((uint8_t *) dst->data_yuv[2] + y * dst->stride[2]);
+
+                for (int x = 0; x < width; x += 6) {
+                        uint32_t w0 = *src_row++;
+                        uint32_t w1 = *src_row++;
+                        uint32_t w2 = *src_row++;
+                        uint32_t w3 = *src_row++;
+
+                        *dst_y++ = (w0 >> 10) & 0x3ff;
+                        *dst_y++ = w1 & 0x3ff;
+                        *dst_y++ = (w1 >> 20) & 0x3ff;
+                        *dst_y++ = (w2 >> 10) & 0x3ff;
+                        *dst_y++ = w3 & 0x3ff;
+                        *dst_y++ = (w3 >> 20) & 0x3ff;
+
+                        *dst_u++ = w0 & 0x3ff;
+                        *dst_u++ = (w1 >> 10) & 0x3ff;
+                        *dst_u++ = (w2 >> 20) & 0x3ff;
+
+                        *dst_v++ = (w0 >> 20) & 0x3ff;
+                        *dst_v++ = w2 & 0x3ff;
+                        *dst_v++ = (w3 >> 10) & 0x3ff;
+                }
+        }       
+}
+
 static const struct uv_to_jpegxs_conversion uv_to_jpegxs_conversions[] = {
         { UYVY, COLOUR_FORMAT_PLANAR_YUV422, uyvy_to_yuv422p },
         { YUYV, COLOUR_FORMAT_PLANAR_YUV422, yuyv_to_yuv422p },
         { I420, COLOUR_FORMAT_PLANAR_YUV420, i420_to_yuv420p },
         { RGB, COLOUR_FORMAT_PLANAR_YUV444_OR_RGB, rgb_to_rgbp },
+        { v210, COLOUR_FORMAT_PLANAR_YUV422, v210_to_yuv422p},
         { VIDEO_CODEC_NONE, COLOUR_FORMAT_INVALID, NULL }
 };
 
