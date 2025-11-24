53892bbd0ad2b74334e1334ebec02bb860eaf8ac
Martin Pulec <martin.pulec@cesnet.cz>
2025-11-24 10:51:46 +0100

    CI Linux AppImage test: run tests for both distros
    run the same test cases for both Arch Linux and Ubuntu
    
    + rename Dockerfile.arch to Dockerfile.archlinux (the former is a bit
    misleading - arch==architecture or what?)

--- .github/scripts/Linux/docker_appimage_tests.sh
+++ .github/scripts/Linux/docker_appimage_tests.sh
@@ -3,17 +3,17 @@
 mkdir aitest-context # empty build context
 ./UltraGrid-"$VERSION"-x86_64.AppImage --appimage-extract
 
-docker build -f .github/scripts/Linux/utils/Dockerfile.ubuntu\
- -t aitest-ubuntu aitest-context
-docker build -f .github/scripts/Linux/utils/Dockerfile.arch\
- -t aitest-arch aitest-context
-docker run --rm -v "$PWD"/squashfs-root/:/AppImage aitest-ubuntu\
- /AppImage/AppRun -v
-docker run --rm -v "$PWD"/squashfs-root/:/AppImage aitest-ubuntu\
- /AppImage/AppRun --tool uv-qt -h
-docker run --rm -v "$PWD"/squashfs-root/:/AppImage aitest-ubuntu\
- sh -c 'xvfb-run /AppImage/AppRun --tool uv-qt & { sleep 10; kill $!; }'
-docker run --rm -v "$PWD"/squashfs-root/:/AppImage aitest-ubuntu\
- /AppImage/AppRun --list-modules
-docker run --rm -v "$PWD"/squashfs-root:/AppImage aitest-arch\
- /AppImage/AppRun --capabilities
+for n in archlinux ubuntu; do
+        docker build -f .github/scripts/Linux/utils/Dockerfile.$n\
+         -t aitest-$n aitest-context
+        docker run --rm -v "$PWD"/squashfs-root/:/AppImage aitest-$n\
+         /AppImage/AppRun -v
+        docker run --rm -v "$PWD"/squashfs-root/:/AppImage aitest-$n\
+         /AppImage/AppRun --tool uv-qt -h
+        docker run --rm -v "$PWD"/squashfs-root/:/AppImage aitest-$n\
+         sh -c 'xvfb-run /AppImage/AppRun --tool uv-qt & { sleep 10; kill $!; }'
+        docker run --rm -v "$PWD"/squashfs-root/:/AppImage aitest-$n\
+         /AppImage/AppRun --list-modules
+        docker run --rm -v "$PWD"/squashfs-root:/AppImage aitest-$n\
+         /AppImage/AppRun --capabilities
+done
