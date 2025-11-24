e693ca0b987a37e6dd524c864da6face91d27d9c
Martin Pulec <martin.pulec@cesnet.cz>
2025-11-24 10:40:32 +0100

    GH/.../docker_appimage_tests.sh: quote vars
    + split long lines
    + move --appimage-extract to the begining
    
    all changes are mostly a refactor

--- .github/scripts/Linux/docker_appimage_tests.sh
+++ .github/scripts/Linux/docker_appimage_tests.sh
@@ -1,11 +1,19 @@
 #!/bin/sh -eu
 
 mkdir aitest-context # empty build context
-docker build -f .github/scripts/Linux/utils/Dockerfile.ubuntu -t aitest-ubuntu aitest-context
-docker build -f .github/scripts/Linux/utils/Dockerfile.arch -t aitest-arch aitest-context
-./UltraGrid-$VERSION-x86_64.AppImage --appimage-extract
-docker run --rm -v $PWD/squashfs-root/:/AppImage aitest-ubuntu /AppImage/AppRun -v
-docker run --rm -v $PWD/squashfs-root/:/AppImage aitest-ubuntu /AppImage/AppRun --tool uv-qt -h
-docker run --rm -v $PWD/squashfs-root/:/AppImage aitest-ubuntu sh -c 'xvfb-run /AppImage/AppRun --tool uv-qt & { sleep 10; kill $!; }'
-docker run --rm -v $PWD/squashfs-root/:/AppImage aitest-ubuntu /AppImage/AppRun --list-modules
-docker run --rm -v $PWD/squashfs-root:/AppImage aitest-arch /AppImage/AppRun --capabilities
+./UltraGrid-"$VERSION"-x86_64.AppImage --appimage-extract
+
+docker build -f .github/scripts/Linux/utils/Dockerfile.ubuntu\
+ -t aitest-ubuntu aitest-context
+docker build -f .github/scripts/Linux/utils/Dockerfile.arch\
+ -t aitest-arch aitest-context
+docker run --rm -v "$PWD"/squashfs-root/:/AppImage aitest-ubuntu\
+ /AppImage/AppRun -v
+docker run --rm -v "$PWD"/squashfs-root/:/AppImage aitest-ubuntu\
+ /AppImage/AppRun --tool uv-qt -h
+docker run --rm -v "$PWD"/squashfs-root/:/AppImage aitest-ubuntu\
+ sh -c 'xvfb-run /AppImage/AppRun --tool uv-qt & { sleep 10; kill $!; }'
+docker run --rm -v "$PWD"/squashfs-root/:/AppImage aitest-ubuntu\
+ /AppImage/AppRun --list-modules
+docker run --rm -v "$PWD"/squashfs-root:/AppImage aitest-arch\
+ /AppImage/AppRun --capabilities
