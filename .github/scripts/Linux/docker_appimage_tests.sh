9e7f643aed145afb69d275591bba316c28ebd521
Martin Pulec <martin.pulec@cesnet.cz>
2025-11-24 13:04:34 +0100

    docker_appimage_tests.sh: simplify
    add the version unconditionally in the script and do not add a placeholder
    to Dockerfile

--- .github/scripts/Linux/docker_appimage_tests.sh
+++ .github/scripts/Linux/docker_appimage_tests.sh
@@ -3,17 +3,12 @@
 mkdir aitest-context # empty build context
 ./UltraGrid-"$VERSION"-x86_64.AppImage --appimage-extract
 
-for n in archlinux ubuntu:22.04 ubuntu:latest; do
-        dockerfile=.github/scripts/Linux/utils/Dockerfile.$n
-        if expr $n : ".*:"; then
-                image_name=$(echo $n | cut -d: -f 1)
-                image_version=$(echo $n | cut -d: -f 2)
-                n_dockerfile=$(mktemp)
-                cat .github/scripts/Linux/utils/Dockerfile."$image_name" |
-                        sed "s/DOCKER_IMAGE_VERSION/$image_version/"\
-                        > "$n_dockerfile"
-                dockerfile=$n_dockerfile
-        fi
+for n in archlinux:latest ubuntu:22.04 ubuntu:latest; do
+        image_name=$(echo $n | cut -d: -f 1)
+        image_version=$(echo $n | cut -d: -f 2)
+        dockerfile=$(mktemp)
+        cat .github/scripts/Linux/utils/Dockerfile."$image_name" |
+                sed "/FROM /s/:\$/$image_version/" > "$dockerfile"
         docker build -f "$dockerfile" -t aitest-$n aitest-context
         docker run --rm -v "$PWD"/squashfs-root/:/AppImage aitest-$n sh -ce '
 /AppImage/AppRun -v
