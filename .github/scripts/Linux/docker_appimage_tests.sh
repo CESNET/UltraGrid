59069b8361212d6c1d0476fce64d9f2e514c810b
Martin Pulec <martin.pulec@cesnet.cz>
2025-11-24 11:12:24 +0100

    CI Linux AppImage check: run at once

--- .github/scripts/Linux/docker_appimage_tests.sh
+++ .github/scripts/Linux/docker_appimage_tests.sh
@@ -6,14 +6,11 @@ mkdir aitest-context # empty build context
 for n in archlinux ubuntu; do
         docker build -f .github/scripts/Linux/utils/Dockerfile.$n\
          -t aitest-$n aitest-context
-        docker run --rm -v "$PWD"/squashfs-root/:/AppImage aitest-$n\
-         /AppImage/AppRun -v
-        docker run --rm -v "$PWD"/squashfs-root/:/AppImage aitest-$n\
-         /AppImage/AppRun --tool uv-qt -h
-        docker run --rm -v "$PWD"/squashfs-root/:/AppImage aitest-$n\
-         sh -c 'xvfb-run /AppImage/AppRun --tool uv-qt & { sleep 10; kill $!; }'
-        docker run --rm -v "$PWD"/squashfs-root/:/AppImage aitest-$n\
-         /AppImage/AppRun --list-modules
-        docker run --rm -v "$PWD"/squashfs-root:/AppImage aitest-$n\
-         /AppImage/AppRun --capabilities
+        docker run --rm -v "$PWD"/squashfs-root/:/AppImage aitest-$n sh -ce '
+/AppImage/AppRun -v
+/AppImage/AppRun --tool uv-qt -h
+xvfb-run /AppImage/AppRun --tool uv-qt & { sleep 10; kill $!; }
+/AppImage/AppRun --list-modules
+/AppImage/AppRun --capabilities
+'
 done
