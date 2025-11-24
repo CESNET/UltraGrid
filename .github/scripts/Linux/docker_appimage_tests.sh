8b13221a056bb3c12f010b2345dde0c4d91a84b0
Martin Pulec <martin.pulec@cesnet.cz>
2025-11-24 14:59:19 +0100

    CI Linux: run Docker tests in parallel

--- .github/scripts/Linux/docker_appimage_tests.sh
+++ .github/scripts/Linux/docker_appimage_tests.sh
@@ -3,18 +3,31 @@
 mkdir aitest-context # empty build context
 ./UltraGrid-"$VERSION"-x86_64.AppImage --appimage-extract
 
-for n in archlinux:latest ubuntu:22.04 ubuntu:latest; do
-        image_name=$(echo $n | cut -d: -f 1)
-        image_version=$(echo $n | cut -d: -f 2)
+run_docker_test() {
+        image_name=$(echo "$1" | cut -d: -f 1)
+        image_version=$(echo "$1" | cut -d: -f 2)
         dockerfile=$(mktemp)
         cat .github/scripts/Linux/utils/Dockerfile."$image_name" |
                 sed "/FROM /s/:\$/$image_version/" > "$dockerfile"
-        docker build -f "$dockerfile" -t aitest-$n aitest-context
-        docker run --rm -v "$PWD"/squashfs-root/:/AppImage aitest-$n sh -ce '
+        docker build -f "$dockerfile" -t aitest-"$1" aitest-context
+        docker run --rm -v "$PWD"/squashfs-root/:/AppImage aitest-"$1" sh -ce '
 /AppImage/AppRun -v
 /AppImage/AppRun --tool uv-qt -h
 xvfb-run /AppImage/AppRun --tool uv-qt & { sleep 10; kill $!; }
 /AppImage/AppRun --list-modules
 /AppImage/AppRun --capabilities
 '
+}
+
+test_list="archlinux:latest ubuntu:22.04 ubuntu:latest"
+
+# run the Docker tests in parallel to lower the time
+for n in $test_list; do
+        run_docker_test "$n" &
+        name=$(printf "%s" "$n" | tr -c '[:alnum:]' '[_*]') # replace :. with _ for valid identifer
+        eval "${name}_pid"=$!
+done
+for n in $test_list; do
+        name=$(printf "%s" "$n" | tr -c '[:alnum:]' '[_*]')
+        eval wait "\$${name}_pid"
 done
