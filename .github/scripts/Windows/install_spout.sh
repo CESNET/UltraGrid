b300fa64f46cc4798b1abaaaaa51d4de407e86b3
Martin Pulec <martin.pulec@cesnet.cz>
2026-01-09 14:57:58 +0100

    CI Win: use pre-built Spout
    instead of building own Spout, use pre-built release on GH
    
    This actually reverts the commit b98b5ab0 from 2023-11-06.
    Date:   Mon Nov 6 09:08:46 2023 +0100

--- .github/scripts/Windows/install_spout.sh
+++ .github/scripts/Windows/install_spout.sh
@@ -1,21 +1,13 @@
 #!/bin/sh -eux
 # Install SPOUT
 
-build() {(
-        cd /c
-        rm -rf Spout2
-        git clone --depth 1 https://github.com/leadedge/Spout2.git
-        cd Spout2
-        /c/Program\ Files/CMake/bin/cmake.exe -Bbuild2 . # ./BUILD already exists
-        /c/Program\ Files/CMake/bin/cmake.exe --build build2 --config Release \
-         -j "$(nproc)"
-)}
-
 install() {(
-        mkdir -p /usr/local/bin /usr/local/include /usr/local/lib
-        cp /c/Spout2/build2/bin/Release/SpoutLibrary.dll /usr/local/bin/
-        cp /c/Spout2/build2/lib/Release/SpoutLibrary.lib /usr/local/lib/
-        cp /c/Spout2/SPOUTSDK/SpoutLibrary/SpoutLibrary.h /usr/local/include/
+        .github/scripts/download-gh-asset.sh leadedge/Spout2 binaries Spout.zip
+        unzip Spout.zip
+        d=$(echo Spout-SDK-binaries/Libs_*)
+        cp "$d"/MT/bin/SpoutLibrary.dll /usr/local/bin/
+        cp "$d"/MT/lib/SpoutLibrary.lib /usr/local/lib/
+        cp "$d"/include/SpoutLibrary/SpoutLibrary.h /usr/local/include/
 )}
 
-$1
+install
