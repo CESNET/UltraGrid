799e4b420fe7a48301b320305f97cf1e3c3257d1
Martin Pulec <martin.pulec@cesnet.cz>
2026-01-12 09:56:58 +0100

    CI Win: temporarily revert to Spout 2.007.016
    refers to <https://github.com/CESNET/UltraGrid/issues/487>

--- .github/scripts/Windows/install_spout.sh
+++ .github/scripts/Windows/install_spout.sh
@@ -2,9 +2,12 @@
 # Install SPOUT
 
 install() {(
-        .github/scripts/download-gh-asset.sh leadedge/Spout2 binaries Spout.zip
+        # .github/scripts/download-gh-asset.sh leadedge/Spout2 binaries Spout.zip
+        curl -LSs https://github.com/leadedge/Spout2/releases/download/\
+2.007.016/Spout-SDK-binaries_2-007-016.zip -o Spout.zip
         unzip Spout.zip
-        d=$(echo Spout-SDK-binaries/Libs_*)
+        # d=$(echo Spout-SDK-binaries/Libs_*)
+        d=$(echo Spout-SDK-binaries/*/Libs)
         cp "$d"/MT/bin/SpoutLibrary.dll /usr/local/bin/
         cp "$d"/MT/lib/SpoutLibrary.lib /usr/local/lib/
         cp "$d"/include/SpoutLibrary/SpoutLibrary.h /usr/local/include/
