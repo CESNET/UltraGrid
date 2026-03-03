e417b4c970f685cce0b6b7c7dc91331d82e28009
Martin Piatka <piatka@cesnet.cz>
2026-03-03 10:48:31 +0100

    video_rxtx/sage: Remove config includes

--- src/video_rxtx/sage.cpp
+++ src/video_rxtx/sage.cpp
@@ -35,13 +35,6 @@
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#include "config_unix.h"
-#include "config_win32.h"
-#endif // HAVE_CONFIG_H
-
 #include <string>
 #include <sstream>
 
