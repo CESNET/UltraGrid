e085b03be85506c402102a0352b7ff22cc8ed95d
Juraj Zemančík <550535@mail.muni.cz>
2026-04-20 15:09:55 +0200

    openapv compress: initial simple configure_with

--- src/video_compress/openapv.cpp
+++ src/video_compress/openapv.cpp
@@ -1,4 +1,5 @@
 #include "lib_common.h"
+#include "utils/misc.h"
 #include "video_compress.h"
 #include <oapv/oapv.h>
 
@@ -13,10 +14,11 @@ struct state_video_compress_oapv {
         oapve_t id;             // OAPV encoder id
         oapvm_t mid;            // OAPV metadata id
         oapve_cdesc_t   cdsc;   // description for encoder creation
+        
         oapv_bitb_t     bitb;   // bitstream buffer (output)
         oapve_stat_t    stat;   // encoding status (output)
 
-        oapv_frms_t input_frms;      // frames for input
+        oapv_frms_t input_frms; // frames for input
         int num_frames;         // number of frames in an access unit
         int preset_id;          // preset of apv (fastest, fast, medium, slow, placebo)
         int qp;                 // quantization parameter
@@ -31,6 +33,24 @@ state_video_compress_oapv::state_video_compress_oapv(module *parent, const char 
                 printf("Failed to get default parameters for OAPV encoder: %d\n", ret);
                 throw 1;
         }
+}
+
+static bool configure_with(struct state_video_compress_oapv *s, struct video_desc desc)
+{
+        s->cdsc.param[0].w = desc.width;
+        s->cdsc.param[0].h = desc.height;
+
+        s->cdsc.param[0].rc_type = OAPV_RC_ABR;
+        s->cdsc.param[0].qp = OAPVE_PARAM_QP_AUTO;
+
+        s->cdsc.param[0].fps_num = get_framerate_n(desc.fps);
+        s->cdsc.param[0].fps_den = get_framerate_d(desc.fps);
+
+        s->cdsc.max_bs_buf_size = 128 * 1024 * 1024;
+        s->cdsc.max_num_frms = 1;
+        s->cdsc.threads = OAPV_CDESC_THREADS_AUTO;
+
+        return true;
 }
 
 static void* openapv_compress_init(module *parent, const char *opts)
