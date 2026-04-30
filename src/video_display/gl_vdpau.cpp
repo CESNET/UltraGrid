1be6f84aae4382e4a9d4c077e6b401f43af57148
Martin Pulec <martin.pulec@cesnet.cz>
2026-04-30 14:52:37 +0200

    mv gl_vdpau impl to .cpp file

--- src/video_display/gl_vdpau.cpp
+++ src/video_display/gl_vdpau.cpp
@@ -41,10 +41,58 @@
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <cassert>
-#include "debug.h"
-
 #include "gl_vdpau.hpp"
+
+#include <cassert>
+
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#include <OpenGL/OpenGL.h> // CGL
+#include <OpenGL/glext.h>
+#else
+#include <GL/glew.h>
+#endif /* __APPLE__ */
+
+#include "debug.h"
+#include "hwaccel_vdpau.h"
+
+typedef GLintptr vdpauSurfaceNV;
+#define NV_CAST(x) ((void *)(uintptr_t)(x))
+
+struct state_vdpau {
+        bool initialized = false;
+        GLuint textures[4] = {0};
+        hw_vdpau_frame lastFrame;
+
+        bool interopInitialized = false;
+        bool mixerInitialized = false;
+        VdpDevice device = VDP_INVALID_HANDLE;
+        VdpGetProcAddress *get_proc_address = nullptr;
+        vdpauSurfaceNV vdpgl_surf = 0;
+
+        VdpOutputSurface out_surf = VDP_INVALID_HANDLE;
+        VdpVideoMixer mixer = VDP_INVALID_HANDLE;
+
+        uint32_t surf_width = 0;
+        uint32_t surf_height = 0;
+        VdpChromaType surf_ct = 0;
+
+        bool init();
+        void checkInterop(VdpDevice device, VdpGetProcAddress *get_proc_address);
+        void initInterop(VdpDevice device, VdpGetProcAddress *get_proc_address);
+        void uninitInterop();
+
+        void loadFrame(hw_vdpau_frame *frame);
+
+        void initMixer(uint32_t w, uint32_t h, VdpChromaType ct);
+        void mixerRender(VdpVideoSurface f);
+        void uninitMixer();
+
+        void uninit();
+
+        vdp_funcs funcs;
+
+};
 
 /**
  * @brief Checks VdpMixer parameters and reinitializes it if they don't match the video parameters
--- src/video_display/gl_vdpau.hpp
+++ src/video_display/gl_vdpau.hpp
@@ -48,56 +48,19 @@
 
 #ifdef HWACC_VDPAU
 
-#ifdef __APPLE__
-#include <OpenGL/gl.h>
-#include <OpenGL/OpenGL.h> // CGL
-#include <OpenGL/glext.h>
-#else
-#include <GL/glew.h>
-#endif /* __APPLE__ */
-
 #include "hwaccel_vdpau.h"
-typedef GLintptr vdpauSurfaceNV;
-#define NV_CAST(x) ((void *)(uintptr_t)(x))
 
-struct state_vdpau {
-        bool initialized = false;
-        GLuint textures[4] = {0};
-        hw_vdpau_frame lastFrame;
+#ifdef __cplusplus
+extern "C" {
+#endif // defined __cplusplus
 
-        bool interopInitialized = false;
-        bool mixerInitialized = false;
-        VdpDevice device = VDP_INVALID_HANDLE;
-        VdpGetProcAddress *get_proc_address = nullptr;
-        vdpauSurfaceNV vdpgl_surf = 0;
-
-        VdpOutputSurface out_surf = VDP_INVALID_HANDLE;
-        VdpVideoMixer mixer = VDP_INVALID_HANDLE;
-
-        uint32_t surf_width = 0;
-        uint32_t surf_height = 0;
-        VdpChromaType surf_ct = 0;
-
-        bool init();
-        void checkInterop(VdpDevice device, VdpGetProcAddress *get_proc_address);
-        void initInterop(VdpDevice device, VdpGetProcAddress *get_proc_address);
-        void uninitInterop();
-
-        void loadFrame(hw_vdpau_frame *frame);
-
-        void initMixer(uint32_t w, uint32_t h, VdpChromaType ct);
-        void mixerRender(VdpVideoSurface f);
-        void uninitMixer();
-
-        void uninit();
-
-        vdp_funcs funcs;
-
-};
-
-struct state_vdpau * vdp_init();
+struct state_vdpau *vdp_init();
 void vdp_load_frame(struct state_vdpau *vdp, hw_vdpau_frame *frame);
 void vdp_destroy(struct state_vdpau *vdp);
+
+#ifdef __cplusplus
+}
+#endif // defined __cplusplus
 
 #else
 
