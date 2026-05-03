2148acfb54b05e076dcbfe263ee43962f4fddb7d
Juraj Zemančík <550535@mail.muni.cz>
2026-05-03 23:52:19 +0200

    openapv decompress: decoder refactor

--- src/video_decompress/openapv.cpp
+++ src/video_decompress/openapv.cpp
@@ -13,21 +13,12 @@ namespace {
 
 #define MOD_NAME "[OpenAPV dec.] "
 
-struct from_openapv_conversion {
-        codec_t         dst_codec;
-        int             required_src_cs;
-        decode_planar_func_t *convert;
-        int             log2_chroma_h;
-};
-
-static const from_openapv_conversion from_openapv_conversions[] = {
-        { UYVY, OAPV_CS_YCBCR422_10LE, yuv422pXX_to_uyvy,   0 },
-        { YUYV, OAPV_CS_YCBCR422_10LE, yuv422p_to_yuyv,     0 },
-        { v210, OAPV_CS_YCBCR422_10LE, yuv422p10le_to_v210, 0 },
-        { VIDEO_CODEC_NONE, OAPV_CS_UNKNOWN, nullptr, 0 },
-};
+#define MAX_NUM_FRMS (1) // support only primary frame
+#define FRM_INDEX    (0) // index of primary frame in oapv_frms_t
 
 struct state_video_decompress_openapv {
+        state_video_decompress_openapv();
+        ~state_video_decompress_openapv();
         
         oapvd_t decoder_handle{};
         oapvm_t metadata_handle{};
@@ -36,9 +27,71 @@ struct state_video_decompress_openapv {
         oapv_frms_t decoded_frames{};
         oapv_bitb_t input_buffer{};
 
+        bool configured = false;
+
+        const struct from_openapv_conversion *convert_from_planar;
+
         struct video_desc desc{};
-        int pitch = 0;
+        int pitch;
         codec_t out_codec = VIDEO_CODEC_NONE;
+};
+
+state_video_decompress_openapv::state_video_decompress_openapv() {
+        int ret = -1;
+
+        cdesc.threads = OAPV_CDESC_THREADS_AUTO;
+        decoder_handle = oapvd_create(&cdesc, &ret);
+        if (OAPV_FAILED(ret)) {
+                log_msg(LOG_LEVEL_ERROR, MOD_NAME "oapvd_create failed (ret=%d).\n", ret);
+                throw 1;
+        }
+
+        metadata_handle = oapvm_create(&ret);
+        if (OAPV_FAILED(ret)) {
+                log_msg(LOG_LEVEL_ERROR, MOD_NAME "oapvm_create failed (ret=%d).\n", ret);
+                oapvd_delete(decoder_handle);
+                throw 1;
+        }
+
+        decoded_frames.num_frms = MAX_NUM_FRMS;
+        decoded_frames.frm[FRM_INDEX].imgb = new oapv_imgb_t();
+        if (!decoded_frames.frm[FRM_INDEX].imgb) {
+                log_msg(LOG_LEVEL_ERROR, MOD_NAME "Failed to allocate oapv_imgb_t for decoded frame.\n");
+                oapvm_delete(metadata_handle);
+                oapvd_delete(decoder_handle);
+                throw 1;
+        }
+        memset(decoded_frames.frm[FRM_INDEX].imgb, 0, sizeof(oapv_imgb_t));
+}
+
+state_video_decompress_openapv::~state_video_decompress_openapv() {
+        if (decoder_handle) {
+                oapvd_delete(decoder_handle);
+        }
+        if (metadata_handle) {
+                oapvm_delete(metadata_handle);
+        }
+        for (int i = 0; i < decoded_frames.num_frms; ++i) {
+                if (decoded_frames.frm[i].imgb) {
+                        for (int j = 0; j < OAPV_MAX_CC; ++j) {
+                                free(decoded_frames.frm[i].imgb->baddr[j]);
+                        }
+                        delete decoded_frames.frm[i].imgb;
+                }
+        }
+}
+
+struct from_openapv_conversion {
+        codec_t         dst_codec;
+        int             required_src_cs;
+        decode_planar_func_t *convert;
+};
+
+static const from_openapv_conversion from_openapv_conversions[] = {
+        { UYVY, OAPV_CS_YCBCR422_10LE, yuv422pXX_to_uyvy},
+        { YUYV, OAPV_CS_YCBCR422_10LE, yuv422p_to_yuyv},
+        { v210, OAPV_CS_YCBCR422_10LE, yuv422p10le_to_v210},
+        { VIDEO_CODEC_NONE, OAPV_CS_UNKNOWN, nullptr},
 };
 
 static const from_openapv_conversion *get_from_openapv_conversion(codec_t dst_codec) {
@@ -50,109 +103,112 @@ static const from_openapv_conversion *get_from_openapv_conversion(codec_t dst_co
         return nullptr;
 }
 
+static bool output_buffer_setup(oapv_imgb_t *imgb, const oapv_frm_info_t *frm_info) {
+        memset(imgb, 0, sizeof(*imgb));
+        imgb->cs = frm_info->cs;
+        imgb->w[0] = frm_info->w;
+        imgb->h[0] = frm_info->h;
+        int cf = OAPV_CS_GET_FORMAT(frm_info->cs);
+        switch(cf) {
+                case OAPV_CF_YCBCR422:
+                        imgb->w[1] = imgb->w[2] = (frm_info->w + 1) / 2;
+                        imgb->h[1] = imgb->h[2] = frm_info->h;
+                        imgb->np = 3;
+                        break;
+                case OAPV_CF_YCBCR444:
+                        imgb->w[1] = imgb->w[2] = frm_info->w;
+                        imgb->h[1] = imgb->h[2] = frm_info->h;
+                        imgb->np = 3;
+                        break;
+                case OAPV_CF_YCBCR4444:
+                        imgb->w[1] = imgb->w[2] = imgb->w[3] = frm_info->w;
+                        imgb->h[1] = imgb->h[2] = imgb->h[3] = frm_info->h;
+                        imgb->np = 4;
+                        break;
+                default:
+                        log_msg(LOG_LEVEL_ERROR, MOD_NAME "Unsupported color format for input buffer: %d\n", cf);
+                        return false;
+        }
+
+        for (int i = 0; i < imgb->np; ++i) {
+                imgb->aw[i] = ((imgb->w[i] + OAPV_MB_W - 1) / OAPV_MB_W) * OAPV_MB_W;
+                imgb->ah[i] = ((imgb->h[i] + OAPV_MB_H - 1) / OAPV_MB_H) * OAPV_MB_H;
+                imgb->s[i] = imgb->aw[i] * OAPV_CS_GET_BYTE_DEPTH(imgb->cs);
+                imgb->e[i] = imgb->ah[i];
+                imgb->bsize[i] = imgb->s[i] * imgb->e[i];
+                imgb->baddr[i] = malloc(imgb->bsize[i]);
+                if (imgb->baddr[i] == nullptr) {
+                        log_msg(LOG_LEVEL_ERROR, MOD_NAME "Failed to allocate plane %d for frame %d (%zu bytes).\n",
+                                i, FRM_INDEX, (size_t) imgb->bsize[i]);
+                        for (int j = 0; j < i; ++j) {
+                                free(imgb->baddr[j]);
+                        }
+                        return false;
+                }
+                memset(imgb->baddr[i], 0, imgb->bsize[i]);
+                imgb->a[i] = imgb->baddr[i];
+        }
+        return true;
+}
+
 static bool configure_with(struct state_video_decompress_openapv *s, unsigned char *bitstream_buffer, size_t codestream_size) {
         int ret = -1;
         oapv_au_info_t aui{};
 
         ret = oapvd_info(bitstream_buffer, codestream_size, &aui);
-        if (OAPV_FAILED(ret) || aui.num_frms < 1) {
+        if (OAPV_FAILED(ret) || aui.num_frms != 1) {
+                log_msg(LOG_LEVEL_ERROR, MOD_NAME "Failed to get APV info (ret=%d, num_frms=%d).\n", ret, aui.num_frms);
                 return false;
         }
 
-        if (s->decoder_handle == nullptr) {
-                s->cdesc.threads = OAPV_CDESC_THREADS_AUTO;
-                s->decoder_handle = oapvd_create(&s->cdesc, &ret);
-                if (OAPV_FAILED(ret)) {
-                        log_msg(LOG_LEVEL_ERROR, MOD_NAME "oapvd_create failed (ret=%d).\n", ret);
-                        return false;
-                }
+        for (int i = 0; i < OAPV_MAX_CC; ++i) {
+                free(s->decoded_frames.frm[FRM_INDEX].imgb->baddr[i]);
         }
 
-        if (s->metadata_handle == nullptr) {
-                s->metadata_handle = oapvm_create(&ret);
-                if (OAPV_FAILED(ret)) {
-                        log_msg(LOG_LEVEL_ERROR, MOD_NAME "oapvm_create failed (ret=%d).\n", ret);
-                        oapvd_delete(s->decoder_handle);
-                        s->decoder_handle = nullptr;
-                        return false;
-                }
+        const oapv_frm_info_t &frm_info = aui.frm_info[FRM_INDEX];
+        int fmt = OAPV_CS_GET_FORMAT(frm_info.cs);
+        if (fmt != OAPV_CF_YCBCR422 && fmt != OAPV_CF_YCBCR444 && fmt != OAPV_CF_YCBCR4444) {
+                log_msg(LOG_LEVEL_ERROR, MOD_NAME "Unsupported APV color format %d for frame %d.\n", fmt, FRM_INDEX);
+                return false;
         }
 
-        if (s->decoded_frames.frm[0].imgb != nullptr) {
-                for (int i = 0; i < OAPV_MAX_CC; ++i) {
-                        free(s->decoded_frames.frm[0].imgb->baddr[i]);
-                }
-                delete s->decoded_frames.frm[0].imgb;
-                s->decoded_frames.frm[0].imgb = nullptr;
+        oapv_imgb_t *imgb = s->decoded_frames.frm[FRM_INDEX].imgb;
+        if (!output_buffer_setup(imgb, &frm_info)) {
+                log_msg(LOG_LEVEL_ERROR, MOD_NAME "Failed to set up output buffer for frame %d.\n", FRM_INDEX);
+                return false;
         }
 
-        s->decoded_frames.num_frms = aui.num_frms;
-        for (int fi = 0; fi < aui.num_frms; ++fi) {
-                const oapv_frm_info_t &frm_info = aui.frm_info[fi];
-                int fmt = OAPV_CS_GET_FORMAT(frm_info.cs);
-                if (fmt != OAPV_CF_YCBCR422 && fmt != OAPV_CF_YCBCR444 && fmt != OAPV_CF_YCBCR4444) {
-                        log_msg(LOG_LEVEL_ERROR, MOD_NAME "Unsupported APV color format %d for frame %d.\n", fmt, fi);
-                        return false;
-                }
+        s->decoded_frames.frm[FRM_INDEX].pbu_type = frm_info.pbu_type;
+        s->decoded_frames.frm[FRM_INDEX].group_id = frm_info.group_id;
 
-                s->decoded_frames.frm[fi].imgb = new oapv_imgb_t();
-                memset(s->decoded_frames.frm[fi].imgb, 0, sizeof(oapv_imgb_t));
-
-                oapv_imgb_t *imgb = s->decoded_frames.frm[fi].imgb;
-                imgb->cs = frm_info.cs;
-                imgb->w[0] = frm_info.w;
-                imgb->h[0] = frm_info.h;
-                int cf = OAPV_CS_GET_FORMAT(frm_info.cs);
-                switch(cf) {
-                        case OAPV_CF_YCBCR422:
-                                imgb->w[1] = imgb->w[2] = (frm_info.w + 1) / 2;
-                                imgb->h[1] = imgb->h[2] = frm_info.h;
-                                imgb->np = 3;
-                                break;
-                        case OAPV_CF_YCBCR444:
-                                imgb->w[1] = imgb->w[2] = frm_info.w;
-                                imgb->h[1] = imgb->h[2] = frm_info.h;
-                                imgb->np = 3;
-                                break;
-                        case OAPV_CF_YCBCR4444:
-                                imgb->w[1] = imgb->w[2] = imgb->w[3] = frm_info.w;
-                                imgb->h[1] = imgb->h[2] = imgb->h[3] = frm_info.h;
-                                imgb->np = 4;
-                                break;
-                        default:
-                                printf("Unsupported color format for input buffer: %d\n", cf);
-                                return false;
-                }
-
-                for (int i = 0; i < imgb->np; ++i) {
-                        imgb->aw[i] = ((imgb->w[i] + OAPV_MB_W - 1) / OAPV_MB_W) * OAPV_MB_W;
-                        imgb->ah[i] = ((imgb->h[i] + OAPV_MB_H - 1) / OAPV_MB_H) * OAPV_MB_H;
-                        imgb->s[i] = imgb->aw[i] * OAPV_CS_GET_BYTE_DEPTH(imgb->cs);
-                        imgb->e[i] = imgb->ah[i];
-                        imgb->bsize[i] = imgb->s[i] * imgb->e[i];
-                        imgb->baddr[i] = malloc(imgb->bsize[i]);
-                        if (imgb->baddr[i] == nullptr) {
-                                log_msg(LOG_LEVEL_ERROR, MOD_NAME "Failed to allocate plane %d for frame %d (%zu bytes).\n",
-                                        i, fi, (size_t) imgb->bsize[i]);
-                                for (int j = 0; j < i; ++j) {
-                                        free(imgb->baddr[j]);
-                                }
-                                delete imgb;
-                                s->decoded_frames.frm[fi].imgb = nullptr;
-                                return false;
-                        }
-                        memset(imgb->baddr[i], 0, imgb->bsize[i]);
-                        imgb->a[i] = imgb->baddr[i];
-                }
-
-                s->decoded_frames.frm[fi].pbu_type = frm_info.pbu_type;
-                s->decoded_frames.frm[fi].group_id = frm_info.group_id;
+        const from_openapv_conversion *conv = get_from_openapv_conversion(s->out_codec);
+        if (conv == nullptr) {
+                log_msg(LOG_LEVEL_WARNING, MOD_NAME "Unsupported out_codec=%s.\n", get_codec_name(s->out_codec));
+                return false;
         }
 
-        s->input_buffer.ssize = codestream_size;
-        s->input_buffer.bsize = codestream_size;
+        s->convert_from_planar = conv;
+        s->configured = true;
 
         return true;
+}
+
+static void openapv_to_uv_convert(struct state_video_decompress_openapv *s,
+                     const oapv_imgb_t *src, uint8_t *dst) {
+        struct from_planar_data d = {};
+        d.width = src->w[0];
+        d.height = src->h[0];
+        d.out_data = dst;
+        d.out_pitch = s->pitch;
+        d.in_data[0] = (const unsigned char *) src->a[0];
+        d.in_data[1] = (const unsigned char *) src->a[1];
+        d.in_data[2] = (const unsigned char *) src->a[2];
+        d.in_linesize[0] = src->s[0];
+        d.in_linesize[1] = src->s[1];
+        d.in_linesize[2] = src->s[2];
+        d.in_depth = OAPV_CS_GET_BIT_DEPTH(src->cs);
+        d.log2_chroma_h = 0;
+        decode_planar_parallel(s->convert_from_planar->convert, d, FROM_PLANAR_THREADS_AUTO);
 }
 
 static void *openapv_decompress_init(void) {
@@ -164,20 +220,24 @@ static int openapv_decompress_reconfigure(void *state, struct video_desc desc,
         int rshift, int gshift, int bshift, int pitch, codec_t out_codec) {
         state_video_decompress_openapv *s = (state_video_decompress_openapv *) state;
 
+        if (s->out_codec == out_codec &&
+                s->pitch == pitch &&
+                video_desc_eq_excl_param(s->desc, desc, PARAM_INTERLACING)) {
+                return true;
+        }
+
         s->desc = desc;
         s->pitch = pitch;
         s->out_codec = out_codec;
-
-        if (out_codec != VIDEO_CODEC_NONE && get_from_openapv_conversion(out_codec) == nullptr) {
-                log_msg(LOG_LEVEL_ERROR, MOD_NAME "Unsupported output codec %s.\n", get_codec_name(out_codec));
-                return false;
-        }
+        s->configured = false;
+        s->convert_from_planar = nullptr;
 
         return true;
 }
 
 static decompress_status openapv_decompress(void *state, unsigned char *dst, unsigned char *buffer, 
         unsigned int src_len, int frame_seq, struct video_frame_callbacks *callbacks, struct pixfmt_desc *internal_prop) {
+        int ret = -1;
         state_video_decompress_openapv *s = (state_video_decompress_openapv *) state;
 
         if (s->out_codec == VIDEO_CODEC_NONE) {
@@ -199,67 +259,59 @@ static decompress_status openapv_decompress(void *state, unsigned char *dst, uns
                 return DECODER_GOT_CODEC;
         }
 
-        const from_openapv_conversion *conv = get_from_openapv_conversion(s->out_codec);
-        if (conv == nullptr) {
+        if (!s->configured) {
+                if (!configure_with(s, buffer, src_len)) {
+                        return DECODER_NO_FRAME;
+                }
+        }
+
+        if (s->convert_from_planar == nullptr) {
                 log_msg(LOG_LEVEL_WARNING, MOD_NAME "Unsupported out_codec=%s.\n", get_codec_name(s->out_codec));
                 return DECODER_UNSUPP_PIXFMT;
         }
 
-        if (!configure_with(s, buffer, src_len)) {
-                log_msg(LOG_LEVEL_WARNING, MOD_NAME "configure_with failed for src_len=%u.\n", src_len);
-                return DECODER_NO_FRAME;
-        }
         s->input_buffer.addr = buffer;
+        s->input_buffer.ssize = src_len;
+        s->input_buffer.bsize = src_len;
+
         oapvd_stat_t stat{};
         oapvm_rem_all(s->metadata_handle);
-        int decode_ret = oapvd_decode(s->decoder_handle, &s->input_buffer, &s->decoded_frames, s->metadata_handle, &stat);
-        if (OAPV_FAILED(decode_ret)) {
-                log_msg(LOG_LEVEL_WARNING, MOD_NAME "oapvd_decode failed ret=%d src_len=%u.\n", decode_ret, src_len);
+        ret = oapvd_decode(s->decoder_handle, &s->input_buffer, &s->decoded_frames, s->metadata_handle, &stat);
+        if (OAPV_FAILED(ret)) {
+                log_msg(LOG_LEVEL_WARNING, MOD_NAME "oapvd_decode failed ret=%d src_len=%u.\n", ret, src_len);
                 return DECODER_NO_FRAME;
         }
 
-        int frm_idx = -1;
-        for (int i = 0; i < s->decoded_frames.num_frms; ++i) {
-                if (s->decoded_frames.frm[i].pbu_type == OAPV_PBU_TYPE_PRIMARY_FRAME) {
-                        frm_idx = i;
-                        break;
-                }
-        }
-        if (frm_idx < 0) {
-                return DECODER_NO_FRAME;
-        }
-
-        oapv_imgb_t *imgb = s->decoded_frames.frm[frm_idx].imgb; 
-        if (imgb == nullptr) {
-                return DECODER_NO_FRAME;
-        }
-        
-        if (imgb->cs != conv->required_src_cs) {
+        oapv_imgb_t *imgb = s->decoded_frames.frm[FRM_INDEX].imgb;
+        if (imgb->cs != s->convert_from_planar->required_src_cs) {
                 log_msg(LOG_LEVEL_WARNING, MOD_NAME "Decoded colorspace 0x%x does not match required 0x%x for output codec %s.\n",
-                        imgb->cs, conv->required_src_cs, get_codec_name(s->out_codec));
+                        imgb->cs, s->convert_from_planar->required_src_cs, get_codec_name(s->out_codec));
                 return DECODER_NO_FRAME;
         }
 
-        struct from_planar_data d = {};
-        d.width = imgb->w[0];
-        d.height = imgb->h[0];
-        d.out_data = dst;
-        d.out_pitch = s->pitch;
-        d.in_data[0] = (const unsigned char *) imgb->a[0];
-        d.in_data[1] = (const unsigned char *) imgb->a[1];
-        d.in_data[2] = (const unsigned char *) imgb->a[2];
-        d.in_linesize[0] = imgb->s[0];
-        d.in_linesize[1] = imgb->s[1];
-        d.in_linesize[2] = imgb->s[2];
-        d.in_depth = OAPV_CS_GET_BIT_DEPTH(imgb->cs);
-        d.log2_chroma_h = conv->log2_chroma_h;
-        decode_planar_parallel(conv->convert, d, FROM_PLANAR_THREADS_AUTO);
+        openapv_to_uv_convert(s, imgb, dst);
 
         return DECODER_GOT_FRAME;
 }
 
 static int openapv_decompress_get_property(void *state, int property, void *val, size_t *len) {
-        return true;
+        struct state_decompress *s = (struct state_decompress *) state;
+        UNUSED(s);
+        int ret = false;
+
+        switch(property) {
+                case DECOMPRESS_PROPERTY_ACCEPTS_CORRUPTED_FRAME:
+                        if(*len >= sizeof(int)) {
+                                *(int *) val = false;
+                                *len = sizeof(int);
+                                ret = true;
+                        }
+                        break;
+                default:
+                        ret = false;
+        }
+
+        return ret;
 }
 
 static void openapv_decompress_done(void *state) {
