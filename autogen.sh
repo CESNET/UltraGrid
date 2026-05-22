f22cebd32d82476175e4402c4bd156cfe8073ce5
Martin Pulec <martin.pulec@cesnet.cz>
2026-05-22 13:56:36 +0200

    configure: better handling of autoconf output
    Until now, the output was completly discarded if there was Makefile.am
    pattern (autoconf technically always fails because of missing that file).
    
    Improved to filter out just the pattern (+ summary) and print the rest.
    If there is a key word error, also fail.
    
    + fix configure.ac warning that is now shown (not relevant - SAGE is there
    just for the reference)
    + fix shellcheck warnings (if someone has spaced in path)

--- autogen.sh
+++ autogen.sh
@@ -6,31 +6,36 @@ command -v autoconf >/dev/null 2>&1 || { echo >&2 "Autoconf missing. Aborting.";
 
 [ -d m4 ] || mkdir m4
 
-srcdir=`dirname $0`
+srcdir=$(dirname "$0")
 test -z "$srcdir" && srcdir=.
-ORIGDIR=`pwd`
-cd $srcdir
+ORIGDIR=$(pwd)
+cd "$srcdir"
 
 . ./data/scripts/add_cl_if_not_present.sh # add --with-cuda-host-compiler=<cl> to current params (Win)
 
 # install config.guess config.sub install-sh missing
 echo "Running automake..."
 RES=$(automake --add-missing -c 2>&1 || true) # actual call will fail - we do not have Makefile.am
-if test -n "$RES" -a -z "$(echo $RES | grep Makefile.am)"; then
+if test -n "$RES" -a -z "$(echo "$RES" | grep Makefile.am)"; then
         echo "$RES"
         exit 1
 fi
 # Running autoreconf is preferred over aclocal/autoheader/autoconf.
 # It, however, needs to be a little bit bent because we do not use automake.
 echo "Running autoreconf..."
-RES=$(autoreconf -i 2>&1 || true)
+set +e
+RES=$(autoreconf -i 2>&1)
 # check if the error was the expected absence of Makefile.am or something else - then fail
-if test -n "$RES" -a -z "$(echo $RES | grep Makefile.am)"; then
-        echo "$RES"
+RES_WO_AM=$(echo "$RES" | grep -Ev "Makefile.am|automake failed with exit status")
+if echo "$RES_WO_AM" | grep -q error; then
+        echo "$RES" >&2
         exit 1
 fi
+if [ "$RES_WO_AM" ]; then
+        echo "$RES_WO_AM"
+fi
 
-cd $ORIGDIR
+cd "$ORIGDIR"
 
 echo "Running configure..."
 "$srcdir"/configure "$@"
