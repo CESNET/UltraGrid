708823c445c6349a35a6165f16feb12a8f40c58d
Martin Pulec <martin.pulec@cesnet.cz>
2026-06-10 08:44:33 +0200

    rxtx/loopback: can be in C

--- src/tv.h
+++ src/tv.h
@@ -96,7 +96,7 @@ typedef long long time_ns_t;
 #define NS_TO_MS(val_ns) ((val_ns) / 1000 / 1000)
 #define NS_TO_SEC_DBL(val_ns) ((double) (val_ns) / 1000 / 1000 / 1000)
 #define NS_TO_US(val_ns) ((val_ns) / 1000)
-#define MS_TO_NS(val_ms) ((val_ms) * 1000)
+#define MS_TO_NS(val_ms) ((time_ns_t) (val_ms) * 1000)
 #define SEC_TO_NS(val_sec) ((val_sec) * 1000LL * 1000 * 1000)
 
 // old macOS compat
---
+++ src/video_rxtx/loopback.c
@@ -0,0 +1,188 @@
+/*
+ * Copyright (c) 2018-2026 CESNET, zájmové sdružení právnických osob
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of CESNET nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING,
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+/**
+ * @file
+ * @author Martin Pulec     <pulec@cesnet.cz>
+ * @todo
+ * * works only when device can directly display the codec natively
+ * * add also audio
+ */
+
+#include <pthread.h> // for pthread_mutex_lock, pthread_mutex_unlock
+#include <stdlib.h>  // for calloc, free
+#include <string.h>  // for memcpy
+
+#include "compat/c23.h"    // IWYU pragma: keep
+#include "debug.h"         // for LOG_LEVEL_ERROR, LOG_LEVEL_WARNING, MSG
+#include "host.h"          // for register_should_exit_callback, unregister...
+#include "lib_common.h"    // for REGISTER_MODULE, library_class
+#include "tv.h"            // for MS_TO_NS, time_ns_t
+#include "utils/list.h"    // for simple_linked_list_size, simple_linked_li...
+#include "utils/pthread.h" // for CHK_PTHR, ug_pthread_cond_init, ug_pthrea...
+#include "utils/thread.h"  // for set_thread_name
+#include "video_display.h" // for PUTF_BLOCKING, display_put_frame, display...
+#include "video_frame.h"   // for video_desc_eq, video_desc_from_frame
+
+#define MOD_NAME "[rxtx/loopback] "
+
+static const int BUFF_MAX_LEN = 2;
+
+#include "types.h"
+#include "video_rxtx.h"
+
+struct loopback_video_rxtx {
+        struct module             *parent;
+        struct display            *display_device;
+        struct video_desc          configured_desc;
+        struct simple_linked_list *frames;
+        pthread_cond_t             frame_ready;
+        pthread_mutex_t            lock;
+};
+
+static void*
+init(const struct vrxtx_params *params)
+{
+        struct loopback_video_rxtx *s = calloc(1, sizeof *s);
+        s->parent = params->parent;
+        s->display_device = params->display_device;
+        s->frames = simple_linked_list_init();
+        ug_pthread_mutex_init(&s->lock);
+        ug_pthread_cond_init(&s->frame_ready);
+        return s;
+}
+
+static void
+send_frame(void *state, struct video_frame *f)
+{
+        struct loopback_video_rxtx *s = state;
+
+        CHK_PTHR(pthread_mutex_lock(&s->lock));
+        {
+                if (simple_linked_list_size(s->frames) >= BUFF_MAX_LEN) {
+                        MSG(WARNING, "Max buffer len %d exceeded.\n",
+                            BUFF_MAX_LEN);
+                }
+                simple_linked_list_append(s->frames, f);
+        }
+        CHK_PTHR(pthread_mutex_unlock(&s->lock));
+
+        CHK_PTHR(pthread_cond_signal(&s->frame_ready));
+}
+
+static void
+should_exit_callback(void *should_exit)
+{
+        *(bool *) should_exit = true;
+}
+
+static void *
+receiver_thread(void *arg)
+{
+        set_thread_name(__func__);
+
+        struct loopback_video_rxtx *s = arg;
+
+        bool should_exit = false;
+        register_should_exit_callback(s->parent, should_exit_callback, &should_exit);
+
+        while (!should_exit) {
+                struct video_frame *frame = nullptr;
+                CHK_PTHR(pthread_mutex_lock(&s->lock));
+                {
+                        time_ns_t timeout = MS_TO_NS(100);
+                        while (simple_linked_list_size(s->frames) == 0 &&
+                               timeout != 0) {
+                                ug_pthread_cond_timedwait(&s->frame_ready,
+                                                          &s->lock, &timeout);
+                        }
+                        if (simple_linked_list_size(s->frames) == 0) {
+                                CHK_PTHR(pthread_mutex_unlock(&s->lock));
+                                continue;
+                        }
+                        frame = simple_linked_list_pop(s->frames);
+                }
+                CHK_PTHR(pthread_mutex_unlock(&s->lock));
+
+                struct video_desc new_desc = video_desc_from_frame(frame);
+                if (!video_desc_eq(s->configured_desc, new_desc)) {
+                        bool reconfigured = display_reconfigure(s->display_device, new_desc, VIDEO_NORMAL);
+                        if (!reconfigured) {
+                                MSG(ERROR, "Unable to reconfigure display!\n");
+                                frame->callbacks.dispose(frame);
+                                continue;
+                        }
+                        s->configured_desc = new_desc;
+                }
+                struct video_frame *display_f = display_get_frame(s->display_device);
+                memcpy(display_f->tiles[0].data, frame->tiles[0].data, frame->tiles[0].data_len);
+                display_put_frame(s->display_device, display_f, PUTF_BLOCKING);
+                frame->callbacks.dispose(frame);
+        }
+        display_put_frame(s->display_device, nullptr, PUTF_BLOCKING);
+        unregister_should_exit_callback(s->parent, should_exit_callback,
+                                        &should_exit);
+
+        return nullptr;
+}
+
+static void done(void *state) {
+        struct loopback_video_rxtx *s = state;
+
+        struct video_frame *f = nullptr;
+        while ((f = simple_linked_list_pop(s->frames))) {
+                f->callbacks.dispose(f);
+        }
+
+        simple_linked_list_destroy(s->frames);
+        CHK_PTHR(pthread_cond_destroy(&s->frame_ready));
+        CHK_PTHR(pthread_mutex_destroy(&s->lock));
+        free(s);
+}
+
+static const struct video_rxtx_info loopback_video_rxtx_info = {
+        .long_name    = "loopback dummy transport",
+        .create       = init,
+        .done         = done,
+        .ctl_property = nullptr,
+
+        .send_audio_frame = nullptr,
+        .recv_audio_frame = nullptr,
+
+        .send_video_frame   = nullptr,
+        .send_video_frame_c = send_frame,
+        .video_recv_routine = receiver_thread,
+        .join_video_sender  = nullptr,
+};
+
+REGISTER_MODULE(loopback, &loopback_video_rxtx_info, LIBRARY_CLASS_VIDEO_RXTX, VIDEO_RXTX_ABI_VERSION);
+
