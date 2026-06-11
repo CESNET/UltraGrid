CESNET/UltraGrid

commit a2d3eb8c2bb44dd83827c7a7aa185f8d567312e0

fixed swittcher when :excl_init is used
\+ fixed device names not being stored except first device with excl_init

see also commit a24e194d fixing the issue without that option

refers to GH-336

src/video_capture/switcher.c
@@ -197,9 +197,11 @@ vidcap_switcher_init(struct vidcap_params *params, void **state)
                                                 vidcap_params_get_fmt(tmp));
                                 goto error;
                         }
-                        snprintf(s->device_names[i], sizeof s->device_names[i], "%s%s%s", vidcap_params_get_driver(tmp),
-                                                strlen(vidcap_params_get_fmt(tmp)) > 0 ? ":" : "", vidcap_params_get_fmt(tmp));
                 }
+                snprintf(s->device_names[i], sizeof s->device_names[i],
+                         "%s%s%s", vidcap_params_get_driver(tmp),
+                         strlen(vidcap_params_get_fmt(tmp)) > 0 ? ":" : "",
+                         vidcap_params_get_fmt(tmp));
         }
 
         s->params = params;
@@ -261,7 +263,7 @@ vidcap_switcher_grab(void *state, struct audio_frame **audio)
                         if (s->excl_init) {
                                 vidcap_done(s->devices[s->selected_device]);
                                 s->devices[s->selected_device] = NULL;
-                                int ret = initialize_video_capture(NULL,
+                                int ret = initialize_video_capture(&s->mod,
                                                 vidcap_params_get_nth((struct vidcap_params *) s->params, new_selected_device + 1),
                                                 &s->devices[new_selected_device]);
                                 assert(ret == 0);

commit 3924390bd3deacdbcbcf082ed7cb08d93e6adaaa

fix: SEGV when switching v4l2 devices with switcher:excl_init
After the init loop advances 'tmp' through all devices via
vidcap_params_get_next(), tmp points to the NULL-driver stopper node,
not the head of the params list. Passing it to vidcap_params_copy_all()
stored only a single stopper node instead of the full device chain.

When a subsequent device switch occurred (via capture.data N),
vidcap_params_get_nth() returned NULL for all devices, which was passed
to initialize_video_capture() and caused a SEGV in
vidcap_params_get_next(NULL).

Use the original 'params' pointer (head of list) instead of the
exhausted loop variable.

Bug introduced in commit a2d3eb8c2 ("fixed switcher when :excl_init is used", Sep 2023).

src/video_capture/switcher.c
@@ -241,7 +241,7 @@ vidcap_switcher_init(const struct vidcap_params *params, void **state)
                 }
         }
 
-        s->params = vidcap_params_copy_all(tmp);
+        s->params = vidcap_params_copy_all(params);
 
         vidcap_switcher_register_keyboard_ctl(s);
 