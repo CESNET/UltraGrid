6b8da9d9e7490a39e5f3c1d1d6d9d0e97c67fde4
Martin Piatka <piatka@cesnet.cz>
2026-07-02 08:51:21 +0200

    hwaccel/vaapi: Remove ancient ffmpeg compat
    This was needed on ffmpeg older than the 3.3 release series which is more than 9 years old now. Really no point in leaving this conditional preprocessor mess around.

--- src/hwaccel_vaapi.c
+++ src/hwaccel_vaapi.c
@@ -48,9 +48,6 @@
 
 #include <libavcodec/version.h>
 #include <libavutil/pixdesc.h>
-#if LIBAVCODEC_VERSION_INT < AV_VERSION_INT(57, 74, 100)
-#include <libavcodec/vaapi.h>
-#endif
 #include <libavutil/hwcontext_vaapi.h>
 
 #include "debug.h"
@@ -68,133 +65,12 @@ struct vaapi_ctx {
 
         AVBufferRef       *hw_frames_ctx;
         AVHWFramesContext *frame_ctx;
-
-#if LIBAVCODEC_VERSION_INT < AV_VERSION_INT(57, 74, 100)
-        VAProfile    va_profile;
-        VAEntrypoint va_entrypoint;
-        VAConfigID   va_config;
-        VAContextID  va_context;
-
-        struct vaapi_context decoder_context;
-#endif
 };
 
 void vaapi_uninit(struct hw_accel_state *s){
 
         free(s->ctx);
 }
-
-#if LIBAVCODEC_VERSION_INT < AV_VERSION_INT(57, 74, 100)
-static const struct {
-        enum AVCodecID av_codec_id;
-        int codec_profile;
-        VAProfile va_profile;
-} vaapi_profiles[] = {
-        {AV_CODEC_ID_MPEG2VIDEO, FF_PROFILE_MPEG2_SIMPLE, VAProfileMPEG2Simple},
-        {AV_CODEC_ID_MPEG2VIDEO, FF_PROFILE_MPEG2_MAIN, VAProfileMPEG2Main},
-        {AV_CODEC_ID_H264, FF_PROFILE_H264_CONSTRAINED_BASELINE, VAProfileH264ConstrainedBaseline},
-        {AV_CODEC_ID_H264, FF_PROFILE_H264_BASELINE, VAProfileH264Baseline},
-        {AV_CODEC_ID_H264, FF_PROFILE_H264_MAIN, VAProfileH264Main},
-        {AV_CODEC_ID_H264, FF_PROFILE_H264_HIGH, VAProfileH264High},
-#if VA_CHECK_VERSION(0, 37, 0)
-        {AV_CODEC_ID_HEVC, FF_PROFILE_HEVC_MAIN, VAProfileHEVCMain},
-#endif
-};
-
-static int vaapi_create_context(struct vaapi_ctx *ctx,
-                AVCodecContext *codec_ctx)
-{
-        const AVCodecDescriptor *codec_desc;
-
-        codec_desc = avcodec_descriptor_get(codec_ctx->codec_id);
-        if(!codec_desc){
-                return -1;
-        }
-
-        int profile_count = vaMaxNumProfiles(ctx->device_vaapi_ctx->display);
-        log_msg(LOG_LEVEL_VERBOSE, "VAAPI Profile count: %d\n", profile_count);
-
-        VAProfile *list = av_malloc(profile_count * sizeof(VAProfile));
-        if(!list){
-                return -1;
-        }
-
-        VAStatus status = vaQueryConfigProfiles(ctx->device_vaapi_ctx->display,
-                        list, &profile_count);
-        if(status != VA_STATUS_SUCCESS){
-                log_msg(LOG_LEVEL_ERROR, "[lavd] Profile query failed: %d (%s)\n", status, vaErrorStr(status));
-                av_free(list);
-                return -1;
-        }
-
-        VAProfile profile = VAProfileNone;
-        int match = 0;
-
-        for(unsigned i = 0; i < FF_ARRAY_ELEMS(vaapi_profiles); i++){
-                if(vaapi_profiles[i].av_codec_id != codec_ctx->codec_id)
-                        continue;
-
-                if(vaapi_profiles[i].codec_profile == codec_ctx->profile){
-                        profile = vaapi_profiles[i].va_profile;
-                        break;
-                }
-        }
-
-        for(int i = 0; i < profile_count; i++){
-                if(profile == list[i])
-                        match = 1;
-        }
-
-        av_freep(&list);
-
-        if(!match){
-                log_msg(LOG_LEVEL_ERROR, "[lavd] Profile not supported \n");
-                return -1;
-        }
-
-        ctx->va_profile = profile;
-        ctx->va_entrypoint = VAEntrypointVLD;
-
-        status = vaCreateConfig(ctx->device_vaapi_ctx->display, ctx->va_profile,
-                        ctx->va_entrypoint, 0, 0, &ctx->va_config);
-        if(status != VA_STATUS_SUCCESS){
-                log_msg(LOG_LEVEL_ERROR, "[lavd] Create config failed: %d (%s)\n", status, vaErrorStr(status));
-                return -1;
-        }
-
-        AVVAAPIHWConfig *hwconfig = av_hwdevice_hwconfig_alloc(ctx->device_ref);
-        if(!hwconfig){
-                log_msg(LOG_LEVEL_WARNING, "[lavd] Failed to get constraints. Will try to continue anyways...\n");
-                return 0;
-        }
-
-        hwconfig->config_id = ctx->va_config;
-        AVHWFramesConstraints *constraints = av_hwdevice_get_hwframe_constraints(ctx->device_ref, hwconfig);
-        if (!constraints){
-                log_msg(LOG_LEVEL_WARNING, "[lavd] Failed to get constraints. Will try to continue anyways...\n");
-                av_freep(&hwconfig);
-                return 0;
-        }
-
-        if (codec_ctx->coded_width  < constraints->min_width  ||
-                        codec_ctx->coded_width  > constraints->max_width  ||
-                        codec_ctx->coded_height < constraints->min_height ||
-                        codec_ctx->coded_height > constraints->max_height)
-        {
-                log_msg(LOG_LEVEL_WARNING, "[lavd] VAAPI hw does not support the resolution %dx%d\n",
-                                codec_ctx->coded_width,
-                                codec_ctx->coded_height);
-                av_hwframe_constraints_free(&constraints);
-                av_freep(&hwconfig);
-                return -1;
-        }
-
-        av_hwframe_constraints_free(&constraints);
-        av_freep(&hwconfig);
-
-        return 0;
-}
-#endif //LIBAVCODEC_VERSION_INT < AV_VERSION_INT(57, 74, 100)
 
 /**
  * Returns first SW format from valid_sw_formats. This is usually
@@ -291,12 +167,6 @@ int vaapi_init(struct AVCodecContext *s,
         ctx->device_ctx = (AVHWDeviceContext*)(void *)ctx->device_ref->data;
         ctx->device_vaapi_ctx = ctx->device_ctx->hwctx;
 
-#if LIBAVCODEC_VERSION_INT < AV_VERSION_INT(57, 74, 100)
-        ret = vaapi_create_context(ctx, s);
-        if(ret < 0)
-                goto fail;
-#endif
-
         int decode_surfaces = DEFAULT_SURFACES;
 
         if (s->active_thread_type & FF_THREAD_FRAME)
@@ -328,28 +198,6 @@ int vaapi_init(struct AVCodecContext *s,
         state->ctx = ctx;
         state->uninit = vaapi_uninit;
 
-#if LIBAVCODEC_VERSION_INT < AV_VERSION_INT(57, 74, 100)
-        AVVAAPIFramesContext *avfc = ctx->frame_ctx->hwctx;
-        VAStatus status = vaCreateContext(ctx->device_vaapi_ctx->display,
-                        ctx->va_config, s->coded_width, s->coded_height,
-                        VA_PROGRESSIVE,
-                        avfc->surface_ids,
-                        avfc->nb_surfaces,
-                        &ctx->va_context);
-
-        if(status != VA_STATUS_SUCCESS){
-                log_msg(LOG_LEVEL_ERROR, "[lavd] Create config failed: %d (%s)\n", status, vaErrorStr(status));
-                ret = -1;
-                goto fail;
-        }
-
-        ctx->decoder_context.display = ctx->device_vaapi_ctx->display;
-        ctx->decoder_context.config_id = ctx->va_config;
-        ctx->decoder_context.context_id = ctx->va_context;
-
-        s->hwaccel_context = &ctx->decoder_context;
-#endif //LIBAVCODEC_VERSION_INT < AV_VERSION_INT(57, 74, 100)
-
         av_buffer_unref(&ctx->device_ref);
         return 0;
 
@@ -357,10 +205,6 @@ int vaapi_init(struct AVCodecContext *s,
 fail:
         av_frame_free(&state->tmp_frame);
         av_buffer_unref(&ctx->hw_frames_ctx);
-#if LIBAVCODEC_VERSION_INT < AV_VERSION_INT(57, 74, 100)
-        if(ctx->device_vaapi_ctx)
-                vaDestroyConfig(ctx->device_vaapi_ctx->display, ctx->va_config);
-#endif
         av_buffer_unref(&ctx->device_ref);
         free(ctx);
         return ret;
