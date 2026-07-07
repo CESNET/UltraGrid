add83c2db9d451408f94d8391fa7c27e64930f6c
Martin Pulec <martin.pulec@cesnet.cz>
2026-07-07 10:28:42 +0200

    cf/resize: OpenCV 5 compat

--- src/capture_filter/resize_utils.cpp
+++ src/capture_filter/resize_utils.cpp
@@ -52,7 +52,9 @@
 #pragma GCC diagnostic ignored "-Wcast-align"
 #pragma GCC diagnostic ignored "-Wcast-qual"
 #include <opencv2/imgproc.hpp>
+#if __has_include(<opencv2/imgproc/types_c.h>)
 #include <opencv2/imgproc/types_c.h>
+#endif
 #pragma GCC diagnostic pop
 
 #include "debug.h"
@@ -94,20 +96,20 @@ static Mat ug_to_rgb_mat(codec_t codec, int width, int height, char *indata) {
         rgb.data = (uchar*)indata;
         return rgb;
     case RGBA:
-        cv_color = CV_RGBA2RGB;
+        cv_color = cv::COLOR_RGBA2RGB;
         pix_fmt = CV_8UC4;
         break;
     case I420:
         pix_fmt = CV_8U;
         num = 3;
         den = 2;
-        cv_color = CV_YUV2RGB_I420;
+        cv_color = cv::COLOR_YUV2RGB_I420;
         break;
     case UYVY:
-        cv_color = CV_YUV2RGB_UYVY;
+        cv_color = cv::COLOR_YUV2RGB_UYVY;
         break;
     case YUYV:
-        cv_color = CV_YUV2RGB_YUYV;
+        cv_color = cv::COLOR_YUV2RGB_YUYV;
         break;
     default:
         LOG(LOG_LEVEL_ERROR) << MOD_NAME "Unsupported codec: " << codec << "\n";
