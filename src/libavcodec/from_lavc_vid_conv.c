CESNET/UltraGrid

commit ecd4c53bf8642e4ad4b5dc1e934420173ea92594

from_lavc_vid_conv: fix verbose message et al.
1. from_lavc_vid_conv after av_to_uv codec was passed incorrectly as final
2. ret->src_pixfmt was used as intermediate - it is ok, it equals to the
above, however (so that 'x over x' were always the same)

in the second (now deleted it was correctly)

so:
- deleted the second instance (needlessly duplicate)
- if no uv_to_uv conversion occur, do not print uv intermediate (2. and 3.
argument will be the same but semantically incorrect - neither memcpy
occurs)

src/libavcodec/from_lavc_vid_conv.c
@@ -2380,9 +2380,17 @@ get_av_to_uv_conversion(int av_codec, codec_t uv_codec)
         if (ret == NULL) {
                 return NULL;
         }
-        MSG(VERBOSE, "converting %s to %s over %s\n",
-            av_get_pix_fmt_name(av_codec), get_codec_name(ret->conversion->uv_codec),
-            get_codec_name(ret->src_pixfmt));
+        if (ret->dst_pixfmt == ret->conversion->uv_codec) {
+                MSG(VERBOSE, "converting %s to %s\n",
+                    av_get_pix_fmt_name(av_codec),
+                    get_codec_name(ret->dst_pixfmt));
+        } else {
+                MSG(VERBOSE,
+                    "converting %s to %s with intermediate UG conv over %s\n",
+                    av_get_pix_fmt_name(av_codec),
+                    get_codec_name(ret->dst_pixfmt),
+                    get_codec_name(ret->conversion->uv_codec));
+        }
         return ret;
 }
 
@@ -2424,8 +2432,6 @@ static enum AVPixelFormat get_ug_codec_to_av(const enum AVPixelFormat *fmt, code
                         // AV+UV conversion needed
                         codec_t c;
                         if (get_av_and_uv_conversion(*fmt_it, *ugc, &c, NULL)) {
-                                log_msg(LOG_LEVEL_VERBOSE, __FILE__ ": selected conversion from %s to %s with %s intermediate.\n",
-                                                av_get_pix_fmt_name(*fmt_it), get_codec_name(*ugc), get_codec_name(c));
                                 return *fmt_it;
                         }
                 } else { // probe

commit 5bb01cb997d18fc9ad89086cb43f185794fcf1eb

from_lavc_vid_conv: guard NULL conversion in get_av_to_uv_conversion
get_av_to_uv_conversion_int() can return a valid struct with
conversion == NULL (direct pixfmt map, decoder, or CUDA early-return
paths), but since ecd4c53bf the VERBOSE message unconditionally
dereferences ret->conversion->uv_codec, SEGV when -t file: decodes to a
directly-mapped format.

Regression introduced in ecd4c53bf.

src/libavcodec/from_lavc_vid_conv.c
@@ -2353,7 +2353,8 @@ get_av_to_uv_conversion(int av_codec, codec_t uv_codec)
         if (ret == NULL) {
                 return NULL;
         }
-        if (ret->dst_pixfmt == ret->conversion->uv_codec) {
+        if (ret->conversion == NULL ||
+            ret->dst_pixfmt == ret->conversion->uv_codec) {
                 MSG(VERBOSE, "converting %s to %s\n",
                     av_get_pix_fmt_name(av_codec),
                     get_codec_name(ret->dst_pixfmt));