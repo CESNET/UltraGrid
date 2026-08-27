CESNET/UltraGrid

commit e9d01fd7ce942782c53ea42074389c8db080a6ab

vcap/dshow: fix gcc warn
suggested braces around empty body

src/video_capture/DirectShowGrabber.cpp
@@ -222,11 +222,12 @@ static bool cleanup(struct vidcap_dshow_state *s) {
 }
 
 #define HANDLE_ERR_ACTION(res, action, msg, ...) \
-        if (res != S_OK) { \
+        do {\
+                if (res == S_OK) break;\
                 MSG(ERROR, msg ": %s\n", \
                     __VA_ARGS__ __VA_OPT__(, ) hresult_to_str(res)); \
                 action; \
-        } else
+        } while(0)
 
 static bool common_init(struct vidcap_dshow_state *s) {
 #define HANDLE_ERR(msg, ...) \

commit a014659f6097ed32ffa8684077584fef5b40aaa2

capture/dshow: Fix faulty macro causing crash on probe
Introduced in e9d01fd7, the do while loop eats the 'continue' passed through tne 'action' parameter  called from dshow_probe_internal so it didn't stop on failure.
For now fix the macro by using an if else. (void)0 in the else branch suppresses the warning that the mentioned commit aimed to avoid.
It would be ideal to eventually rewrite without the use of macros, because they really are just too bug-prone.

src/video_capture/DirectShowGrabber.cpp
@@ -225,12 +225,11 @@ static bool cleanup(struct vidcap_dshow_state *s) {
 }
 
 #define HANDLE_ERR_ACTION(res, action, msg, ...) \
-        do {\
-                if (res == S_OK) break;\
+        if (res == S_OK) { \
                 MSG(ERROR, msg ": %s\n", \
                     __VA_ARGS__ __VA_OPT__(, ) hresult_to_str(res)); \
                 action; \
-        } while(0)
+        } else {(void)0;}
 
 static bool common_init(struct vidcap_dshow_state *s) {
 #define HANDLE_ERR(msg, ...) \